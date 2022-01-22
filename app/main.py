9b322920bb1dd3de9ce3c7ca7b575953055109e8
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-01-22 17:49:49 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -7,14 +7,20 @@ class Vector:
         self.y = round(y, 2)
 
     def __add__(self, other):
-        x = self.x + other.x
-        y = self.y + other.y
-        return Vector(x, y)
+        if isinstance(other, Vector):
+            x = self.x + other.x
+            y = self.y + other.y
+            return Vector(x, y)
+        else:
+            raise TypeError
 
     def __sub__(self, other):
-        x = self.x - other.x
-        y = self.y - other.y
-        return Vector(x, y)
+        if isinstance(other, Vector):
+            x = self.x - other.x
+            y = self.y - other.y
+            return Vector(x, y)
+        else:
+            raise TypeError
 
     def __mul__(self, other):
         if isinstance(other, (int, float)):
@@ -29,7 +35,7 @@ class Vector:
     def create_vector_by_two_points(cls, start_point, end_point):
         cls.x = end_point[0] - start_point[0]
         cls.y = end_point[1] - start_point[1]
-        return Vector(cls.x, cls.y)
+        return cls(cls.x, cls.y)
 
     def get_length(self):
         return (self.x ** 2 + self.y ** 2) ** 0.5
@@ -46,13 +52,13 @@ class Vector:
         return round(math.degrees(math.acos(cos_a)))
 
     def get_angle(self):
-        m = self.y - self.x
-        cos_a = (self.y * m) / (self.get_length() * m)
+        yaxis = self.y - self.x
+        cos_a = (self.y * yaxis) / (self.get_length() * yaxis)
         return round(math.degrees(math.acos(cos_a)))
 
     def rotate(self, degrees):
-        m = math.radians(degrees)
+        converted = math.radians(degrees)
         return Vector(
-            x=(math.cos(m) * self.x) - (math.sin(m) * self.y),
-            y=(math.sin(m) * self.x) + (math.cos(m) * self.y)
+            x=(math.cos(converted) * self.x) - (math.sin(converted) * self.y),
+            y=(math.sin(converted) * self.x) + (math.cos(converted) * self.y)
         )
