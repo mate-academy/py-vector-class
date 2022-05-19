bcb4653839e2199ce7af5126b444d453135a4cbf
Roman <romanschneider9@gmail.com>
2022-05-19 20:40:41 +0300

    replaced cls variable with class name / changed the way of using magic methods

--- app/main.py
+++ app/main.py
@@ -30,9 +30,9 @@ class Vector:
 
     @classmethod
     def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple):
-        start_point = Vector(start_point[0], start_point[1])
-        end_point = Vector(end_point[0], end_point[1])
-        return end_point.__sub__(start_point)
+        start_point = cls(start_point[0], start_point[1])
+        end_point = cls(end_point[0], end_point[1])
+        return end_point - start_point
 
     def get_length(self):
         length = (self.x ** 2 + self.y ** 2) ** 0.5
@@ -45,7 +45,7 @@ class Vector:
         return Vector(x, y)
 
     def angle_between(self, vector):
-        dot_product = self.__mul__(vector)
+        dot_product = self * vector
         length_1 = self.get_length()
         length_2 = vector.get_length()
         cosine = dot_product / (length_1 * length_2)
