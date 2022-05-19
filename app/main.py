ab532df96ddda04143dd336ce6522a2a4d11b2ff
Dorius <kornelydorius@gmail.com>
2022-05-19 18:32:12 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,46 @@
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x, y):
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        return (self.x * other.x) + (self.y * other.y)
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point, end_point):
+        local_x = end_point[0] - start_point[0]
+        local_y = end_point[1] - start_point[1]
+        return Vector(local_x, local_y)
+
+    def get_length(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self):
+        vector_length = self.get_length()
+        return Vector(self.x / vector_length, self.y / vector_length)
+
+    def angle_between(self, other):
+        cos_a = (self * other) / (self.get_length() * other.get_length())
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self):
+        cos_a = self.y / self.get_length()
+        return round(math.degrees(math.acos(cos_a)))
+
+    def rotate(self, degrees):
+        rad = math.radians(degrees)
+        return Vector(
+            (math.cos(rad) * self.x) - (math.sin(rad) * self.y),
+            (math.sin(rad) * self.x) + (math.cos(rad) * self.y)
+        )
