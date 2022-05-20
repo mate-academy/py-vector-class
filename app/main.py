0fe6d0df3934d0c158477bc186aff88448d4728d
Igor Cegelnyk <il.cegelnik@gmail.com>
2022-05-20 19:01:57 +0300

    'Second solution'

--- app/main.py
+++ app/main.py
@@ -8,23 +8,23 @@ class Vector:
         self.y = round(y, 2)
 
     def __add__(self, other):
-        self.x = round(self.x + other.x, 2)
-        self.y = round(self.y + other.y, 2)
-        return self
+        x = round(self.x + other.x, 2)
+        y = round(self.y + other.y, 2)
+        return Vector(x, y)
 
     def __sub__(self, other):
-        self.x = round(self.x - (other.x), 2)
-        self.y = round(self.y - (other.y), 2)
-        return self
+        x = round(self.x - (other.x), 2)
+        y = round(self.y - (other.y), 2)
+        return Vector(x, y)
 
     def __mul__(self, other):
         if isinstance(other, Vector):
             product = self.x * other.x + (self.y * other.y)
             return product
         elif isinstance(other, (int, float)):
-            self.x = round(self.x * other, 2)
-            self.y = round(self.y * other, 2)
-            return self
+            x = round(self.x * other, 2)
+            y = round(self.y * other, 2)
+            return Vector(x, y)
 
     @classmethod
     def create_vector_by_two_points(cls,
@@ -32,7 +32,7 @@ class Vector:
                                     end_point: tuple):
         x = end_point[0] - start_point[0]
         y = end_point[1] - start_point[1]
-        return cls(x, y)
+        return Vector(x, y)
 
     def get_length(self):
         return math.sqrt(self.x ** 2 + (self.y ** 2))
@@ -40,9 +40,7 @@ class Vector:
     def get_normalized(self):
         x = round(self.x / Vector.get_length(self), 2)
         y = round(self.y / Vector.get_length(self), 2)
-        self.x = x
-        self.y = y
-        return self
+        return Vector(x, y)
 
     def angle_between(self, vector):
         length_self = Vector.get_length(self)
@@ -62,6 +60,4 @@ class Vector:
         sin_a = math.sin(radians)
         x = round(self.x * cos_a - self.y * sin_a, 2)
         y = round(self.x * sin_a + self.y * cos_a, 2)
-        self.x = x
-        self.y = y
-        return self
+        return Vector(x, y)
