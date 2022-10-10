ec9e16492cd7630326df4bb950587baa41195ebf
Roman Pisarskiy <darkvader.ukr.net@gmail.com>
2022-10-10 15:35:25 +0300

    "init, add" has create

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: (int, float), y: (int, float)) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: (int, float)) -> object:
+        other_distance = other if isinstance(other, (int, float)) else other.km
+        return Distance(self.km + other_distance)
