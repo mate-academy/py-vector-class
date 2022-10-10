2f7715c1cf062955909a7bb4609e1a5af39a8172
Roman Pisarskiy <darkvader.ukr.net@gmail.com>
2022-10-10 22:08:02 +0300

    "@classmetod" has create

--- app/main.py
+++ app/main.py
@@ -3,16 +3,25 @@ class Vector:
         self.x = round(x, 2)
         self.y = round(y, 2)
 
-    def __add__(self, other: (int, float)) -> object:  # done -?
+    def __add__(self, other: (int, float)) -> object:
         return self.x + other.x, self.y + other.y
 
     def __sub__(self, other):
         return self.x - other.x, self.y - other.y
 
-    def __mul__(self, other):  # done - V
+    def __mul__(self, other):
         if isinstance(other, (int, float)):
             return Vector(self.x * other, self.y * other)
         else:
             return (self.x * other.x) + (self.y * other.y)
-    @staticmethod
-    def create_vector_by_two_points(start_point, end_point):
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point, end_point):
+        return cls(end_point[0] - start_point[0],
+                   end_point[1] - end_point[1]
+                   )
+
+start_point = (5.2, 2.6)
+end_point = (10.7, 6)
+
+vector = Vector.create_vector_by_two_points(start_point, end_point)
