dc4b9ca94048e5c6b23a0b407dedc3186e5d39e7
Roman Pisarskiy <darkvader.ukr.net@gmail.com>
2022-10-10 16:26:05 +0300

    "sub, mul" has create

--- app/main.py
+++ app/main.py
@@ -3,6 +3,16 @@ class Vector:
         self.x = round(x, 2)
         self.y = round(y, 2)
 
-    def __add__(self, other: (int, float)) -> object:
-        other_distance = other if isinstance(other, (int, float)) else other.km
-        return Distance(self.km + other_distance)
+    def __add__(self, other: (int, float)) -> object:  # done -?
+        return self.x + other.x, self.y + other.y
+
+    def __sub__(self, other):
+        return self.x - other.x, self.y - other.y
+
+    def __mul__(self, other):  # done - V
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        else:
+            return (self.x * other.x) + (self.y * other.y)
+    @staticmethod
+    def create_vector_by_two_points(start_point, end_point):
