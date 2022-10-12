59a5368290dad9820f1c6f9eca1896829a4f97dc
Roman Pisarskiy <darkvader.ukr.net@gmail.com>
2022-10-12 17:22:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,35 +1,38 @@
-from math import *
+from math import degrees, acos, cos, sin, radians, sqrt
 
 
 class Vector:
-    def __init__(self, x: (int, float), y: (int, float)) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_cord: (int, float), y_cord: (int, float)) -> None:
+        self.x = round(x_cord, 2)
+        self.y = round(y_cord, 2)
 
     def __add__(self, other: (int, float)) -> object:
         return Vector(
             round(self.x + other.x, 2),
             round(self.y + other.y, 2))
 
-    def __sub__(self, other):
+    def __sub__(self, other: object) -> object:
         return Vector(
             round(self.x - other.x, 2),
             round(self.y - other.y, 2))
 
-    def __mul__(self, other):
+    def __mul__(self, other: object) -> object:
         if isinstance(other, (int, float)):
             return Vector(self.x * other, self.y * other)
         else:
             return (self.x * other.x) + (self.y * other.y)
 
     @classmethod
-    def create_vector_by_two_points(cls, start_cord: tuple, end_cord: tuple) -> "Vector":
+    def create_vector_by_two_points(
+            cls,
+            start_cord: tuple,
+            end_cord: tuple) -> "Vector":
         return cls(
             round(end_cord[0] - start_cord[0], 2),
             round(end_cord[1] - start_cord[1], 2)
-            )
+        )
 
-    def get_length(self):
+    def get_length(self) -> object:
         return sqrt((self.x - 0) ** 2 + (self.y - 0) ** 2)
 
     def get_normalized(self) -> "Vector":
@@ -39,7 +42,7 @@ class Vector:
 
     def angle_between(self, other: "Vector") -> int:
         cos_temp = (Vector.__mul__(self, other)) / \
-              (Vector.get_length(self) * abs(Vector.get_length(other)))
+            (Vector.get_length(self) * abs(Vector.get_length(other)))
         return round(degrees(acos(cos_temp)))
 
     def get_angle(self) -> float:
