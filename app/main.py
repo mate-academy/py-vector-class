d3633eb69df15ed0372409315af8896412242c61
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2022-10-13 16:06:28 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,50 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x_axis: float, y_axis: float) -> None:
+        self.x = round(x_axis, 2)
+        self.y = round(y_axis, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: Vector | float) -> Vector | float:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        else:
+            return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(cls,
+                                    start_point: tuple,
+                                    end_point: tuple) -> Vector:
+        return Vector(end_point[0] - start_point[0],
+                      end_point[1] - start_point[1])
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> Vector:
+        return Vector(self.x / Vector.get_length(self),
+                      self.y / Vector.get_length(self))
+
+    def angle_between(self, other: Vector) -> int:
+        cos_a = self * other / (self.get_length() * other.get_length())
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> int:
+        return Vector.angle_between(self, Vector(0, 1))
+
+    def rotate(self, degrees: int) -> Vector:
+        new_x = self.x * math.cos(
+            math.radians(degrees)) - self.y * math.sin(math.radians(degrees)
+                                                       )
+        new_y = self.x * math.sin(
+            math.radians(degrees)) + self.y * math.cos(math.radians(degrees)
+                                                       )
+        return Vector(new_x, new_y)
