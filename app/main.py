956a9d248cc8d0449ff32aaf39eb4af7690f3a37
Ihor Ivashov <dm-i@i.ua>
2022-10-27 17:25:51 +0300

    Fixed ax_

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, ax_x: float, ax_y: float) -> None:
+        self.x = round(ax_x, 2)
+        self.y = round(ax_y, 2)
 
     def __add__(self, other: object) -> object:
         return Vector(self.x + other.x, self.y + other.y)
