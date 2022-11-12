9eadb302e23fea9b132bb550e4067cfa49588730
Alex Isaev <lesisv@gmail.com>
2022-11-12 13:55:23 +0200

    Solution fixed

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
+
 import math
 
 
 class Vector:
 
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, coordinate_x: float, coordinate_y: float) -> None:
+        self.x = round(coordinate_x, 2)
+        self.y = round(coordinate_y, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
