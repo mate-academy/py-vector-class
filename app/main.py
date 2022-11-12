1b7384beece405547ab19cd07e350cbad7e6af2e
Tatasha <NatashaLeonidovna319@gmail.com>
2022-11-12 15:21:58 +0200

    Changes

--- app/main.py
+++ app/main.py
@@ -4,27 +4,27 @@ import math
 
 
 class Vector:
-    def __init__(self, x: int, y: int) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: int, y_coord: int) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(
-            x=self.x + other.x,
-            y=self.y + other.y
+            self.x + other.x,
+            self.y + other.y
         )
 
     def __sub__(self, other: Vector) -> Vector:
         return Vector(
-            x=self.x - other.x,
-            y=self.y - other.y
+            self.x - other.x,
+            self.y - other.y
         )
 
     def __mul__(self, other: Union[int, Vector]) -> Union[float, Vector]:
         if type(other) == int or type(other) == float:
             return Vector(
-                x=self.x * other,
-                y=self.y * other
+                self.x * other,
+                self.y * other
             )
         return self.x * other.x + self.y * other.y
 
@@ -36,12 +36,12 @@ class Vector:
         if start_point[0] < 0 or start_point[1] < 0 or \
                 end_point[0] < 0 or end_point[1] < 0:
             return cls(
-                x=end_point[0] - start_point[0],
-                y=end_point[1] - start_point[1]
+                end_point[0] - start_point[0],
+                end_point[1] - start_point[1]
             )
         return cls(
-            x=end_point[0] + start_point[0],
-            y=end_point[1] + start_point[1]
+            end_point[0] + start_point[0],
+            end_point[1] + start_point[1]
         )
 
     def get_length(self) -> float:
@@ -49,8 +49,8 @@ class Vector:
 
     def get_normalized(self) -> Vector:
         return Vector(
-            x=self.x / self.get_length(),
-            y=self.y / self.get_length()
+            self.x / self.get_length(),
+            self.y / self.get_length()
         )
 
     def angle_between(self, vector: Vector) -> int:
@@ -59,15 +59,13 @@ class Vector:
                       / (self.get_length() * vector.get_length()))))
 
     def get_angle(self) -> int:
-        return round(math.degrees(
-            math.acos((self.x * 0 + self.y * 9)
-                      / (math.sqrt(self.x ** 2 + self.y ** 2)
-                         * (math.sqrt(0 ** 2 + 9 ** 2))))))
+        vector = Vector(0, 9)
+        return vector.angle_between(self)
 
     def rotate(self, degrees: int) -> Vector:
         return Vector(
-            x=(self.x * math.cos(math.radians(degrees))
-               - self.y * math.sin(math.radians(degrees))),
-            y=(self.x * math.sin(math.radians(degrees))
-               + self.y * math.cos(math.radians(degrees)))
+            (self.x * math.cos(math.radians(degrees))
+             - self.y * math.sin(math.radians(degrees))),
+            (self.x * math.sin(math.radians(degrees))
+             + self.y * math.cos(math.radians(degrees)))
         )
