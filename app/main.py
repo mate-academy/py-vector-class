787587051ee6a36de43cc97d2a313c254ce6f95d
Haishuk Pavlo <xomas@ukr.net>
2022-11-18 16:08:28 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,3 +1,57 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+
+    def __init__(self, x_point: float, y_point: float) -> None:
+        self.x = round(x_point, 2)
+        self.y = round(y_point, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: int | Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: int | float | Vector) -> float | Vector:
+        if isinstance(other, Vector):
+            return (self.x * other.x) + (self.y * other.y)
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls, start_point: tuple, end_point: tuple
+    ) -> Vector:
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
+
+    def get_length(self) -> float:
+        return math.sqrt((self.x ** 2) + (self.y ** 2))
+
+    def get_normalized(self) -> Vector:
+        return Vector(self.x / self.get_length(), self.y / self.get_length())
+
+    def angle_between(self, other: Vector) -> int:
+        product = self * other
+        len_self = self.get_length()
+        len_other = other.get_length()
+        cosine = product / (len_self * len_other)
+        return round(math.degrees(math.acos(cosine)))
+
+    def get_angle(self) -> int:
+        return self.angle_between(Vector(0, 1))
+
+    def rotate(self, degrees: int) -> Vector:
+        math1 = self.x * math.cos(math.radians(degrees))
+        math2 = self.y * math.sin(math.radians(degrees))
+        math3 = self.x * math.sin(math.radians(degrees))
+        math4 = self.y * math.cos(math.radians(degrees))
+
+        return Vector(
+            math1 - math2,
+            math3 + math4
+        )
