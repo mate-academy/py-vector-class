91d00600f6fec26849677ff729455b294859dd64
Sashkoooo <o.denyseko@hotmail.com>
2022-12-28 20:36:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
+from __future__ import annotations
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: int | float, y: int | float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+
+
