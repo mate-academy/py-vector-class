2ac07840d3f7a9ecd3420a5e5862fb18ddbc0fa9
Vadym Bihun <bihunva@gmail.com>
2023-02-04 20:54:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,15 +5,15 @@ import math
 
 
 class Vector:
-    def __init__(self, x: Real, y: Real) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: Real, y_coord: Real) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: Vector) -> Vector:
-        return Vector(x=self.x + other.x, y=self.y + other.y)
+        return Vector(x_coord=self.x + other.x, y_coord=self.y + other.y)
 
     def __sub__(self, other: Vector) -> Vector:
-        return Vector(x=self.x - other.x, y=self.y - other.y)
+        return Vector(x_coord=self.x - other.x, y_coord=self.y - other.y)
 
     def __mul__(self, other: Vector | Real) -> Vector | Real:
         if isinstance(other, Vector):
@@ -21,17 +21,22 @@ class Vector:
             return dot_product
 
         if isinstance(other, Real):
-            return Vector(x=self.x * other, y=self.y * other)
+            return Vector(x_coord=self.x * other, y_coord=self.y * other)
 
     @staticmethod
-    def create_vector_by_two_points(start_point: tuple, end_point: tuple) -> Vector:
-        return Vector(x=end_point[0] - start_point[0], y=end_point[1] - end_point[1])
+    def create_vector_by_two_points(
+            start_point: tuple,
+            end_point: tuple
+    ) -> Vector:
+        return Vector(x_coord=end_point[0] - start_point[0],
+                      y_coord=end_point[1] - end_point[1])
 
     def get_length(self) -> Real:
         return math.sqrt(self.x**2 + self.y**2)
 
     def get_normalized(self) -> Vector:
-        return Vector(x=self.x / self.get_length(), y=self.y / self.get_length())
+        return Vector(x_coord=self.x / self.get_length(),
+                      y_coord=self.y / self.get_length())
 
     def angle_between(self, other: Vector) -> Real:
         dot_product = self * other
@@ -42,7 +47,7 @@ class Vector:
 
         return round(math.degrees(math.acos(cos_theta)))
 
-    def get_angle(self):
+    def get_angle(self) -> Real:
         y_axis = Vector(0, 1)
         return self.angle_between(y_axis)
 
@@ -52,4 +57,3 @@ class Vector:
         rotated_vector_x = (cos * self.x) - (sin * self.y)
         rotated_vector_y = (sin * self.x) + (cos * self.y)
         return Vector(rotated_vector_x, rotated_vector_y)
-
