a5c5eb103c7eb68d9589f250729ba67864aafc28
Kristina <tina.vasylenko@gmail.com>
2023-03-24 09:48:03 +0200

    Solution1

--- app/main.py
+++ app/main.py
@@ -4,26 +4,26 @@ import math
 
 
 class Vector:
-    def __init__(self, ox: float, oy: float) -> None:
-        self.x = round(ox, 2)
-        self.y = round(oy, 2)
+    def __init__(self, x_: float, y_: float) -> None:
+        self.x = round(x_, 2)
+        self.y = round(y_, 2)
 
     def __add__(self, other: Any) -> Any:
         if isinstance(other, Vector):
             return Vector(
-                ox=self.x + other.x,
-                oy=self.y + other.y)
+                x_=self.x + other.x,
+                y_=self.y + other.y)
 
     def __sub__(self, other: Any) -> Any:
         if isinstance(other, Vector):
             return Vector(
-                ox=self.x - other.x,
-                oy=self.y - other.y)
+                x_=self.x - other.x,
+                y_=self.y - other.y)
 
     def __mul__(self, other: Any) -> Any:
         if isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
-        return Vector(ox=self.x * other, oy=self.y * other)
+        return Vector(x_=self.x * other, y_=self.y * other)
 
     @classmethod
     def create_vector_by_two_points(cls,
@@ -31,16 +31,16 @@ class Vector:
                                     end_point: Any
                                     ) -> Any:
         return Vector(
-            ox=end_point[0] - start_point[0],
-            oy=end_point[1] - start_point[1])
+            x_=end_point[0] - start_point[0],
+            y_=end_point[1] - start_point[1])
 
     def get_length(self) -> Any:
         return math.sqrt(self.x**2 + self.y**2)
 
     def get_normalized(self) -> Any:
         return Vector(
-            ox=round(self.x / self.get_length(), 2),
-            oy=round(self.y / self.get_length(), 2))
+            x_=round(self.x / self.get_length(), 2),
+            y_=round(self.y / self.get_length(), 2))
 
     def angle_between(self, other: Any) -> Any:
         cos_a = (self.__mul__(other)) / \
@@ -53,7 +53,7 @@ class Vector:
 
     def rotate(self, degrees: Any) -> Any:
         return Vector(
-            ox=math.cos(math.radians(degrees))
+            x_=math.cos(math.radians(degrees))
             * self.x - math.sin(math.radians(degrees)) * self.y,
-            oy=math.sin(math.radians(degrees))
+            y_=math.sin(math.radians(degrees))
             * self.x + math.cos(math.radians(degrees)) * self.y)
