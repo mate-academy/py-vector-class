da91e55749af80f53ba94e4255ffe0db0967ebbb
Natalia Stepanenko <natti8006@gmail.com>
2023-04-02 21:05:49 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,54 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: int | float, y: int | float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        sum_x = self.x + other.x
+        sum_y = self.y + other.y
+        return Vector(sum_x, sum_y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        sub_x = self.x - other.x
+        sub_y = self.y - other.y
+        return Vector(sub_x, sub_y)
+
+    def __mul__(self, other: int | float | Vector) -> Vector | int | float:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        x = round(self.x * other, 2)
+        y = round(self.y * other, 2)
+        return Vector(x, y)
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: int | float, end_point: int | float) -> Vector:
+        cls.x = end_point[0] - start_point[0]
+        cls.y = end_point[1] - start_point[1]
+        return Vector(cls.x, cls.y)
+
+    def get_length(self) -> int | float:
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def get_normalized(self) -> Vector:
+        return Vector(self.x / self.get_length(), self.y / self.get_length())
+
+    def angle_between(self, other: Vector) -> int:
+        dot_product = self.x * other.x + self.y * other.y
+        module = self.get_length() * other.get_length()
+        cos_a = dot_product / module
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> int:
+        cos_a = self.y / self.get_length()
+        return round(math.degrees(math.acos(cos_a)))
+
+    def rotate(self, degrees: int | float) -> Vector:
+        cos = math.cos(math.radians(degrees))
+        sin = math.sin(math.radians(degrees))
+        x = cos * self.x - sin * self.y
+        y = sin * self.x + cos * self.y
+        return Vector(x, y)
