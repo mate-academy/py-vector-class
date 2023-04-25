ec5545dce7d78d9f38b04c0fc1925c74de0fb521
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-04-25 15:08:42 +0300

    Fixed Py Vector Class

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
         return Vector(self.x + other.x, self.y + other.y)
@@ -21,10 +21,11 @@ class Vector:
     @classmethod
     def create_vector_by_two_points(cls,
                                     start_point: tuple[float, float],
-                                    end_point: tuple[float, float]) -> "Vector":
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - start_point[1]
-        return cls(x, y)
+                                    end_point: tuple[float, float]) \
+            -> "Vector":
+        x_coord = end_point[0] - start_point[0]
+        y_coord = end_point[1] - start_point[1]
+        return cls(x_coord, y_coord)
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
@@ -42,6 +43,6 @@ class Vector:
 
     def rotate(self, degrees: float) -> "Vector":
         radians = math.radians(degrees)
-        x = self.x * math.cos(radians) - self.y * math.sin(radians)
-        y = self.x * math.sin(radians) + self.y * math.cos(radians)
-        return Vector(x, y)
+        x_coord = self.x * math.cos(radians) - self.y * math.sin(radians)
+        y_coord = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(x_coord, y_coord)
