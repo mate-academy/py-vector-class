7194b1f4d5c357dbd0070f95c2c57cc8fc564cd0
Illya Zhmurskiy <thebrawler6@gmail.com>
2023-05-01 23:38:44 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,47 @@
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x, y):
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        else:
+            return Vector(self.x * other, self.y * other)
+
+    def get_length(self):
+        return round(math.sqrt(self.x ** 2 + self.y ** 2), 15)
+
+    def get_normalized(self):
+        length = self.get_length()
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other):
+        dot_product = self * other
+        cos_a = dot_product / (self.get_length() * other.get_length())
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self):
+        return self.angle_between(Vector(0, 1))
+
+    def rotate(self, degrees):
+        radians = math.radians(degrees)
+        x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(x, y)
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point, end_point):
+        x = end_point[0] - start_point[0]
+        y = end_point[1] - start_point[1]
+        return cls(x, y)
+
