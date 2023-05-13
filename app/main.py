bee9868b04f23b0e8cb79c321a0dea988538eb77
Volodymyr Bohomol <8773927@gmail.com>
2023-05-13 11:44:36 +0300

    implemented class Vector and all its methods

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, _x: float, _y: float) -> None:
+        self.x = round(_x, 2)
+        self.y = round(_y, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
