093e275937560207362b42f2eadfd48acee3bdb5
Valeriia Tymchenko <maksimova.tymchenko@gmail.com>
2023-05-18 22:02:10 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,62 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x,
+                      self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x,
+                      self.y - other.y)
+
+    def __mul__(self, other: int | Vector) -> Vector:
+        if isinstance(other, (int, float)):
+            return Vector(round(self.x * other, 2),
+                          round(self.y * other, 2))
+        else:
+            return self.x * other.x + self.y * other.y
+
+    @classmethod
+    def create_vector_by_two_points(cls,
+                                    start_point: tuple,
+                                    end_point: tuple) -> Vector:
+        x = end_point[0] - start_point[0]
+        y = end_point[1] - start_point[1]
+        return cls(x, y)
+
+    def get_length(self) -> float:
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def get_normalized(self) -> Vector:
+        return Vector(self.x / self.get_length(),
+                      self.y / self.get_length())
+
+    def angle_between(self, vector: Vector) -> int:
+        dot = self.x * vector.x + self.y * vector.y
+        cos_a = dot / (self.get_length() * vector.get_length())
+        degrees = math.degrees(math.acos(cos_a))
+        return round(degrees)
+
+    def get_angle(self) -> int:
+        vector_y = Vector(0, 1)
+        return self.angle_between(vector_y)
+
+    def rotate(self, degrees: int) -> Vector:
+        radians = math.radians(degrees)
+        cos_a = math.cos(radians)
+        sin_a = math.sin(radians)
+        x = self.x * cos_a - self.y * sin_a
+        y = self.x * sin_a + self.y * cos_a
+        return Vector(x, y)
+
+
+
+
+
+
