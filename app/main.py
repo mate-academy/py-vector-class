cf47f4d2770209e24e1261fe6e4bab7daff32efa
mark.shapka <mark.shapka1@gmail.com>
2023-05-29 21:41:01 +0300

    fix flake8

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ from math import sqrt, acos, degrees, ceil, atan, cos, sin, radians
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: float , y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(
