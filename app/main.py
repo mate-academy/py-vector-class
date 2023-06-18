c4ce9b7dcd5c2cb75dd9da9ac2b989c129ff6748
Mikhail <tiunovmixs@gmail.com>
2023-06-18 11:52:43 +0300

    Fixed flake8 class Vector

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ import math
 
 class Vector:
 
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, axis_x: float, axis_y: float) -> None:
+        self.x = round(axis_x, 2)
+        self.y = round(axis_y, 2)
 
     def __add__(self, other: float | Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -20,8 +20,15 @@ class Vector:
         return Vector(self.x * other, self.y * other)
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> Vector:
-        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+    def create_vector_by_two_points(
+            cls,
+            start_point: tuple,
+            end_point: tuple,
+    ) -> Vector:
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1],
+        )
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
@@ -31,13 +38,14 @@ class Vector:
         return Vector(self.x / length, self.y / length)
 
     def angle_between(self, other: Vector) -> int:
-        cos_a = (self.x * other.x + self.y * other.y) / (self.get_length() * other.get_length())
+        cos_a = (self.x * other.x + self.y * other.y) /\
+                (self.get_length() * other.get_length())
         return round(math.degrees(math.acos(cos_a)))
 
     def get_angle(self) -> int:
         return self.angle_between(Vector(0, abs(self.y)))
 
-    def rotate(self, degrees) -> Vector:
+    def rotate(self, degrees: int) -> Vector:
         radians = math.radians(degrees)
         rotate_x = self.x * math.cos(radians) - self.y * math.sin(radians)
         rotate_y = self.x * math.sin(radians) + self.y * math.cos(radians)
