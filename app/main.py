37a3d2b8c6e1c49e368ff18f4da2d56ac6fc9a6b
Ivan Zhaivoronok <ivan.zhaivoronok@ukr.net>
2023-06-26 12:12:06 +0300

    Create __sub__ in Vector class

--- app/main.py
+++ app/main.py
@@ -8,3 +8,6 @@ class Vector:
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
+    
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
