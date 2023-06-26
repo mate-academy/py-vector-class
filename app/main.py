1315c64bb4a1a8aac558a38cfc81532f67556610
Ivan Zhaivoronok <ivan.zhaivoronok@ukr.net>
2023-06-26 12:11:26 +0300

    Create __add__ in Vector class

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
+from __future__ import annotations
+
+
 class Vector:
     def __init__(self, x: float, y: float) -> None:
         self.x = round(x, 2)
         self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
