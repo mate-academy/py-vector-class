d783acf69e4a5ebef00c88004fb57b7b5802be48
Dmytro Beschastnyi <d.beschastnyi@icloud.com>
2023-06-30 00:23:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,52 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, point_x: int | float, point_y: int | float) -> None:
+        self.x = round(point_x, 2)
+        self.y = round(point_y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: Vector | int | float) -> Vector | float:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        else:
+            return Vector(round(self.x * other, 2), round(self.y * other, 2))
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls,
+            start_point: tuple,
+            end_point: tuple
+    ) -> Vector:
+        return Vector(end_point[0] - start_point[0],
+                      end_point[1] - start_point[1])
+
+    def get_length(self) -> float:
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def get_normalized(self) -> Vector:
+        return Vector(self.x / self.get_length(), self.y / self.get_length())
+
+    def angle_between(self, vector: Vector) -> int:
+        cos_a = (self * vector) / (self.get_length() * vector.get_length())
+        angle = round(math.degrees(math.acos(cos_a)))
+        return angle
+
+    def get_angle(self) -> int:
+        cos_a = self.y / self.get_length()
+        return round(math.degrees(math.acos(cos_a)))
+
+    def rotate(self, degrees: int) -> Vector:
+        x_point = math.cos(math.radians(degrees)) * self.x\
+                  - math.sin(math.radians(degrees)) * self.y
+        y_point = math.sin(math.radians(degrees)) * self.x\
+                  + math.cos(math.radians(degrees)) * self.y
+
+        return Vector(round(x_point, 2), round(y_point, 2))
