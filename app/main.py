4017540e0e269122f17359656ef2565e896d2d3f
Manynik <s.onufriyenko@gmail.com>
2023-07-09 15:53:15 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,53 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: float) -> Vector | float:
+        if isinstance(other, Vector):
+            return (self.x * other.x) + (self.y * other.y)
+        else:
+            return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: tuple,
+                                    end_point: tuple) -> Vector:
+        x_coord = end_point[0] - start_point[0]
+        y_coord = end_point[1] - start_point[1]
+        return cls(x_coord, y_coord)
+
+    def get_length(self) -> float:
+        length = math.sqrt(self.x ** 2 + self.y ** 2)
+        return length
+
+    def get_normalized(self) -> Vector:
+        return Vector(self.x / self.get_length(), self.y / self.get_length())
+
+    def angle_between(self, other: Vector) -> float:
+        product = self * other
+        cos_a = product / (self.get_length() * other.get_length())
+        degrees = math.degrees(math.acos(cos_a))
+        return round(degrees)
+
+    def get_angle(self) -> float:
+        ref_angle = Vector(0, 1)
+        angle = self.angle_between(ref_angle)
+        return angle
+
+    def rotate(self, degrees: int) -> Vector:
+        return Vector(
+            self.x * math.cos(math.radians(degrees))
+            - self.y * math.sin(math.radians(degrees)),
+            self.x * math.sin(math.radians(degrees))
+            + self.y * math.cos(math.radians(degrees))
+        )
