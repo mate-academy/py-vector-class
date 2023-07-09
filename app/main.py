e5c8eae14c8765ac32b3f82593d6ac6d8a6b86c1
makukh <makukh@gmail.com>
2023-07-09 23:10:40 +0300

    10 / 22 passed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from math import sqrt, acos, degrees, cos, sin, radians
+
+
 class Vector:
 
     def __init__(self, x: float, y: float) -> None:
@@ -11,8 +14,7 @@ class Vector:
         return Vector((self.x - other.x),(self.y - other.y))
 
     def __mul__(self, other: "Vector") -> "Vector":
-        return Vector(
-            (self.x * other.x) + (self.y * other.y))
+        return Vector(self.x * other.x + self.y * other.y)
 
     @classmethod
     def create_vector_by_two_points(cls, start_point: tuple[float], end_point: tuple[float]) -> "Vector":
@@ -20,16 +22,26 @@ class Vector:
         y = end_point[1] - start_point[1]
         return cls(round(x, 2), round(y, 2))
 
-    def get_length(self) -> "Vector":
-        pass
+    def get_length(self) -> float:
+        return sqrt(self.x ** 2 + self.y ** 2)
 
     def get_normalized(self) -> "Vector":
-        pass
+        length = self.get_length()
+        if length != 0:
+            return Vector(self.x / length, self.y / length)
+        return Vector(self.x, self.y)
 
-    def angle_between(self) -> int | float:
+    def angle_between(self, other: "Vector") -> float:
+        dot_product = (self.x * other.x) + (self.y * other.y)
+        magnitude_product = self.get_length() * other.get_length()
+        if magnitude_product != 0:
+            radians = acos(dot_product / magnitude_product)
+            return degrees(radians)
+        return 0.0
 
-        return degrees
-        pass
-
-    def rotate(degrees: int | float) -> "Vector":
-        pass
+    def rotate(self, degree: int) -> "Vector":
+        cos_angle = cos(radians(degree))
+        sin_angle = sin(radians(degree))
+        x = self.x * cos_angle - self.y * sin_angle
+        y = self.x * sin_angle + self.y * cos_angle
+        return Vector(round(x, 2), round(y, 2))
