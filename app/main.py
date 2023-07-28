23c3e129fd842388c5c39cfad955f1ed469b3e0a
Ruslan <rusipbox@gmail.com>
2023-07-29 01:01:31 +0300

    Test (py-vector-class) final

--- app/main.py
+++ app/main.py
@@ -1,65 +1,55 @@
 import math
-from typing import Tuple, Union
 
 
 class Vector:
-    def __init__(self, x_coord: float, y_coord: float) -> None:
-        self.x = round(x_coord, 2)
-        self.y = round(y_coord, 2)
+    def __init__(self, x: float, y: float):
+        self.x: float = round(x, 2)
+        self.y: float = round(y, 2)
 
-    def __add__(self, other: "Vector") -> "Vector":
-        new_x = self.x + other.x
-        new_y = self.y + other.y
-        return Vector(new_x, new_y)
+    def __add__(self, other: 'Vector') -> 'Vector':
+        return Vector(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other: "Vector") -> "Vector":
-        new_x = self.x - other.x
-        new_y = self.y - other.y
-        return Vector(new_x, new_y)
+    def __sub__(self, other: 'Vector') -> float or 'Vector':
+        return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other: Union[float, "Vector"]) -> Union[float, "Vector"]:
-        if isinstance(other, (int, float)):
-            new_x = round(self.x * other, 2)
-            new_y = round(self.y * other, 2)
-            return Vector(new_x, new_y)
-        elif isinstance(other, Vector):
+    def __mul__(self, other: 'Vector') -> float or 'Vector':
+        if isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
+        else:
+            return Vector(self.x * other, self.y * other)
 
     @classmethod
     def create_vector_by_two_points(cls,
-                                    start_point: Tuple[float, float],
-                                    end_point: Tuple[float, float]
-                                    ) -> "Vector":
-        x_coord = end_point[0] - start_point[0]
-        y_coord = end_point[1] - start_point[1]
-        return cls(x_coord, y_coord)
+                                    start_point: tuple,
+                                    end_point: tuple
+                                    ) -> 'Vector':
+        x = round(end_point[0] - start_point[0], 2)
+        y = round(end_point[1] - start_point[1], 2)
+        return cls(x, y)
 
     def get_length(self) -> float:
-        return (self.x ** 2 + self.y ** 2) ** 0.5
+        return math.sqrt(self.x ** 2 + self.y ** 2)
 
-    def get_normalized(self) -> "Vector":
+    def get_normalized(self) -> 'Vector':
         length = self.get_length()
         if length == 0:
             return Vector(0, 0)
         return Vector(self.x / length, self.y / length)
 
-    def angle_between(self, other: "Vector") -> int:
-        dot_product = self.x * other.x + self.y * other.y
-        cos_angle = dot_product / (self.get_length() * other.get_length())
-        angle_radians = math.acos(cos_angle)
-        angle_degrees = round(math.degrees(angle_radians))
-        return angle_degrees
+    def angle_between(self, other: 'Vector') -> int:
+        cos_a = (self * other) / (self.get_length() * other.get_length())
+        angle_radians = math.acos(cos_a)
+        angle_degrees = math.degrees(angle_radians)
+        return round(angle_degrees)
 
     def get_angle(self) -> int:
-        angle_rad = math.atan2(self.x, self.y)
-        angle_deg = math.degrees(angle_rad)
-        angle_int = int(round(angle_deg))
-        return angle_int
+        reference_vector = Vector(0, 1)
+        return self.angle_between(reference_vector)
 
-    def rotate(self, degrees: int) -> "Vector":
+    def rotate(self, degrees: int) -> float or 'Vector':
         angle_radians = math.radians(degrees)
-        cos_theta = math.cos(angle_radians)
-        sin_theta = math.sin(angle_radians)
-        new_x = cos_theta * self.x - sin_theta * self.y
-        new_y = sin_theta * self.x + cos_theta * self.y
-        return Vector(new_x, new_y)
+        cos_a = math.cos(angle_radians)
+        sin_a = math.sin(angle_radians)
+        x = self.x * cos_a - self.y * sin_a
+        y = self.x * sin_a + self.y * cos_a
+        return Vector(round(x, 2), round(y, 2))
