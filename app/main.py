4bf66476f7e778755ed9075a19ceaff000d53933
Ruslan <rusipbox@gmail.com>
2023-07-29 00:44:40 +0300

    Test (py-vector-class)2

--- app/main.py
+++ app/main.py
@@ -3,25 +3,25 @@ from typing import Tuple, Union
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
-        add_x = self.x + other.x
-        add_y = self.y + other.y
-        return Vector(add_x, add_y)
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+        return Vector(new_x, new_y)
 
     def __sub__(self, other: "Vector") -> "Vector":
-        sub_x = self.x - other.x
-        sub_y = self.y - other.y
-        return Vector(sub_x, sub_y)
+        new_x = self.x - other.x
+        new_y = self.y - other.y
+        return Vector(new_x, new_y)
 
     def __mul__(self, other: Union[float, "Vector"]) -> Union[float, "Vector"]:
         if isinstance(other, (int, float)):
-            mul_x = round(self.x * other, 2)
-            mul_y = round(self.y * other, 2)
-            return Vector(mul_x, mul_y)
+            new_x = round(self.x * other, 2)
+            new_y = round(self.y * other, 2)
+            return Vector(new_x, new_y)
         elif isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
 
@@ -29,9 +29,9 @@ class Vector:
     def create_vector_by_two_points(cls,
                                     start_point: Tuple[float, float],
                                     end_point: Tuple[float, float]) -> "Vector":
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - start_point[1]
-        return cls(x, y)
+        x_coord = end_point[0] - start_point[0]
+        y_coord = end_point[1] - start_point[1]
+        return cls(x_coord, y_coord)
 
     def get_length(self) -> float:
         return (self.x ** 2 + self.y ** 2) ** 0.5
@@ -45,7 +45,7 @@ class Vector:
     def angle_between(self, other: "Vector") -> int:
         dot_product = self.x * other.x + self.y * other.y
         cos_angle = dot_product / (self.get_length() * other.get_length())
-        angle_radians = math.acos(cos_angle)    # we use it to get the inverse function for the cosine
+        angle_radians = math.acos(cos_angle)
         angle_degrees = round(math.degrees(angle_radians))
         return angle_degrees
 
@@ -59,6 +59,6 @@ class Vector:
         angle_radians = math.radians(degrees)
         cos_theta = math.cos(angle_radians)
         sin_theta = math.sin(angle_radians)
-        f_new_x = cos_theta * self.x - sin_theta * self.y
-        f_new_y = sin_theta * self.x + cos_theta * self.y
-        return Vector(f_new_x, f_new_y)
+        new_x = cos_theta * self.x - sin_theta * self.y
+        new_y = sin_theta * self.x + cos_theta * self.y
+        return Vector(new_x, new_y)
