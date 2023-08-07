6ead37fbb8b5afaf8c68ae3f20946a62ec72b1ff
Kateryna Zadorozhna <136272476+katryana@users.noreply.github.com>
2023-08-07 23:32:36 +0300

    Update main.py final version

--- app/main.py
+++ app/main.py
@@ -21,8 +21,10 @@ class Vector:
         return Vector(self.x * other, self.y * other)
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> Any:
-        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+    def create_vector_by_two_points(cls, start_point: tuple,
+                                    end_point: tuple) -> Any:
+        return cls(end_point[0] - start_point[0],
+                   end_point[1] - start_point[1])
 
     def get_length(self) -> float:
         return (self.x ** 2 + self.y ** 2) ** 0.5
@@ -38,7 +40,8 @@ class Vector:
 
     def get_angle(self) -> int:
         other = Vector(0, 1)
-        cos_angle = self.__mul__(other) / (self.get_length() * other.get_length())
+        cos_angle = self.__mul__(other)
+        cos_angle /= (self.get_length() * other.get_length())
         angle = math.degrees(math.acos(cos_angle))
         return round(angle)
 
@@ -46,4 +49,6 @@ class Vector:
         radians = math.radians(degrees)
         cos_a = math.cos(radians)
         sin_a = math.sin(radians)
-        return Vector(self.x * cos_a - self.y * sin_a, self.x * sin_a + self.y * cos_a)
+        return Vector(self.x * cos_a - self.y * sin_a,
+                      self.x * sin_a + self.y * cos_a)
+
