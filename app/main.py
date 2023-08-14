ea54dfab56bd869a57e07d528cbf2b619290d56c
Daniil Hinzburh <2ilgan1601@gmail.com>
2023-08-14 17:59:14 +0300

    task completed

--- app/main.py
+++ app/main.py
@@ -6,10 +6,10 @@ class Vector:
         self.x = round(x_coordinate, 2)
         self.y = round(y_coordinate, 2)
 
-    def __add__(self, other: "Vector") -> "Vector":
+    def __add__(self, other: object) -> object:
         return Vector(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other: "Vector") -> "Vector":
+    def __sub__(self, other: object) -> object:
         return Vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self, other: object) -> object | float:
@@ -30,18 +30,18 @@ class Vector:
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
 
-    def get_normalized(self) -> "Vector":
+    def get_normalized(self) -> object:
         length = self.get_length()
         return Vector(self.x / length, self.y / length)
 
-    def angle_between(self, other: "Vector") -> int:
+    def angle_between(self, other: object) -> int:
         cos_angle = (self * other) / (self.get_length() * other.get_length())
         return round(math.degrees(math.acos(cos_angle)))
 
     def get_angle(self) -> int:
         return self.angle_between(Vector(0, 1))
 
-    def rotate(self, degrees: int) -> "Vector":
+    def rotate(self, degrees: int) -> object:
         radians = math.radians(degrees)
         new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
         new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
