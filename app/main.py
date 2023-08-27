7230aad9f7e2fba790ed57e93c9688e5e18d20d2
Serhii Budko <sergeybudko.kr@gmail.com>
2023-08-27 17:10:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,51 @@
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float):
+        self.x: float = round(x, 2)
+        self.y: float = round(y, 2)
+
+    def __add__(self, other: 'Vector') -> 'Vector':
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: 'Vector') -> 'Vector':
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: "Vector") -> float:
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> 'Vector':
+        x = end_point[0] - start_point[0]
+        y = end_point[1] - start_point[1]
+        return cls(x, y)
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> 'Vector':
+        length = self.get_length()
+        if length != 0:
+            return Vector(self.x / length, self.y / length)
+        return Vector(0, 0)
+
+    def angle_between(self, other: 'Vector') -> int:
+        dot_product = self * other
+        cos_angle = dot_product / (self.get_length() * other.get_length())
+        angle_in_radians = math.acos(cos_angle)
+        angle_in_degrees = math.degrees(angle_in_radians)
+        return round(angle_in_degrees)
+
+    def get_angle(self) -> int:
+        reference_vector = Vector(0, 1)  # Positive Y axis
+        return self.angle_between(reference_vector)
+
+    def rotate(self, degrees: int) -> 'Vector':
+        radians = math.radians(degrees)
+        new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(new_x, new_y)
