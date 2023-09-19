a7a3c85b320792a4729b50e366e7bf55d816967f
Alex <oleksa.shcherbakov@gmail.com>
2023-09-19 13:47:16 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -2,41 +2,48 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float | int, y: float | int) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coor: float | int, y_coor: float | int) -> None:
+        self.x_coor = round(x_coor, 2)
+        self.y_coor = round(y_coor, 2)
 
-    def __add__(self, vector) -> object:
+    def __add__(self, vector: object) -> object:
         return Vector(
-            self.x + vector.x,
-            self.y + vector.y
+            self.x_coor + vector.x_coor,
+            self.y_coor + vector.y_coor
         )
 
-    def __sub__(self, vector) -> object:
+    def __sub__(self, vector: object) -> object:
         return Vector(
-            self.x - vector.x,
-            self.y - vector.y
+            self.x_coor - vector.x_coor,
+            self.y_coor - vector.y_coor
         )
 
     def __mul__(self, multiplier: object | float) -> object | float:
         if isinstance(multiplier, Vector):
-            return self.x * multiplier.x + self.y * multiplier.y
+            return self.x_coor * multiplier.x_coor + self.y_coor * multiplier.y_coor
         return Vector(
-            self.x * multiplier,
-            self.y * multiplier
+            self.x_coor * multiplier,
+            self.y_coor * multiplier
         )
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> object:
-        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+    def create_vector_by_coor_two_points(
+            cls,
+            start_point: tuple,
+            end_point: tuple
+    ) -> object:
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
 
     def get_length(self) -> float | int:
-        return (self.x ** 2 + self.y ** 2) ** 0.5
+        return (self.x_coor ** 2 + self.y_coor ** 2) ** 0.5
 
     def get_normalized(self) -> object:
         return Vector(
-            self.x * (1 / Vector.get_length(self)),
-            self.y * (1 / Vector.get_length(self))
+            self.x_coor * (1 / Vector.get_length(self)),
+            self.y_coor * (1 / Vector.get_length(self))
         )
 
     def angle_between(self, other: object) -> int:
@@ -51,6 +58,6 @@ class Vector:
         cos = math.cos(radians)
         sin = math.sin(radians)
         return Vector(
-            self.x * cos - self.y * sin,
-            self.x * sin + self.y * cos
+            self.x_coor * cos - self.y_coor * sin,
+            self.x_coor * sin + self.y_coor * cos
         )
