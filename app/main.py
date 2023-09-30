6781ab98f1b74f9722d92703dbc8c7e858858a59
naiv <nazarivankiv1@gmail.com>
2023-09-30 20:02:12 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import math
 
 class Vector:
 
-    def __init__(self, x: int, y: int) -> None:
+    def __init__(self, x: float, y: float) -> None:
         self.x = round(x, 2)
         self.y = round(y, 2)
 
