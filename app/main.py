52e0024322ed29af41f8d682d8f2a6df4c5b6509
Danylo Sheremeta <danylo3002@gmail.com>
2023-10-07 17:02:58 +0300

    fixed flake8

--- app/main.py
+++ app/main.py
@@ -3,38 +3,38 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_cord: float, y_cord: float) -> None:
+        self.x = round(x_cord, 2)
+        self.y = round(y_cord, 2)
 
     def __add__(self, other: Vector) -> Vector:
-        return Vector(x=self.x + other.x,
-                      y=self.y + other.y)
+        return Vector(x_cord=self.x + other.x,
+                      y_cord=self.y + other.y)
 
     def __sub__(self, other: Vector) -> Vector:
-        return Vector(x=self.x - other.x,
-                      y=self.y - other.y)
+        return Vector(x_cord=self.x - other.x,
+                      y_cord=self.y - other.y)
 
     def __mul__(self, other: float | int | Vector) -> Vector | float:
         if isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
-        return Vector(x=self.x * other,
-                      y=self.y * other)
+        return Vector(x_cord=self.x * other,
+                      y_cord=self.y * other)
 
     @classmethod
     def create_vector_by_two_points(cls, start_point: tuple,
                                     end_point: tuple) -> Vector:
         return cls(
-            x=end_point[0] - start_point[0],
-            y=end_point[1] - start_point[1]
+            x_cord=end_point[0] - start_point[0],
+            y_cord=end_point[1] - start_point[1]
         )
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
 
     def get_normalized(self) -> Vector:
-        return Vector(x=self.x / self.get_length(),
-                      y=self.y / self.get_length())
+        return Vector(x_cord=self.x / self.get_length(),
+                      y_cord=self.y / self.get_length())
 
     def angle_between(self, other: Vector) -> int:
         dot_product = Vector.__mul__(self, other)
@@ -48,4 +48,4 @@ class Vector:
         rads = math.radians(degrees)
         x_new = self.x * math.cos(rads) - self.y * math.sin(rads)
         y_new = self.x * math.sin(rads) + self.y * math.cos(rads)
-        return Vector(x=x_new, y=y_new)
+        return Vector(x_cord=x_new, y_cord=y_new)
