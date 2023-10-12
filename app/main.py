742f3da1314de1f22149b1cbe904d4b0f14f1efd
Max Muhanuk <ytmaksik425@gmail.com>
2023-10-12 16:03:08 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,43 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: int | float, y: int | float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: Vector | int | float) -> Vector | int | float:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        return Vector(self.x * other, self.y * other)
+
+    @staticmethod
+    def create_vector_by_two_points(start_point: tuple[int | float], end_point: tuple[int | float]) -> Vector:
+        return Vector(end_point[0] - start_point[0], end_point[1] - start_point[1])
+
+    def get_length(self) -> int | float:
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def get_normalized(self) -> Vector:
+        length = self.get_length()
+        return Vector(round(self.x / length, 2), round(self.y / length, 2))
+
+    def angle_between(self, other: Vector) -> int | float:
+        vec = Vector(self.x * other.x, self.y * other.y)
+        return round(math.degrees(math.acos((vec.x + vec.y) / (self.get_length() * other.get_length()))))
+
+    def get_angle(self) -> int | float:
+        return self.angle_between(Vector(0, 1))
+
+    def rotate(self, degrees: int | float) -> Vector:
+        deg_to_rad = math.radians(degrees)
+        x_prime = self.x * math.cos(deg_to_rad) - self.y * math.sin(deg_to_rad)
+        y_prime = self.x * math.sin(deg_to_rad) + self.y * math.cos(deg_to_rad)
+        return Vector(round(x_prime, 2), round(y_prime, 2))
