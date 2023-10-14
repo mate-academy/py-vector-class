22919709a2cd1770386e3c8a41a6f726cd09060c
Yuliia Dudych <juliadudych@gmail.com>
2023-10-14 15:03:12 +0300

    Change layout to fix flake8 errors

--- app/main.py
+++ app/main.py
@@ -3,21 +3,33 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_point: float, y_point: float) -> None:
+        self.x_point = round(x_point, 2)
+        self.y_point = round(y_point, 2)
 
     def __add__(self, other: Vector) -> Vector:
-        return Vector(self.x + other.x, self.y + other.y)
+        return Vector(
+            self.x_point + other.x_point,
+            self.y_point + other.y_point
+        )
 
     def __sub__(self, other: Vector) -> Vector:
-        return Vector(self.x - other.x, self.y - other.y)
+        return Vector(
+            self.x_point - other.x_point,
+            self.y_point - other.y_point
+        )
 
     def __mul__(self, other: Vector | float | int) -> Vector | float:
         if type(other) == float or type(other) == int:
-            return Vector(self.x * other, self.y * other)
+            return Vector(
+                self.x_point * other,
+                self.y_point * other
+            )
         else:
-            return self.x * other.x + self.y * other.y
+            return (
+                self.x_point * other.x_point
+                + self.y_point * other.y_point
+            )
 
     @classmethod
     def create_vector_by_two_points(
@@ -32,10 +44,13 @@ class Vector:
         )
 
     def get_length(self) -> float:
-        return math.sqrt(self.x ** 2 + self.y ** 2)
+        return math.sqrt(self.x_point ** 2 + self.y_point ** 2)
 
     def get_normalized(self) -> Vector:
-        return Vector(self.x / self.get_length(), self.y / self.get_length())
+        return Vector(
+            self.x_point / self.get_length(),
+            self.y_point / self.get_length()
+        )
 
     def angle_between(self, other: Vector) -> int:
         return round(
@@ -47,13 +62,13 @@ class Vector:
         )
 
     def get_angle(self) -> int:
-        angle = math.degrees(math.atan2(self.x, self.y))
+        angle = math.degrees(math.atan2(self.x_point, self.y_point))
         return abs(round(angle))
 
     def rotate(self, degrees: int) -> Vector:
         rad = math.radians(degrees)
         cos_rad = math.cos(rad)
         sin_rad = math.sin(rad)
-        x = self.x * cos_rad - self.y * sin_rad
-        y = self.x * sin_rad + self.y * cos_rad
-        return Vector(x, y)
+        x_point = self.x_point * cos_rad - self.y_point * sin_rad
+        y_point = self.x_point * sin_rad + self.y_point * cos_rad
+        return Vector(x_point, y_point)
