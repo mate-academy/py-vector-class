b3ebaae61d51578a11a2d4ed2e888e32f20ba8bb
Yuliia Dudych <juliadudych@gmail.com>
2023-10-14 14:43:20 +0300

    Define class method for Vector creation from given points

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import math
 
 
 class Vector:
@@ -17,3 +18,15 @@ class Vector:
             return Vector(self.x * other, self.y * other)
         else:
             return self.x * other.x + self.y * other.y
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls,
+            start_point: tuple[float],
+            end_point: tuple[float]
+    ) -> Vector:
+
+        return Vector(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
