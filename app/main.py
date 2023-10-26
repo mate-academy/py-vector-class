3c5e7dd0579c045bf7aae7bba8a6e48c9ad2fbd5
Vitalii Taran <vitaliy.taran.1993@gmail.com>
2023-10-26 20:25:18 +0300

    Solution_2

--- app/main.py
+++ app/main.py
@@ -5,30 +5,30 @@ import math
 class Vector:
 
     def __init__(self, coord_x: float, coord_y: float) -> None:
-        self.coord_x = round(coord_x, 2)
-        self.coord_y = round(coord_y, 2)
+        self.x = round(coord_x, 2)
+        self.y = round(coord_y, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(
-            self.coord_x + other.coord_x,
-            self.coord_y + other.coord_y
+            self.x + other.x,
+            self.y + other.y
         )
 
     def __sub__(self, other: Vector) -> Vector:
         return Vector(
-            self.coord_x - other.coord_x,
-            self.coord_y - other.coord_y
+            self.x - other.x,
+            self.y - other.y
         )
 
     @staticmethod
     def dot_product(vec_1: Vector, vec_2: Vector) -> float:
-        return vec_1.coord_x * vec_2.coord_x + vec_1.coord_y * vec_2.coord_y
+        return vec_1.x * vec_2.x + vec_1.y * vec_2.y
 
     def __mul__(self, value: float | int | Vector) -> float | int | Vector:
         if isinstance(value, Vector):
             return self.dot_product(self, value)
 
-        return Vector(self.coord_x * value, self.coord_y * value)
+        return Vector(self.x * value, self.y * value)
 
     @classmethod
     def create_vector_by_two_points(
@@ -42,14 +42,14 @@ class Vector:
 
     @staticmethod
     def def_len(vec: Vector) -> float:
-        return (vec.coord_x ** 2 + vec.coord_y ** 2) ** 0.5
+        return (vec.x ** 2 + vec.y ** 2) ** 0.5
 
     def get_length(self) -> float:
         return self.def_len(self)
 
     def get_normalized(self) -> Vector:
         len_ = self.def_len(self)
-        return Vector(self.coord_x / len_, self.coord_y / len_)
+        return Vector(self.x / len_, self.y / len_)
 
     @classmethod
     def def_angle(cls, vec_1: Vector, vec_2: Vector) -> int:
@@ -63,12 +63,12 @@ class Vector:
         return self.def_angle(self, other)
 
     def get_angle(self) -> int:
-        other = Vector(0, int(math.fabs(self.coord_y)))
+        other = Vector(0, int(math.fabs(self.y)))
         return self.def_angle(self, other)
 
     def rotate(self, angel: int) -> Vector:
         ang = math.radians(angel)
         return Vector(
-            self.coord_x * math.cos(ang) - self.coord_y * math.sin(ang),
-            self.coord_x * math.sin(ang) + self.coord_y * math.cos(ang)
+            self.x * math.cos(ang) - self.y * math.sin(ang),
+            self.x * math.sin(ang) + self.y * math.cos(ang)
         )
