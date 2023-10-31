e9b6554a695968b2536544ed88fd6c5d022a92f7
Bohdan Tsapenko <earldoomy@gmail.com>
2023-10-31 18:47:30 +0400

    Amended 2

--- app/main.py
+++ app/main.py
@@ -1,49 +1,51 @@
 import math
+from typing import Tuple
 
 
 class Vector:
     def __init__(self, xaxis: float, yaxis: float) -> None:
-        self.xaxis = round(xaxis, 2)
-        self.yaxis = round(yaxis, 2)
+        self.x = round(xaxis, 2)
+        self.y = round(yaxis, 2)
 
-    def __add__(self, other: float) -> None:
-        return Vector(self.xaxis + other.xaxis, self.yaxis + other.yaxis)
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other: float) -> None:
-        return Vector(self.xaxis - other.xaxis, self.yaxis - other.yaxis)
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other: float) -> None:
+    def __mul__(self, other: "Vector" or float) -> "Vector" or float:
         if isinstance(other, (int, float)):
-            return Vector(self.xaxis * other, self.yaxis * other)
+            return Vector(self.x * other, self.y * other)
         elif isinstance(other, Vector):
-            return self.xaxis * other.xaxis + self.yaxis * other.yaxis
+            return self.x * other.x + self.y * other.y
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: float, end_point: float)\
-            -> None:
-        xaxis = end_point[0] - start_point[0]
-        yaxis = end_point[1] - start_point[1]
-        return cls(xaxis, yaxis)
+    def create_vector_by_two_points(cls, start_point: Tuple[float, float],
+                                    end_point: Tuple[float, float]) \
+            -> "Vector":
+        xaxis1 = end_point[0] - start_point[0]
+        yaxis1 = end_point[1] - start_point[1]
+        return cls(xaxis1, yaxis1)
 
     def get_length(self) -> float:
-        return math.sqrt(self.xaxis ** 2 + self.yaxis ** 2)
+        return math.sqrt(self.x ** 2 + self.y ** 2)
 
     def get_normalized(self) -> "Vector":
         length = self.get_length()
         if length == 0:
             return Vector(0, 0)
-        return Vector(self.xaxis / length, self.yaxis / length)
+        return Vector(self.x / length, self.y / length)
 
     def angle_between(self, other: "Vector") -> int:
         cos_a = (self * other) / (self.get_length() * other.get_length())
         return round(math.degrees(math.acos(cos_a)))
 
     def get_angle(self) -> int:
-        cos_a = self.yaxis / self.get_length()
+        cos_a = self.y / self.get_length()
         return round(math.degrees(math.acos(cos_a)))
 
-    def rotate(self, degrees: int) -> "Vector":
+    def rotate(self, degrees: float) -> "Vector":
         radians = math.radians(degrees)
-        new_x = self.xaxis * math.cos(radians) - self.yaxis * math.sin(radians)
-        new_y = self.xaxis * math.sin(radians) + self.yaxis * math.cos(radians)
+        new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
         return Vector(new_x, new_y)
