0efbcaa6e98d054f9f04ac57287a226baaad7889
V_core <bitard1990vn@gmail.com>
2023-11-14 22:53:36 +0200

    flake8 fixed

--- app/main.py
+++ app/main.py
@@ -3,27 +3,27 @@ from math import sqrt, cos, sin, acos, degrees, atan2, radians
 
 
 class Vector:
-    def __init__(self, x: int | float, y: int | float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_cord: int | float, y_cord: int | float) -> None:
+        self.x_cord = round(x_cord, 2)
+        self.y_cord = round(y_cord, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(
-            (self.x + other.x),
-            (self.y + other.y)
+            (self.x_cord + other.x),
+            (self.y_cord + other.y)
         )
 
     def __sub__(self, other: Vector) -> Vector:
         return Vector(
-            (self.x - other.x),
-            (self.y - other.y)
+            (self.x_cord - other.x),
+            (self.y_cord - other.y)
         )
 
     def __mul__(self, other: Vector) -> Vector:
         if isinstance(other, int) or isinstance(other, float):
-            return Vector(self.x * other, self.y * other)
+            return Vector(self.x_cord * other, self.y_cord * other)
         else:
-            return self.x * other.x + self.y * other.y
+            return self.x_cord * other.x + self.y_cord * other.y
 
     @classmethod
     def create_vector_by_two_points(
@@ -37,16 +37,16 @@ class Vector:
         )
 
     def get_length(self) -> int | float:
-        return sqrt(self.x ** 2 + self.y ** 2)
+        return sqrt(self.x_cord ** 2 + self.y_cord ** 2)
 
     def get_normalized(self) -> Vector:
         return Vector(
-            (self.x / self.get_length()),
-            (self.y / self.get_length())
+            (self.x_cord / self.get_length()),
+            (self.y_cord / self.get_length())
         )
 
     def angle_between(self, other: Vector) -> float:
-        dot_prod = (self.x * other.x) + (self.y * other.y)
+        dot_prod = (self.x_cord * other.x) + (self.y_cord * other.y)
         scalar = self.get_length() * other.get_length()
         angle_radians = acos(dot_prod / scalar)
         return round(degrees(angle_radians))
@@ -55,14 +55,14 @@ class Vector:
         return abs(
             round(
                 degrees(
-                    atan2(self.x, self.y)
+                    atan2(self.x_cord, self.y_cord)
                 )
             )
         )
 
     def rotate(self, degr: int | float) -> Vector:
         theta = radians(degr)
-        x = self.x * cos(theta) - self.y * sin(theta)
-        y = self.x * sin(theta) + self.y * cos(theta)
+        x_cord = self.x_cord * cos(theta) - self.y_cord * sin(theta)
+        y_cord = self.x_cord * sin(theta) + self.y_cord * cos(theta)
 
-        return Vector(x, y)
+        return Vector(x_cord, y_cord)
