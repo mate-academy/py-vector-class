4d81edced4de87209802eb33a4da7c3e11aa3259
Mikola Rozhansky <bymikola@gmail.com>
2023-11-21 23:21:02 +0200

    1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,40 @@
+import math
+
 class Vector:
-    # write your code here
-    pass
+
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other):
+        return Vector(
+            x=self.x + other.x,
+            y=self.y + other.y
+        )
+
+    def __sub__(self, other):
+        return Vector(
+            x=self.x - other.x,
+            y=self.y - other.y
+        )
+
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+
+        return Vector(
+            x=self.x * other,
+            y=self.y * other
+        )
+
+    def get_length(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def
+
+
+
+
+
+
+
