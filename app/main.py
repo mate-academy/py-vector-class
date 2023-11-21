9f1887e0c3e3176bb81df603a80f7058f416e7f1
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2023-11-21 12:04:04 +0100

    solve py-vector-class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,64 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
     # write your code here
-    pass
+    def __init__(self, x: int | float, y: int | float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(x=(self.x + other.x), y=(self.y + other.y))
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(x=(self.x - other.x), y=(self.y - other.y))
+
+    def __mul__(self, other: int | float | Vector) -> Vector | int | float:
+        if isinstance(other, (int, float)):
+            return Vector(
+                x=round((self.x * other), 2),
+                y=round((self.y * other), 2)
+            )
+        if isinstance(other, Vector):
+            return (self.x * other.x) + (self.y * other.y)
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls,
+            start_point: tuple,
+            end_point: tuple
+    ) -> Vector:
+        return cls(
+            x=(end_point[0] - start_point[0]),
+            y=(end_point[1] - start_point[1])
+        )
+
+    def get_length(self) -> float:
+        return math.sqrt(
+            (math.pow(abs(self.x), 2)) + (math.pow(abs(self.y), 2))
+        )
+
+    def get_normalized(self) -> Vector:
+        length = self.get_length()
+        return Vector(x=self.x / length, y=self.y / length)
+
+    def angle_between(self, other: Vector) -> int:
+        dot_product = (self.x * other.x) + (self.y * other.y)
+        magnitude_of_vectors_product = \
+            math.sqrt((math.pow(self.x, 2)) + (math.pow(self.y, 2))) \
+            * math.sqrt((math.pow(other.x, 2)) + (math.pow(other.y, 2)))
+        cosine_of_angle = dot_product / magnitude_of_vectors_product
+        return round(math.degrees(math.acos(cosine_of_angle)))
+
+    def get_angle(self) -> int:
+        result = (self.y / math.sqrt((math.pow(self.x, 2))
+                                     + (math.pow(self.y, 2))))
+        return round(math.degrees(math.acos(result)))
+
+    def rotate(self, degrees: int) -> Vector:
+        degrees = math.radians(degrees)
+        return Vector(
+            x=(self.x * math.cos(degrees) - self.y * math.sin(degrees)),
+            y=(self.x * math.sin(degrees) + self.y * math.cos(degrees))
+        )
