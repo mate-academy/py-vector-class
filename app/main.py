04fc818da796f29d1a1fd57da829665749103c4a
Mikola Rozhansky <bymikola@gmail.com>
2023-11-22 00:18:11 +0200

    5

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Vector:
 
     def __init__(self, x: float, y: float) -> None:
@@ -30,7 +31,44 @@ class Vector:
     def get_length(self):
         return (self.x ** 2 + self.y ** 2) ** 0.5
 
-    def
+    def get_normalized(self):
+        # a = 1 / self.get_length()
+        return Vector(
+            x=self.x / self.get_length(),
+            y=self.y / self.get_length()
+        )
+
+    def angle_between(self, other):
+        return round(math.degrees(math.acos((self * other) / (self.get_length() * other.get_length()))), 0)
+
+    def get_angle(self):
+        return self.angle_between(Vector(0, 1))
+
+    def rotate(self, angle: float):
+        return Vector(
+            x=math.cos(math.radians(angle)) * self.x - math.sin(math.radians(angle)) * self.y,
+            y=math.sin(math.radians(angle)) * self.x + math.cos(math.radians(angle)) * self.y,
+        )
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple):
+        return cls(list(end_point)[0] - list(start_point)[0], list(end_point)[1] - list(start_point)[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
