db17a7dd053bc4a0ef1c464a522fc0a692ec988c
Dmytro Semenets <dmitriymine@gmail.com>
2023-12-05 20:09:47 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,34 +3,34 @@ import math
 
 
 class Vector:
-    def __init__(self, xflake8: float, yflake8: float) -> None:
-        self.xflake8 = round(xflake8, 2)
-        self.yflake8 = round(yflake8, 2)
+    def __init__(self, xxx: float, yyy: float) -> None:
+        self.xxx = round(xxx, 2)
+        self.yyy = round(yyy, 2)
 
     @classmethod
     def create_vector_by_two_points(cls, start: any, end: any) -> Vector:
-        xflake8 = end[0] - start[0]
-        yflake8 = end[1] - start[1]
-        return cls(xflake8, yflake8)
+        xxx = end[0] - start[0]
+        yyy = end[1] - start[1]
+        return cls(xxx, yyy)
 
     def __add__(self, other: any) -> Vector:
-        return Vector(self.xflake8 + other.xflake8, self.yflake8 + other.yflake8)
+        return Vector(self.xxx + other.xxx, self.yyy + other.yflake8)
 
     def __sub__(self, other: any) -> Vector:
-        return Vector(self.xflake8 - other.xflake8, self.yflake8 - other.yflake8)
+        return Vector(self.xxx - other.xxx, self.yyy - other.yflake8)
 
     def __mul__(self, other: any) -> Vector | float:
         if isinstance(other, (int, float)):
-            return Vector(self.xflake8 * other, self.yflake8 * other)
+            return Vector(self.xxx * other, self.yyy * other)
         elif isinstance(other, Vector):
-            return self.xflake8 * other.xflake8 + self.yflake8 * other.yflake8
+            return self.xxx * other.xxx + self.yyy * other.yyy
 
     def get_length(self) -> any:
-        return math.sqrt(self.xflake8 ** 2 + self.yflake8 ** 2)
+        return math.sqrt(self.xxx ** 2 + self.yyy ** 2)
 
     def get_normalized(self) -> Vector:
         length = self.get_length()
-        return Vector(self.xflake8 / length, self.yflake8 / length)
+        return Vector(self.xxx / length, self.yyy / length)
 
     def angle_between(self, other: any) -> any:
         cos_angle = (self * other) / (self.get_length() * other.get_length())
@@ -41,6 +41,6 @@ class Vector:
 
     def rotate(self, degrees: float) -> Vector:
         radians = math.radians(degrees)
-        new_x = self.xflake8 * math.cos(radians) - self.yflake8 * math.sin(radians)
-        new_y = self.xflake8 * math.sin(radians) + self.yflake8 * math.cos(radians)
+        new_x = self.xxx * math.cos(radians) - self.yyy * math.sin(radians)
+        new_y = self.xxx * math.sin(radians) + self.yyy * math.cos(radians)
         return Vector(new_x, new_y)
