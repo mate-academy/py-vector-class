98719507461c0aeb18926901a11d6a9dd6aa4cc2
Andrii Zubchenko <dissom1987@gmail.com>
2023-12-12 11:24:35 +0200

    Changed

--- app/main.py
+++ app/main.py
@@ -3,31 +3,31 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, coordinate_x: float, coordinate_y: float) -> None:
+        self.x = round(coordinate_x, 2)
+        self.y = round(coordinate_y, 2)
 
     def __add__(self, other: int | float | Vector) -> Vector:
         if isinstance(other, Vector):
-            x = self.x + other.x
-            y = self.y + other.y
-            return Vector(x, y)
+            coordinate_x = self.x + other.x
+            coordinate_y = self.y + other.y
+            return Vector(coordinate_x, coordinate_y)
 
     def __sub__(self, other: int | float | Vector) -> Vector:
         if isinstance(other, Vector):
-            x = self.x - other.x
-            y = self.y - other.y
-            return Vector(x, y)
+            coordinate_x = self.x - other.x
+            coordinate_y = self.y - other.y
+            return Vector(coordinate_x, coordinate_y)
 
     def __mul__(self, other: int | float | Vector) -> Vector:
         if isinstance(other, int | float):
-            x = self.x * other
-            y = self.y * other
-            return Vector(x, y)
+            coordinate_x = self.x * other
+            coordinate_y = self.y * other
+            return Vector(coordinate_x, coordinate_y)
         elif isinstance(other, Vector):
-            x = self.x * other.x
-            y = self.y * other.y
-            dot_product = x + y
+            coordinate_x = self.x * other.x
+            coordinate_y = self.y * other.y
+            dot_product = coordinate_x + coordinate_y
             return dot_product
 
     @classmethod
@@ -52,17 +52,17 @@ class Vector:
     def get_normalized(self) -> Vector:
         length = self.get_length()
         if length != 0:
-            x = self.x / length
-            y = self.y / length
-            return Vector(x, y)
+            coordinate_x = self.x / length
+            coordinate_y = self.y / length
+            return Vector(coordinate_x, coordinate_y)
 
     def angle_between(self, other: Vector) -> int | float:
-        scalar_product = self * other
+        scalar = self * other
         length_self = self.get_length()
         length_other = other.get_length()
 
         if length_self != 0 and length_other != 0:
-            cosine = scalar_product / (length_self * length_other)
+            cosine = scalar / (length_self * length_other)
             degrees = math.degrees(math.acos(cosine))
             return round(degrees)
 
@@ -73,10 +73,10 @@ class Vector:
 
     def rotate(self, degrees: int | float) -> Vector:
         radians = math.radians(degrees)
-        x = round(
+        coordinate_x = round(
             self.x * math.cos(
                 radians) - self.y * math.sin(radians), 2)
-        y = round(
+        coordinate_y = round(
             self.x * math.sin(
                 radians) + self.y * math.cos(radians), 2)
-        return Vector(x, y)
+        return Vector(coordinate_x, coordinate_y)
