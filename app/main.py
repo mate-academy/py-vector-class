782fbf021275af99424fad671968f6687f2dd64f
ladmael <don.sosiska339@gmail.com>
2023-12-27 11:01:49 +0200

    fix flake8

--- app/main.py
+++ app/main.py
@@ -1,50 +1,50 @@
+from __future__ import annotations
 import math
 
 
 class Vector:
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float) -> None:
         self.x = round(x, 2)
         self.y = round(y, 2)
 
-    def __add__(self, other):
+    def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other):
+    def __sub__(self, other: Vector) -> Vector:
         return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other):
+    def __mul__(self, other: Vector) -> Vector | float:
         if isinstance(other, (int, float)):
             return Vector(self.x * other, self.y * other)
         elif isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point, end_point):
+    def create_vector_by_two_points(cls, start_point: list, end_point: list) -> Vector:
         x = end_point[0] - start_point[0]
         y = end_point[1] - start_point[1]
         return cls(x, y)
 
-    def get_length(self):
+    def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
 
-    def get_normalized(self):
+    def get_normalized(self) -> Vector:
         length = self.get_length()
         return Vector(self.x / length, self.y / length)
 
-    def angle_between(self, other):
+    def angle_between(self, other: Vector) -> float:
         dot_product = self * other
         cos_angle = dot_product / (self.get_length() * other.get_length())
         angle_rad = math.acos(cos_angle)
         angle_deg = round(math.degrees(angle_rad))
         return angle_deg
 
-    def get_angle(self):
+    def get_angle(self) -> float:
         reference_vector = Vector(0, 1)
         return self.angle_between(reference_vector)
 
-    def rotate(self, degrees):
+    def rotate(self, degrees: float) -> Vector:
         radians = math.radians(degrees)
         new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
         new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
         return Vector(new_x, new_y)
-
