36f349ee86f7f4976dd614c74ae25f42ee097990
p.zbarazkyy <mtu09@ukr.net>
2023-12-28 06:39:12 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ class Vector:
     def __add__(self, other: Vector) -> Vector:
         return Vector((self.x + other.x), (self.y + other.y))
 
+
     def __sub__(self, other: Vector) -> Vector:
         return Vector((self.x - other.x), (self.y - other.y))
 
