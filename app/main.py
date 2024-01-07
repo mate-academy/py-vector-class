40410c1a56c7c2ede81cc11975b80a3a1527c295
barabarinov <barabarinov@gmail.com>
2024-01-07 18:14:48 +0200

    fixed variables names

--- app/main.py
+++ app/main.py
@@ -8,9 +8,9 @@ from math import (
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_point: float, y_point: float) -> None:
+        self.x = round(x_point, 2)
+        self.y = round(y_point, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
