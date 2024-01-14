53b4a370e329ab743c55021c072491cf9ea1eab8
Polonnikova Anna <xxxuko@gmail.com>
2024-01-14 20:49:45 +0200

    solution2

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 from math import sqrt, acos, degrees, atan2, radians, cos, sin
 
+
 class Vector:
     def __init__(self, x: int | float, y: int | float) -> None:
         self.x = round(x, 2)
         self.y = round(y, 2)
 
-    def __add__(self, other) -> Vector:
+    def __add__(self, other: Vector) -> Vector:
         return Vector(x=self.x + other.x, y=self.y + other.y)
 
-    def __sub__(self, other) -> Vector:
+    def __sub__(self, other: Vector) -> Vector:
         return Vector(x=self.x - other.x, y=self.y - other.y)
 
     def __mul__(self, other: int | float | Vector) -> Vector / float:
@@ -18,14 +19,22 @@ class Vector:
         return Vector(x=self.x * other, y=self.y * other)
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> Vector:
-        return cls(x=end_point[0] - start_point[0], y=end_point[1] - start_point[1])
+    def create_vector_by_two_points(
+            cls,
+            start_point: tuple,
+            end_point: tuple
+    ) -> Vector:
+        return cls(
+            x=end_point[0] - start_point[0],
+            y=end_point[1] - start_point[1]
+        )
 
     def get_length(self) -> int | float:
         return sqrt(self.x ** 2 + self.y ** 2)
 
     def get_normalized(self) -> Vector:
-        return Vector(x=self.x / Vector.get_length(self), y=self.y / Vector.get_length(self))
+        return Vector(x=self.x / Vector.get_length(self),
+                      y=self.y / Vector.get_length(self))
 
     def angle_between(self, other: Vector) -> int | float:
         numerator = Vector.__mul__(self, other)
@@ -41,13 +50,9 @@ class Vector:
             [cos(deg_to_radians), -sin(deg_to_radians)],
             [sin(deg_to_radians), cos(deg_to_radians)]
         ]
-        rotaded_x = rotation_matrix[0][0] * self.x + rotation_matrix[0][1] * self.y
-        rotated_y = rotation_matrix[1][0] * self.x + rotation_matrix[1][1] * self.y
+        rotaded_x = (rotation_matrix[0][0]
+                     * self.x + rotation_matrix[0][1] * self.y)
+        rotated_y = (rotation_matrix[1][0]
+                     * self.x + rotation_matrix[1][1] * self.y)
 
         return Vector(x=rotaded_x, y=rotated_y)
-
-vector = Vector(33, 8)
-vector2 = vector.rotate(45)
-
-print(vector2.x) #== 17.68
-print(vector2.y) #== 28.99
