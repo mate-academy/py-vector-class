5a6cebb081f672c5e06f0d5aabdfbbf97eea768a
Sofia Zholud <szholuds@gmail.com>
2024-01-17 15:40:40 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,60 @@
+import math
+from typing import Tuple
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: "Vector" or float) -> "Vector" or float:
+        if isinstance(other, Vector):
+            dot_product = self.x * other.x + self.y * other.y
+            return round(dot_product, 14)
+        elif isinstance(other, (int, float)):
+            return Vector(round(self.x * other, 2),
+                          round(self.y * other, 2))
+        else:
+            raise TypeError("Unsupported type for multiplication")
+
+    @classmethod
+    def create_vector_by_two_points(
+        cls, start_point: Tuple[float, float],
+        end_point: Tuple[float, float]
+    ) -> "Vector":
+        return cls(end_point[0] - start_point[0],
+                   end_point[1] - start_point[1])
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        if length == 0:
+            return Vector(0, 0)
+        return Vector(round(self.x / length, 2),
+                      round(self.y / length, 2))
+
+    def angle_between(self, other: "Vector") -> float:
+        if self.get_length() == 0 or other.get_length() == 0:
+            raise ValueError("Cannot compute angle with zero vector")
+        dot_product = self * other
+        cos_a = dot_product / (self.get_length() * other.get_length())
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> int:
+        angle = math.atan2(self.x, self.y)
+        angle_degrees = round(math.degrees(angle))
+        return abs(angle_degrees)
+
+    def rotate(self, degrees: int) -> "Vector":
+        radians = math.radians(degrees)
+        x_new = self.x * math.cos(radians) - self.y * math.sin(radians)
+        y_new = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(x_new, y_new)
