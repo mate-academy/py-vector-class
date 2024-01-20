35cb5277fb7d993eaa9f590e6ceda7d7c7daabb2
volry <v.v.ryzhuk@gmail.com>
2024-01-20 15:38:01 +0200

    add mul

--- app/main.py
+++ app/main.py
@@ -5,16 +5,44 @@ class Vector:
         self.x = round(x,2)
         self.y = round(y,2)
 
-    def __add__(self, other: 'Vector'):
+    def __add__(self, other: "Vector"):
         if not isinstance(other, Vector):
             raise ValueError("Add Vector to Vector or go drink coffee")
         return Vector(self.x + other.x, self.y + other.y)
+    
+    def __sub__(self, other: "Vector") -> "Vector":
+        if not isinstance(other, Vector):
+            raise ValueError ("Substract error")
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: Vector) -> None:
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        else:
+            raise TypeError("Something with numbers")
+
+
 
     def create_vector_by_two_points(start_point: tuple, end_point: tuple) -> None:
         pass
+
 #%%
-vector = Vector(-2.343, 8.008)
+
 
 # %%
-vector.x
-vector.y
+vector1 = Vector(2, 4)
+vector2 = Vector(-1, 3)
+vector3 = vector1 + vector2
+#%%
+isinstance(vector3, Vector)
+#vector3.x
+vector3.y
+# %%
+vector1 = Vector(2, 4)
+vector2 = vector1 * 3.743
+# %%
+isinstance(vector2, Vector)
+vector2.x
+vector2.y
