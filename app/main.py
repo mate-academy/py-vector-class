91579b94d28a289177dfc920f41bed6859a101b6
volry <v.v.ryzhuk@gmail.com>
2024-01-20 14:56:19 +0200

    initial start

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+#%%
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x,2)
+        self.y = round(y,2)
+
+#%%
+vector = Vector(-2.343, 8.008)
+
+# %%
+vector.x
+vector.y
