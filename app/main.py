9d39a53f5f8f16e8bf7fc8d2932302ef6c6ff149
voronvasia <voronvasia@gmail.com>
2024-01-23 00:09:45 +0200

    3 methods remain to be added

--- app/main.py
+++ app/main.py
@@ -1,3 +1,36 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> Vector:
+        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+
+    def __init__(self, x: int | float, y: int | float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: int | float | Vector) -> int | float | Vector:
+        if isinstance(other, Vector):
+            return (self.x * other.x) + (self.y * other.y)
+        return Vector(round(self.x * other, 2), round(self.y * other, 2))
+
+    def get_length(self) -> int | float:
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def get_normalized(self) -> Vector:
+        return Vector(self.x / self.get_length(), self.y / self.get_length())
+
+    # def angle_between(self):
+
+    # def get_angle(self):
+
+    # def rotate(self):
+
