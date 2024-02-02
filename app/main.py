1a31ff3e662af70bafab49ecb7fd843710a994de
Futilite <marketesh@gmail.com>
2024-02-02 21:38:01 +0200

    ClVect_2

--- app/main.py
+++ app/main.py
@@ -2,44 +2,52 @@ import math
 
 
 class Vector:
-    def __init__(self, x, y):
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coordinate: float, y_coordinate: float) -> None:
+        self.x = round(x_coordinate, 2)
+        self.y = round(y_coordinate, 2)
 
-    def __add__(self, other):
+    def __add__(self, other: "Vector") -> "Vector":
         return Vector(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other):
+    def __sub__(self, other: "Vector") -> "Vector":
         return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other):
-        if isinstance(other, Vector):
-            return self.x * other.x + self.y * other.y  # Dot product
-        else:
+    def __mul__(self, other: float) -> "Vector":
+        if isinstance(other, (int, float)):
             return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point, end_point):
-        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+    def create_vector_by_two_points(
+            cls,
+            start_point: tuple,
+            end_point: tuple
+    ) -> "Vector":
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
 
-    def get_length(self):
-        return math.sqrt(self.x ** 2 + self.y ** 2)
+    def get_length(self) -> float:
+        return math.sqrt(self.x**2 + self.y**2)
 
-    def get_normalized(self):
+    def get_normalized(self) -> "Vector":
         length = self.get_length()
         return Vector(self.x / length, self.y / length)
 
-    def angle_between(self, other):
+    def angle_between(self, other: "Vector") -> int:
         dot_product = self * other
-        angle_radians = math.acos(dot_product / (self.get_length() * other.get_length()))
-        return round(math.degrees(angle_radians))
+        cos_angle = dot_product / (self.get_length() * other.get_length())
+        angle_in_radians = math.acos(cos_angle)
+        angle_in_degrees = round(math.degrees(angle_in_radians))
+        return angle_in_degrees
 
-    def get_angle(self):
-        angle_radians = math.atan2(self.y, self.x)
-        angle_degrees = math.degrees(angle_radians)
-        return round(angle_degrees)
+    def get_angle(self) -> int:
+        reference_vector = Vector(0, 1)  # Positive Y axis
+        return self.angle_between(reference_vector)
 
-    def rotate(self, degrees):
+    def rotate(self, degrees: int) -> "Vector":
         radians = math.radians(degrees)
         new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
         new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
