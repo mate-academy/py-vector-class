8ce62a62a2c16332d9ddb3686545cc3e4e6ceea7
Andrei Butko <andreybutkomerefa@gmail.com>
2024-02-08 18:19:16 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,78 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: int | float, y: int | float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector | int | float) -> Vector:
+
+        if isinstance(other, Vector):
+            self.x += other.x
+            self.y += other.y
+
+        else:
+            self.x += other
+            self.y += other
+
+        return Vector(self.x, self.y)
+
+    def __sub__(self, other: Vector | int | float) -> Vector:
+
+        if isinstance(other, Vector):
+            self.x -= other.x
+            self.y -= other.y
+
+        else:
+            self.x -= other
+            self.y -= other
+
+        return Vector(self.x, self.y)
+
+    def __mul__(self, other: Vector | int | float) -> Vector | int | float:
+
+        if isinstance(other, Vector):
+            self.x *= round(other.x, 2)
+            self.y *= round(other.y, 2)
+            return self.x + self.y
+
+        else:
+            self.x *= other
+            self.y *= other
+            return Vector(self.x, self.y)
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> Vector:
+        start = Vector(start_point[0], start_point[1])
+        end = Vector(end_point[0], end_point[1])
+        return cls.__sub__(end, start)
+
+    def get_length(self) -> int | float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> Vector:
+        length = math.sqrt(self.x ** 2 + self.y ** 2)
+        x = self.x / length
+        y = self.y / length
+        return Vector(x, y)
+
+    def angle_between(self, other: Vector) -> int | float:
+        scalar = self.x * other.x + self.y * other.y
+        multiplication = (math.sqrt(self.x ** 2 + self.y ** 2)) * (math.sqrt(other.x ** 2 + other.y ** 2))
+        result = scalar / multiplication
+        return round(math.degrees(math.acos(result)))
+
+    def get_angle(self) -> int | float:
+        axis = Vector(0, 1)  # axis y
+        scalar = self.x * axis.x + self.y * axis.y
+        multiplication = (math.sqrt(self.x ** 2 + self.y ** 2)) * (math.sqrt(axis.x ** 2 + axis.y ** 2))
+        result = scalar / multiplication
+        return round(math.degrees(math.acos(result)))
+
+    def rotate(self, degrees: int | float) -> Vector:
+        degrees = math.radians(degrees)
+        x = self.x * math.cos(degrees) - self.y * math.sin(degrees)
+        y = self.x * math.sin(degrees) + self.y * math.cos(degrees)
+        return Vector(x, y)
