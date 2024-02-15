81306f9bb4b549c6436ca4c4c563666fe613a526
= <=>
2024-02-15 18:44:57 +0200

    Revert "created class Vector and his functions"
    This reverts commit 6e1e90c6cd2fa0325e4eade81d07275159569161.

--- app/main.py
+++ app/main.py
@@ -28,9 +28,7 @@ class Vector:
         )
 
     @classmethod
-    def create_vector_by_two_points(
-            cls, start_point: tuple, end_point: tuple
-    ) -> object:
+    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple):
         return cls(
             end_point[0] - start_point[0],
             end_point[1] - start_point[1]
