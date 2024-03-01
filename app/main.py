0af73638b60afa88f8ecbbe76b9fc6949002a387
Yaschenko1 <hectorlitopisec@gmail.com>
2024-03-01 17:52:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from typing import List, Union
+from typing import Union
 import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, point_x: float, point_y: float) -> None:
+        self.x = round(point_x, 2)
+        self.y = round(point_y, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
         return Vector(self.x + other.x, self.y + other.y)
@@ -13,15 +13,19 @@ class Vector:
     def __sub__(self, other: "Vector") -> "Vector":
         return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other: Union[int, float, "Vector"]) -> Union["Vector", float]:
+    def __mul__(self, other: Union[int, float]) -> Union["Vector", float]:
         if isinstance(other, (int, float)):
             return Vector(self.x * other, self.y * other)
         elif isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple[float, float], end_point: tuple[float, float]) -> "Vector":
-        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+    def create_vector_by_two_points(cls,
+                                    start_point: tuple[float, float],
+                                    end_point: tuple[float, float])\
+            -> "Vector":
+        return cls(end_point[0] - start_point[0],
+                   end_point[1] - start_point[1])
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
