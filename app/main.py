6c8fc5f481e6fd688aaac439aa8b2b3c837c6c0a
Olha <olgazinevych@gmail.com>
2024-03-02 22:01:30 +0200

    Fixed failed tests

--- app/main.py
+++ app/main.py
@@ -3,35 +3,36 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x1: float, y1: float) -> None:
+        self.x1 = round(x1, 2)
+        self.y1 = round(y1, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
-        return Vector(self.x + other.x, self.y + other.y)
+        return Vector(self.x1 + other.x1, self.y1 + other.y1)
 
     def __sub__(self, other: "Vector") -> "Vector":
-        return Vector(self.x - other.x, self.y - other.y)
+        return Vector(self.x1 - other.x1, self.y1 - other.y1)
 
     def __mul__(self, other: int | float | "Vector") -> "Vector" | float:
         # a · b = ax × bx + ay × by
         if isinstance(other, Vector):
-            return self.x * other.x + self.y * other.y
-        return Vector(self.x * other, self.y * other)
+            return self.x1 * other.x1 + self.y1 * other.y1
+        return Vector(self.x1 * other, self.y1 * other)
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> "Vector":
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - start_point[1]
-        return Vector(x, y)
+    def create_vector_by_two_points(cls, start_point: tuple,
+                                    end_point: tuple) -> "Vector":
+        x1 = end_point[0] - start_point[0]
+        y1 = end_point[1] - start_point[1]
+        return Vector(x1, y1)
 
     def get_length(self) -> float:
-        return (self.x ** 2 + self.y ** 2) ** 0.5
+        return (self.x1 ** 2 + self.y1 ** 2) ** 0.5
 
     def get_normalized(self) -> "Vector":
-        x = self.x / self.get_length()
-        y = self.y / self.get_length()
-        return Vector(x, y)
+        x1 = self.x1 / self.get_length()
+        y1 = self.y1 / self.get_length()
+        return Vector(x1, y1)
 
     def angle_between(self, vector: "Vector") -> int:
         dot_product = Vector.__mul__(self, vector)
@@ -40,11 +41,11 @@ class Vector:
         return round(math.degrees(math.acos(cos)))
 
     def get_angle(self) -> int:
-        y_axis = Vector(0, abs(self.y))
+        y_axis = Vector(0, abs(self.y1))
         return self.angle_between(y_axis)
 
     def rotate(self, degrees: int) -> "Vector":
         radians = math.radians(degrees)
-        x = self.x * math.cos(radians) - self.y * math.sin(radians)
-        y = self.x * math.sin(radians) + self.y * math.cos(radians)
-        return Vector(x, y)
+        x1 = self.x1 * math.cos(radians) - self.y1 * math.sin(radians)
+        y1 = self.x1 * math.sin(radians) + self.y1 * math.cos(radians)
+        return Vector(x1, y1)
