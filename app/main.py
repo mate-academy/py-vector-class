c4314908ae7e562b4f9a780ec1d9bdda5806a8f7
Olha <olgazinevych@gmail.com>
2024-03-02 22:05:46 +0200

    Final fix

--- app/main.py
+++ app/main.py
@@ -4,20 +4,20 @@ import math
 
 class Vector:
     def __init__(self, x1: float, y1: float) -> None:
-        self.x1 = round(x1, 2)
-        self.y1 = round(y1, 2)
+        self.x = round(x1, 2)
+        self.y = round(y1, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
-        return Vector(self.x1 + other.x1, self.y1 + other.y1)
+        return Vector(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other: "Vector") -> "Vector":
-        return Vector(self.x1 - other.x1, self.y1 - other.y1)
+        return Vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self, other: int | float | "Vector") -> "Vector" | float:
         # a · b = ax × bx + ay × by
         if isinstance(other, Vector):
-            return self.x1 * other.x1 + self.y1 * other.y1
-        return Vector(self.x1 * other, self.y1 * other)
+            return self.x * other.x + self.y * other.y
+        return Vector(self.x * other, self.y * other)
 
     @classmethod
     def create_vector_by_two_points(cls, start_point: tuple,
@@ -27,11 +27,11 @@ class Vector:
         return Vector(x1, y1)
 
     def get_length(self) -> float:
-        return (self.x1 ** 2 + self.y1 ** 2) ** 0.5
+        return (self.x ** 2 + self.y ** 2) ** 0.5
 
     def get_normalized(self) -> "Vector":
-        x1 = self.x1 / self.get_length()
-        y1 = self.y1 / self.get_length()
+        x1 = self.x / self.get_length()
+        y1 = self.y / self.get_length()
         return Vector(x1, y1)
 
     def angle_between(self, vector: "Vector") -> int:
@@ -41,11 +41,11 @@ class Vector:
         return round(math.degrees(math.acos(cos)))
 
     def get_angle(self) -> int:
-        y_axis = Vector(0, abs(self.y1))
+        y_axis = Vector(0, abs(self.y))
         return self.angle_between(y_axis)
 
     def rotate(self, degrees: int) -> "Vector":
         radians = math.radians(degrees)
-        x1 = self.x1 * math.cos(radians) - self.y1 * math.sin(radians)
-        y1 = self.x1 * math.sin(radians) + self.y1 * math.cos(radians)
+        x1 = self.x * math.cos(radians) - self.y * math.sin(radians)
+        y1 = self.x * math.sin(radians) + self.y * math.cos(radians)
         return Vector(x1, y1)
