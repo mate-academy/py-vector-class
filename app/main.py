c490902dddd4433c372aad4836a320398a8b905c
nazarkravchenko <nazarnazarik5@gmail.com>
2024-03-13 14:42:34 +0200

    check

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_component: float, y_component: float) -> None:
+        self.x = round(x_component, 2)
+        self.y = round(y_component, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
         return Vector(self.x + other.x, self.y + other.y)
@@ -21,9 +21,9 @@ class Vector:
     @classmethod
     def create_vector_by_two_points(cls, start_point: tuple,
                                     end_point: tuple) -> "Vector":
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - start_point[1]
-        return cls(x, y)
+        x_component = end_point[0] - start_point[0]
+        y_component = end_point[1] - start_point[1]
+        return cls(x_component, y_component)
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
