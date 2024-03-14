0282c96ec4ee62993f1b6491fb53bbb8248474d0
pochtalon <pochtalon1010@gmail.com>
2024-03-14 16:58:09 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_cor: float, y_cor: float) -> None:
+        self.x = round(x_cor, 2)
+        self.y = round(y_cor, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -20,8 +20,8 @@ class Vector:
             return Vector(round(self.x * other, 2), round(self.y * other, 2))
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> Vector:
-        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+    def create_vector_by_two_points(cls, start: tuple, end: tuple) -> Vector:
+        return cls(end[0] - start[0], end[1] - start[1])
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
