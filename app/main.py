3434a0a5e5f4ab07693eb5fc1691124815381cd1
Shafran Andriy <shafran.andriy@gmail.com>
2024-03-15 05:01:39 -0700

    Solution

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ class Vector:
     def __init__(self, x: float = 0, y: float = 0) -> None:
         self.x = round(x, 2)
         self.y = round(y, 2)
+
     def __add__(self, other: Vector) -> Vector:
         return Vector(
             x=round(self.x + other.x, 2),
@@ -34,17 +35,16 @@ class Vector:
     def create_vector_by_two_points(cls,
                                     start_point: tuple,
                                     end_point: tuple) -> Vector:
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - end_point[1]
-        return cls(x, y)
+        return cls(end_point[0] - start_point[0],
+                   end_point[1] - end_point[1])
 
     def get_length(self) -> float:
-        return (self.x**2 + self.y**2)**(1/2)
+        return (self.x ** 2 + self.y ** 2) ** (1 / 2)
 
     def get_normalized(self) -> Vector:
         return Vector(
-            x=round(self.x * (1/self.get_length()), 2),
-            y=round(self.y * (1/self.get_length()), 2)
+            x=round(self.x * (1 / self.get_length()), 2),
+            y=round(self.y * (1 / self.get_length()), 2)
         )
 
     def angle_between(self, other: Vector) -> int:
@@ -55,9 +55,8 @@ class Vector:
                 (self.__mul__(other) / self.get_length()
                  * other.get_length())))))
 
-    def get_angle(self):
+    def get_angle(self) -> None:
         pass
 
-    def rotate(self):
+    def rotate(self) -> None:
         pass
-
