7b27d8f463866bb7684cd675b6aa9bbd0a0422b5
Artem Andriushchenko <link.by.artem@gmail.com>
2024-03-16 03:57:52 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,49 @@
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: float) -> "Vector":
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y  # dot product
+        else:
+            return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> "Vector":
+        start_x, start_y = start_point
+        end_x, end_y = end_point
+        return cls(end_x - start_x, end_y - start_y)
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other: "Vector") -> int:
+        dot_product = self * other
+        cos_angle = dot_product / (self.get_length() * other.get_length())
+        angle_in_radians = math.acos(cos_angle)
+        angle_in_degrees = round(math.degrees(angle_in_radians))
+        return angle_in_degrees
+
+    def get_angle(self) -> int:
+        unit_y = Vector(0, 1)
+        return self.angle_between(unit_y)
+
+    def rotate(self, degrees: int) -> "Vector":
+        radians = math.radians(degrees)
+        rotated_x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        rotated_y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(rotated_x, rotated_y)
