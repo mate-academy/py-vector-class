422b0c5af9a976e9bd753d607fbf14b9b4bff079
Andrew Chugreev <andrey.4.1985@gmail.com>
2024-03-27 00:07:53 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,44 +3,44 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(
-            x=round(self.x + other.x, 2),
-            y=round(self.y + other.y, 2)
+            round(self.x + other.x, 2),
+            round(self.y + other.y, 2)
         )
 
     def __sub__(self, other: Vector) -> Vector:
         return Vector(
-            x=round(self.x - other.x, 2),
-            y=round(self.y - other.y, 2)
+            round(self.x - other.x, 2),
+            round(self.y - other.y, 2)
         )
 
-    def __mul__(self, other: Vector) -> Vector | float:
+    def __mul__(self, other: Vector | float) -> Vector | float:
         if isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
         return Vector(
-            x=round(self.x * other, 2),
-            y=round(self.y * other, 2)
+            round(self.x * other, 2),
+            round(self.y * other, 2)
         )
 
     @classmethod
     def create_vector_by_two_points(cls,
                                     start_point: tuple,
                                     end_point: tuple) -> Vector:
-        return cls(x=round(end_point[0] - start_point[0], 2),
-                   y=round(end_point[1] - start_point[1], 2))
+        return cls(round(end_point[0] - start_point[0], 2),
+                   round(end_point[1] - start_point[1], 2))
 
     def get_length(self) -> float:
         return (self.x ** 2 + self.y ** 2) ** (1 / 2)
 
     def get_normalized(self) -> Vector:
         return Vector(
-            x=round((self.x * (1 / self.get_length())), 2),
-            y=round((self.y * (1 / self.get_length())), 2)
+            round((self.x * (1 / self.get_length())), 2),
+            round((self.y * (1 / self.get_length())), 2)
         )
 
     def angle_between(self, other: Vector) -> float:
