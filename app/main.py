d62c57622d97dfaed4edb26ae02af0fe1400123e
Oleksandr Taranenko <oleksandrtaranenko239@gmail.com>
2024-04-15 20:43:01 +0300

    Solution with reformatting

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_value: float, y_value: float) -> None:
+        self.x = round(x_value, 2)
+        self.y = round(y_value, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -20,8 +20,12 @@ class Vector:
             return (self.x * other.x) + (self.y * other.y)
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> Vector:
-        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+    def create_vector_by_two_points(
+            cls, start_point: tuple, end_point: tuple
+    ) -> Vector:
+        return cls(
+            end_point[0] - start_point[0], end_point[1] - start_point[1]
+        )
 
     def get_length(self) -> float:
         return math.hypot(self.x, self.y)
@@ -37,7 +41,8 @@ class Vector:
         return round(
             math.degrees(
                 math.acos(
-                    self.__mul__(other) / (self.get_length() * other.get_length())
+                    self.__mul__(other)
+                    / (self.get_length() * other.get_length())
                 )
             )
         )
