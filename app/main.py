4ad1dbbece1ba3ad270f062c0e487e559176da15
Limbxbx <ilya.msd2003@gmail.com>
2024-04-22 19:36:01 +0300

    Implemented Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,58 @@
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
+        else:
+            raise TypeError("Unsupported operand type for +: {} and {}".format(type(self), type(other)))
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        if isinstance(other, Vector):
+            return Vector(self.x - other.x, self.y - other.y)
+        else:
+            raise TypeError("Unsupported operand type for -: {} and {}".format(type(self), type(other)))
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> "Vector":
+        x = end_point[0] - start_point[0]
+        y = end_point[1] - start_point[1]
+        return cls(x, y)
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        if length != 0:
+            return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other: 'Vector') -> int:
+        cos_angle = (self * other) / (self.get_length() * other.get_length())
+        angle_in_radians = math.acos(cos_angle)
+        angle_in_degrees = math.degrees(angle_in_radians)
+        return round(angle_in_degrees)
+
+    def get_angle(self) -> int:
+        angle_in_radians = math.atan2(self.x, self.y)
+        angle_in_degrees = math.degrees(angle_in_radians)
+        return round(math.fabs(angle_in_degrees))
+
+    def rotate(self, degrees: int) -> 'Vector':
+        radians = math.radians(degrees)
+        cos_theta = math.cos(radians)
+        sin_theta = math.sin(radians)
+        new_x = self.x * cos_theta - self.y * sin_theta
+        new_y = self.x * sin_theta + self.y * cos_theta
+        return Vector(new_x, new_y)
