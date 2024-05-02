cb164a59e96f1f6f645fef091d0da2ce3359323d
Artem <mikhailetc.artem@gmail.com>
2024-05-02 14:16:42 +0300

    rename x, y in __init__

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, math_x: float, math_y: float) -> None:
+        self.x = round(math_x, 2)
+        self.y = round(math_y, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
