ae193be0a3e61ff6756a5de4be88f717bae92558
Buchak Tamara <Buchak.Tamara@gmail.com>
2024-05-15 00:49:00 +0300

    add solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,66 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(
+            self.x + other.x,
+            self.y + other.y
+        )
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(
+            self.x - other.x,
+            self.y - other.y
+        )
+
+    def __mul__(self, other: Vector | float) -> Vector | float:
+        if isinstance(other, Vector):
+            return (self.x * other.x) + (self.y * other.y)
+
+        return Vector(
+            self.x * other,
+            self.y * other
+        )
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls,
+            start_point: tuple,
+            end_point: tuple
+    ) -> Vector:
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
+
+    def get_length(self) -> float:
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def get_normalized(self) -> Vector:
+        return Vector(
+            round(self.x / self.get_length(), 2),
+            round(self.y / self.get_length(), 2)
+        )
+
+    def angle_between(self, other: Vector) -> int:
+        cos_a = math.acos(
+            self.__mul__(other)
+            / (self.get_length() * other.get_length())
+        )
+        return round(math.degrees(cos_a))
+
+    def get_angle(self) -> int:
+        return self.angle_between(Vector(0, 1))
+
+    def rotate(self, degrees: int) -> Vector:
+        radians = math.radians(degrees)
+        return Vector(
+            self.x * math.cos(radians) - self.y * math.sin(radians),
+            self.y * math.cos(radians) + self.x * math.sin(radians),
+        )
