2c868e011915e3784cfb7178cbec7eddfd1c2b0d
unlie9 <unlie.devops9@gmail.com>
2024-05-26 10:42:20 +0300

    sol2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 import math
 
+
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_n: float, y_n: float) -> None:
+        self.x = round(x_n, 2)
+        self.y = round(y_n, 2)
 
     def __add__(self, other: Vector) -> Vector:
         if isinstance(other, Vector):
@@ -24,10 +25,12 @@ class Vector:
         return NotImplemented
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> Vector:
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - start_point[1]
-        return cls(x, y)
+    def create_vector_by_two_points(cls, start_point: tuple,
+                                    end_point: tuple) -> Vector:
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
@@ -47,11 +50,7 @@ class Vector:
         return round(angle_in_degrees)
 
     def get_angle(self) -> int:
-        angle_in_radians = math.atan2(self.x, self.y)
-        angle_in_degrees = math.degrees(angle_in_radians)
-        if angle_in_degrees < 0:
-            angle_in_degrees += 360
-        return round(angle_in_degrees)
+        return round(math.acos(self.y / self.get_length()) * (180 / math.pi))
 
     def rotate(self, degrees: int) -> Vector:
         radians = math.radians(degrees)
