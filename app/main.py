9a558051cd8f5abadb5ef2d996458d6e9c445283
Daria Ganchukova <umi194@gmail.com>
2024-06-16 18:14:42 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float | int, y: float | int) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self,
+                 coordinate_x: float | int,
+                 coordinate_y: float | int) -> None:
+        self.x = round(coordinate_x, 2)
+        self.y = round(coordinate_y, 2)
 
     def __add__(self, other: Vector) -> Vector:
         if not isinstance(other, Vector):
@@ -13,8 +15,8 @@ class Vector:
                             f" for +: 'Vector' and {type(other)}")
 
         return Vector(
-            x=self.x + other.x,
-            y=self.y + other.y
+            coordinate_x=self.x + other.x,
+            coordinate_y=self.y + other.y
         )
 
     def __sub__(self, other: Vector) -> Vector:
@@ -23,8 +25,8 @@ class Vector:
                             f" for +: 'Vector' and {type(other)}")
 
         return Vector(
-            x=self.x - other.x,
-            y=self.y - other.y
+            coordinate_x=self.x - other.x,
+            coordinate_y=self.y - other.y
         )
 
     def __mul__(self, other: Vector | float | int) -> Vector | float | int:
@@ -35,8 +37,8 @@ class Vector:
 
         if isinstance(other, (float, int)):
             return Vector(
-                x=self.x * other,
-                y=self.y * other
+                coordinate_x=self.x * other,
+                coordinate_y=self.y * other
             )
 
         return self.x * other.x + self.y * other.y
@@ -46,8 +48,8 @@ class Vector:
                                     start_point: tuple,
                                     end_point: tuple) -> Vector:
         return cls(
-            x=end_point[0] - start_point[0],
-            y=end_point[1] - start_point[1]
+            coordinate_x=end_point[0] - start_point[0],
+            coordinate_y=end_point[1] - start_point[1]
         )
 
     def get_length(self) -> float:
@@ -55,8 +57,8 @@ class Vector:
 
     def get_normalized(self) -> Vector:
         return Vector(
-            x=self.x / self.get_length(),
-            y=self.y / self.get_length()
+            coordinate_x=self.x / self.get_length(),
+            coordinate_y=self.y / self.get_length()
         )
 
     def angle_between(self, other: Vector) -> int:
@@ -69,14 +71,16 @@ class Vector:
 
     def get_angle(self) -> int:
         y_vect = Vector(
-            x=0,
-            y=1
+            coordinate_x=0,
+            coordinate_y=1
         )
         return self.angle_between(y_vect)
 
     def rotate(self, degrees: int) -> Vector:
         radians = math.radians(degrees)
         return Vector(
-            x=self.x * math.cos(radians) - self.y * math.sin(radians),
-            y=self.x * math.sin(radians) + self.y * math.cos(radians)
+            coordinate_x=(self.x * math.cos(radians)
+                          - self.y * math.sin(radians)),
+            coordinate_y=(self.x * math.sin(radians)
+                          + self.y * math.cos(radians))
         )
