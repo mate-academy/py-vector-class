519b4a9829ec285c265e05b53f53bf7c7dbd3191
maksym_korniev <maksymkorniev88@gmail.com>
2024-06-23 18:20:32 +0300

    rename x, y variables because of them flake gives error like that - "VNE001 single letter variable names like 'x' are not allowed"

--- app/main.py
+++ app/main.py
@@ -3,20 +3,20 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, first_cord: float, second_cord: float) -> None:
+        self.first_cord = round(first_cord, 2)
+        self.second_cord = round(second_cord, 2)
 
     def __add__(self, other: Vector) -> Vector:
-        return Vector(self.x + other.x, self.y + other.y)
+        return Vector(self.first_cord + other.first_cord, self.second_cord + other.second_cord)
 
     def __sub__(self, other: Vector) -> Vector:
-        return Vector(self.x - other.x, self.y - other.y)
+        return Vector(self.first_cord - other.first_cord, self.second_cord - other.second_cord)
 
     def __mul__(self, other: int | float | Vector) -> Vector | float:
         if isinstance(other, float) or isinstance(other, int):
-            return Vector(round(self.x * other, 2), round(self.y * other, 2))
-        return (self.x * other.x) + (self.y * other.y)
+            return Vector(round(self.first_cord * other, 2), round(self.second_cord * other, 2))
+        return (self.first_cord * other.first_cord) + (self.second_cord * other.second_cord)
 
     @classmethod
     def create_vector_by_two_points(cls, start_point: tuple,
@@ -26,13 +26,13 @@ class Vector:
         return cls(x, y)
 
     def get_length(self) -> float:
-        return math.sqrt(self.x ** 2 + self.y ** 2)
+        return math.sqrt(self.first_cord ** 2 + self.second_cord ** 2)
 
     def get_normalized(self) -> Vector:
         length = self.get_length()
         if length == 0:
             return Vector(0, 0)
-        return Vector(round(self.x / length, 2), round(self.y / length, 2))
+        return Vector(round(self.first_cord / length, 2), round(self.second_cord / length, 2))
 
     def angle_between(self, other: Vector) -> int:
         dot_product = self * other
@@ -51,6 +51,6 @@ class Vector:
         radians = math.radians(degrees)
         cos_angle = math.cos(radians)
         sin_angle = math.sin(radians)
-        x_new = self.x * cos_angle - self.y * sin_angle
-        y_new = self.x * sin_angle + self.y * cos_angle
+        x_new = self.first_cord * cos_angle - self.second_cord * sin_angle
+        y_new = self.first_cord * sin_angle + self.second_cord * cos_angle
         return Vector(round(x_new, 2), round(y_new, 2))
