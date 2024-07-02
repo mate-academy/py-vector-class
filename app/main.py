94a06afc89b5b5d5c2044f58cf5ad221099a37a4
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-07-02 20:25:22 +0300

    New VectorClass

--- app/main.py
+++ app/main.py
@@ -3,21 +3,27 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float | int, y: float | int) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, axis_x: float | int, axis_y: float | int) -> None:
+        self.axis_x = round(axis_x, 2)
+        self.axis_y = round(axis_y, 2)
 
     def __add__(self, other: int | float) -> int | float:
-        return Vector((self.x + other.x), (self.y + other.y))
+        return Vector(
+            (self.axis_x + other.axis_x),
+            (self.axis_y + other.axis_y)
+        )
 
     def __sub__(self, other: int | float) -> int | float:
-        return Vector((self.x - other.x), (self.y - other.y))
+        return Vector(
+            (self.axis_x - other.axis_x),
+            (self.axis_y - other.axis_y)
+        )
 
     def __mul__(self, other: int | float | Vector) -> int | float | Vector:
         if isinstance(other, (int, float)):
-            return Vector(self.x * other, self.y * other)
+            return Vector(self.axis_x * other, self.axis_y * other)
         elif isinstance(other, Vector):
-            return self.x * other.x + self.y * other.y
+            return self.axis_x * other.axis_x + self.axis_y * other.axis_y
 
     @classmethod
     def create_vector_by_two_points(
@@ -29,16 +35,19 @@ class Vector:
         for point in range(len(start_point)):
             point_list.append(end_point[point] - start_point[point])
 
-        x = point_list[0]
-        y = point_list[1]
+        axis_x = point_list[0]
+        axis_y = point_list[1]
 
-        return cls(x, y)
+        return cls(axis_x, axis_y)
 
     def get_length(self) -> Vector:
-        return math.sqrt(self.x ** 2 + self.y ** 2)
+        return math.sqrt(self.axis_x ** 2 + self.axis_y ** 2)
 
     def get_normalized(self) -> Vector:
-        return Vector(self.x / self.get_length(), self.y / self.get_length())
+        return Vector(
+            self.axis_x / self.get_length(),
+            self.axis_y / self.get_length()
+        )
 
     def angle_between(
             self,
@@ -60,6 +69,6 @@ class Vector:
         radians = math.radians(degrees)
         cos = math.cos(radians)
         sin = math.sin(radians)
-        new_x = round((self.x * cos) - (self.y * sin), 2)
-        new_y = round((self.x * sin) + (self.y * cos), 2)
+        new_x = round((self.axis_x * cos) - (self.axis_y * sin), 2)
+        new_y = round((self.axis_x * sin) + (self.axis_y * cos), 2)
         return Vector(new_x, new_y)
