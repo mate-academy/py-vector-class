4182e1876621925f06c9a0fe825bd2b1fd900384
Adrianna Regulska <adrianna.regulska.1@gmail.com>
2024-12-29 14:19:58 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,46 +1,47 @@
+from __future__ import annotations
 import math
 
 
 class Vector:
-    def __init__(self, vector_x: float, vector_y: float) -> None:
-        self.vector_x = round(vector_x, 2)
-        self.vector_y = round(vector_y, 2)
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
-        return Vector(self.vector_x + other.vector_x, self.vector_y
-                      + other.vector_y)
+        return Vector(self.x + other.x, self.y
+                      + other.y)
 
     def __sub__(self, other: "Vector") -> "Vector":
-        return Vector(self.vector_x - other.vector_x, self.vector_y
-                      - other.vector_y)
+        return Vector(self.x - other.x, self.y
+                      - other.y)
 
-    def __mul__(self, other: "Vector") -> "Vector" | float:
+    def __mul__(self, other: Vector | float) -> Vector | float:
         if isinstance(other, (int, float)):
-            return Vector(self.vector_x * other, self.vector_y * other)
+            return Vector(round(self.x * other, 2), round(self.y * other, 2))
         elif isinstance(other, Vector):
-            dot_product = (self.vector_x * other.vector_x + self.vector_y
-                           * other.vector_y)
-            return round(dot_product, 4)
+            dot_product = (self.x * other.x + self.y
+                           * other.y)
+            return dot_product
         else:
-            raise TypeError("Unsupported operation for * with type {}".
-                            format(type(other)))
+            raise TypeError(f"Unsupported operation for * with type "
+                            f"{type(other)}")
 
     @classmethod
     def create_vector_by_two_points(cls, start_point: tuple,
                                     end_point: tuple) -> "Vector":
-        vector_x = end_point[0] - start_point[0]
-        vector_y = end_point[1] - start_point[1]
-        return cls(vector_x, vector_y)
+        x = end_point[0] - start_point[0]
+        y = end_point[1] - start_point[1]
+        return cls(x, y)
 
     def get_length(self) -> float:
-        return math.sqrt(self.vector_x ** 2 + self.vector_y ** 2)
+        return math.sqrt(self.x ** 2 + self.y ** 2)
 
     def get_normalized(self) -> "Vector":
         length = self.get_length()
         if length == 0:
             raise ValueError("Cannot normalize a zero-length vector.")
         else:
-            return Vector(self.vector_x / length, self.vector_y / length)
+            return Vector(self.x / length, self.y / length)
 
     def angle_between(self, other: "Vector") -> float:
         dot_product = self * other
@@ -57,8 +58,8 @@ class Vector:
 
     def rotate(self, degrees: float) -> "Vector":
         radians = math.radians(degrees)
-        new_x = (self.vector_x * math.cos(radians) - self.vector_y
+        new_x = (self.x * math.cos(radians) - self.y
                  * math.sin(radians))
-        new_y = (self.vector_x * math.sin(radians) + self.vector_y
+        new_y = (self.x * math.sin(radians) + self.y
                  * math.cos(radians))
         return Vector(new_x, new_y)
