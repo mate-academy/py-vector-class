01b27005b1bde87acc4a257299d9e7d3e5d2be38
DAVYD <davfares@gmail.com>
2025-01-07 21:27:31 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -20,12 +20,15 @@ class Vector:
     def __mul__(self, other: Union[float, "Vector"]) -> Union[float, "Vector"]:
         if isinstance(other, (int, float)):
             return Vector(self.coord_x * other, self.coord_y * other)
-        dot_product = self.coord_x * other.coord_x + self.coord_y * other.coord_y
+
+        dot_product = (
+                self.coord_x * other.coord_x + self.coord_y * other.coord_y
+        )
         return round(dot_product, 4)
 
     @classmethod
     def create_vector_by_two_points(
-        cls, start: tuple[float, float], end: tuple[float, float]
+            cls, start: tuple[float, float], end: tuple[float, float]
     ) -> "Vector":
         coord_x_diff = end[0] - start[0]
         coord_y_diff = end[1] - start[1]
@@ -34,12 +37,14 @@ class Vector:
     def get_length(self) -> float:
         coord_x_squared = self.coord_x ** 2
         coord_y_squared = self.coord_y ** 2
-        return math.sqrt(coord_x_squared + coord_y_squared)
+        length = math.sqrt(coord_x_squared + coord_y_squared)
+        return length
 
     def get_normalized(self) -> "Vector":
         length = self.get_length()
         if length == 0:
             raise ValueError("Cannot normalize a zero-length vector.")
+
         coord_x_normalized = self.coord_x / length
         coord_y_normalized = self.coord_y / length
         return Vector(coord_x_normalized, coord_y_normalized)
@@ -58,6 +63,13 @@ class Vector:
         radians = math.radians(degrees)
         cos_angle = math.cos(radians)
         sin_angle = math.sin(radians)
-        coord_x_rotated = self.coord_x * cos_angle - self.coord_y * sin_angle
-        coord_y_rotated = self.coord_x * sin_angle + self.coord_y * cos_angle
+
+        coord_x_rotated = (
+                self.coord_x * cos_angle - self.coord_y * sin_angle
+        )
+        coord_y_rotated = (
+                self.coord_x * sin_angle + self.coord_y * cos_angle
+        )
+
         return Vector(coord_x_rotated, coord_y_rotated)
+
