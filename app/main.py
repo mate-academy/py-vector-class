2f3d18aef312566abea9690c73d89955fe5f8af3
Nick Nakonechnuy <teddykolia@ukr.net>
2025-01-31 12:49:18 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,36 +3,36 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float | int, y: float | int) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: float | int, y_coord: float | int) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: Vector) -> Vector:
-        x = self.x + other.x
-        y = self.y + other.y
-        return Vector(x, y)
+        x_coord = self.x + other.x
+        y_coord = self.y + other.y
+        return Vector(x_coord, y_coord)
 
     def __sub__(self, other: Vector) -> Vector:
-        x = self.x - other.x
-        y = self.y - other.y
-        return Vector(x, y)
+        x_coord = self.x - other.x
+        y_coord = self.y - other.y
+        return Vector(x_coord, y_coord)
 
     def __mul__(self, other: int | Vector) -> Vector | float:
         if not isinstance(other, Vector):
-            x = self.x * other
-            y = self.y * other
-            return Vector(x, y)
+            x_coord = self.x * other
+            y_coord = self.y * other
+            return Vector(x_coord, y_coord)
         else:
-            x = self.x * other.x
-            y = self.y * other.y
-            return x + y
+            x_coord = self.x * other.x
+            y_coord = self.y * other.y
+            return x_coord + y_coord
 
     @classmethod
     def create_vector_by_two_points(cls, start_point: tuple[float, float],
                                     end_point: tuple[float, float]) -> Vector:
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - start_point[1]
-        return cls(x, y)
+        x_coord = end_point[0] - start_point[0]
+        y_coord = end_point[1] - start_point[1]
+        return cls(x_coord, y_coord)
 
     def get_length(self) -> float:
         length = (self.x ** 2 + self.y ** 2) ** (1 / 2)
@@ -43,9 +43,9 @@ class Vector:
 
     def get_normalized(self) -> Vector:
         magnitude = self.magnitude()
-        x = self.x / magnitude
-        y = self.y / magnitude
-        return Vector(x, y)
+        x_coord = self.x / magnitude
+        y_coord = self.y / magnitude
+        return Vector(x_coord, y_coord)
 
     def angle_between(self, other: Vector) -> int:
         dot = self * other
