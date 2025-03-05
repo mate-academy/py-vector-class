956b65af9c772362dccdc99313c4a22eb45a9829
Sasha Shvaiuk <shvaiuk2310@gmail.com>
2025-03-05 21:07:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,52 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, vector_x: int | float, vector_y: int | float) -> None:
+        self.x = round(vector_x, 2)
+        self.y = round(vector_y, 2)
+
+    def __add__(self, other: Vector | int | float) -> Vector:
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
+        return Vector(self.x + other, self.y + other)
+
+    def __sub__(self, other: Vector | int | float) -> Vector:
+        if isinstance(other, Vector):
+            return Vector(self.x - other.x, self.y - other.y)
+        return Vector(self.x - other, self.y - other)
+
+    def __mul__(self, other: Vector | int | float) -> Vector:
+        if isinstance(other, Vector):
+            return Vector(self.x * other.x, self.y * other.y)
+        return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: tuple,
+                                    end_point: tuple) -> Vector:
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
+
+    def get_length(self) -> int | float:
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def get_normalized(self) -> Vector:
+        return Vector(self.x / self.get_length(), self.y / self.get_length())
+
+    def angle_between(self, other: Vector) -> int | float:
+        cos_a = ((self.x * other.x + self.y * other.y)
+                 / (self.get_length() * other.get_length()))
+        return math.degrees(math.acos(cos_a))
+
+    def get_angle(self) -> int | float:
+        return round(math.degrees(math.acos(self.y / self.get_length())))
+
+    def rotate(self, degrees: int) -> Vector:
+        radians = math.radians(degrees)
+        return Vector(
+            (self.x * math.cos(radians) - (self.y * math.sin(radians))),
+            (self.x * math.sin(radians) + (self.y * math.cos(radians)))
+        )
