e8c03a51de243d0198c5ac525ebe45222aa24288
Andrii Shtaher <andriishtaher@gmail.com>
2025-03-07 23:56:14 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -2,50 +2,53 @@ import math
 
 
 class Vector:
-    def __init__(self, x, y):
-        self.x = round(x, 2)
-        self.y = round(y, 2)
 
-    def __add__(self, other):
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
         return Vector(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other):
+    def __sub__(self, other: "Vector") -> "Vector":
         return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other):
+    def __mul__(self, other: "Vector") -> "Vector":
         if isinstance(other, Vector):
-            # Match test precision for dot product
             return round(self.x * other.x + self.y * other.y, 14)
         elif isinstance(other, (int, float)):
             return Vector(round(self.x * other, 2), round(self.y * other, 2))
-        else:
-            raise TypeError("Unsupported type for multiplication")
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point, end_point):
-        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+    def create_vector_by_two_points(
+            cls, start_point: tuple, end_point: tuple
+    ) -> "Vector":
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
 
-    def get_length(self):
+    def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
 
-    def get_normalized(self):
+    def get_normalized(self) -> "Vector":
         length = self.get_length()
         return Vector(round(self.x / length, 2), round(self.y / length, 2))
 
-    def angle_between(self, other):
+    def angle_between(self, other: "Vector") -> float:
         dot_product = self * other
         length_product = self.get_length() * other.get_length()
         cos_angle = dot_product / length_product
         angle = math.degrees(math.acos(cos_angle))
         return round(angle)
 
-    def get_angle(self):
+    def get_angle(self) -> float:
         radians = math.atan2(self.y, self.x)
         degrees = math.degrees(radians)
         angle_from_y_axis = (degrees - 90) % 360
         return round(angle_from_y_axis)
 
-    def rotate(self, degrees):
+    def rotate(self, degrees: float) -> "Vector":
         radians = math.radians(degrees)
         cos_angle = math.cos(radians)
         sin_angle = math.sin(radians)
