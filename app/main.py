9afe515888eade13e5c99ce920d2340a89afd5ca
Dmytro Stehantsev <stegan296@gmail.com>
2025-03-11 16:21:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,54 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:  # noqa VNE001
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x * -1, self.y + other.y * - 1)
+
+    def __mul__(self, other: Vector) -> Vector | float:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls,
+            start_point: tuple,
+            end_point: tuple
+    ) -> Vector:
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> Vector:
+        vector_length = self.get_length()
+        return Vector(self.x / vector_length, self.y / vector_length)
+
+    def angle_between(self, other: Vector) -> float:
+        scalar_product = self.__mul__(other)
+        modul_a = self.get_length()
+        modul_b = other.get_length()
+        cos_a = scalar_product / (modul_a * modul_b)
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> float:
+        cos_a = self.y / self.get_length()
+        return round(math.degrees(math.acos(cos_a)))
+
+    def rotate(self, degrees: int) -> Vector:
+        radians = math.radians(degrees)
+        new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(new_x, new_y)
