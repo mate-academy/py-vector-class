165e668803e70c8a712bce3532f0bda344a5b8b2
Farzam <farzamgt@gmail.com>
2025-03-24 18:03:21 +0200

    Implement Vector class with rotation method and fix related tests

--- app/main.py
+++ app/main.py
@@ -1,3 +1,61 @@
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: "Vector | int | float") -> "Vector | float":
+        if isinstance(other, (int, float)):
+            return Vector(round(self.x * other, 2), round(self.y * other, 2))
+        elif isinstance(other, Vector):
+            return round(self.x * other.x + self.y * other.y, 15)
+        raise TypeError("Unsupported operand type")
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls, start_point: tuple, end_point: tuple
+    ) -> "Vector":
+        return cls(
+            round(end_point[0] - start_point[0], 2),
+            round(end_point[1] - start_point[1], 2)
+        )
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        if length == 0:
+            return Vector(0, 0)
+        return Vector(round(self.x / length, 2), round(self.y / length, 2))
+
+    def angle_between(self, other: "Vector") -> int:
+        dot_product = self * other
+        length_mult = self.get_length() * other.get_length()
+        if length_mult == 0:
+            return 0
+        cos_angle = dot_product / length_mult
+        cos_angle = max(-1, min(1, cos_angle))
+        return round(math.degrees(math.acos(cos_angle)))
+
+    def get_angle(self) -> int:
+        reference_vector = Vector(0, 1)
+        return self.angle_between(reference_vector)
+
+    def rotate(self, degrees: int) -> "Vector":
+        radians = math.radians(degrees)
+        x_new = round(
+            self.x * math.cos(radians) - self.y * math.sin(radians)
+            , 2)
+        y_new = round(
+            self.x * math.sin(radians) + self.y * math.cos(radians)
+            , 2)
+        return Vector(x_new, y_new)
