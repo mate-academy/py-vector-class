08b50063ea9bf3ad865649bc162ce7aa27800ca8
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2025-03-24 19:21:08 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 import math
+from typing import Union
 
 
 class Vector:
-    def __init__(self, x_vector: int | float, y_vector: int | float) -> None:
+    def __init__(self,
+                 x_vector: Union[int, float],
+                 y_vector: Union[int, float]) -> None:
         self.x = round(x_vector, 2)
         self.y = round(y_vector, 2)
 
@@ -12,7 +15,7 @@ class Vector:
     def __sub__(self, other: "Vector") -> "Vector":
         return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other: "Vector" | int | float) -> "Vector":
+    def __mul__(self, other: Union["Vector", int, float]) -> "Vector":
         if isinstance(other, (int, float)):
             return Vector(self.x * other, self.y * other)
         else:
@@ -26,7 +29,7 @@ class Vector:
         y_diff = end_point[1] - start_point[1]
         return cls(x_diff, y_diff)
 
-    def get_length(self) -> int | float:
+    def get_length(self) -> Union[int, float]:
         return math.sqrt(self.x**2 + self.y**2)
 
     def get_normalized(self) -> "Vector":
@@ -37,7 +40,7 @@ class Vector:
         normalized_y = self.y / length
         return Vector(normalized_x, normalized_y)
 
-    def angle_between(self, other_vector: "Vector") -> int | float:
+    def angle_between(self, other_vector: "Vector") -> Union[int, float]:
         dot_product = self.x * other_vector.x + self.y * other_vector.y
         length_self = self.get_length()
         length_other = other_vector.get_length()
@@ -47,13 +50,13 @@ class Vector:
         angle_deg = math.degrees(angle_rad)
         return round(angle_deg)
 
-    def get_angle(self) -> int | float:
+    def get_angle(self) -> Union[int, float]:
         cos_angle = self.y / self.get_length()
         angle_rad = math.acos(cos_angle)
         angle_deg = math.degrees(angle_rad)
         return round(angle_deg)
 
-    def rotate(self, degrees: int | float) -> "Vector":
+    def rotate(self, degrees: Union[int, float]) -> "Vector":
         radians = math.radians(degrees)
         new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
         new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
