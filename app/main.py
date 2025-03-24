6529fa5ba7a54b50752d4dc5ecc0030bc1419cf7
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-03-24 18:41:10 +0100

    fix flake8

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x: float, y: float) -> None:  # noqa: VNE001
+        self.x = round(x, 2)  # noqa: VNE001
+        self.y = round(y, 2)  # noqa: VNE001
 
     def __add__(self, other: "Vector") -> "Vector":
         return Vector(
