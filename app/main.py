42e4d10c4d292cd91e445f9bc6b48c921d02a431
Ol <holmate@dontsp.am>
2025-04-05 17:35:26 +0300

    Fixed incorrect values for Flake8

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_cor: float, y_cor: float) -> None:
+        self.x = round(x_cor, 2)
+        self.y = round(y_cor, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -28,10 +28,10 @@ class Vector:
             start_point: tuple[float, float],
             end_point: tuple[float, float]
     ) -> Vector:
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - start_point[1]
+        x_point = end_point[0] - start_point[0]
+        y_point = end_point[1] - start_point[1]
 
-        return cls(x, y)
+        return cls(x_point, y_point)
 
     def get_length(self) -> float:
         length = (self.x ** 2 + self.y ** 2) ** 0.5
