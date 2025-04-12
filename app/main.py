775c43219520f1baea6b9bcae790acd63b76757a
Antonina Danylchenko <antonina.ivanets@gmail.com>
2025-04-12 16:30:14 +0200

    Implement Vector class with methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,51 @@
+from __future__ import annotations
+
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: Vector | float) -> Vector | float:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: tuple,
+                                    end_point: tuple) -> Vector:
+        new_x = end_point[0] - start_point[0]
+        new_y = end_point[1] - start_point[1]
+        return cls(new_x, new_y)
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> Vector:
+        length = self.get_length()
+        self.x = round(self.x / length, 2)
+        self.y = round(self.y / length, 2)
+        return self
+
+    def angle_between(self, other: Vector) -> int:
+        len_other = math.sqrt(other.x ** 2 + other.y ** 2)
+        cos_a = (self.__mul__(other) / (self.get_length() * len_other))
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> int:
+        y_axis = Vector(0, 1)
+        return self.angle_between(y_axis)
+
+    def rotate(self, degrees: int) -> Vector:
+        radians = math.radians(degrees)
+        new_x = math.cos(radians) * self.x - math.sin(radians) * self.y
+        new_y = math.sin(radians) * self.x + math.cos(radians) * self.y
+        return Vector(new_x, new_y)
