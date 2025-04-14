7cbf3289b86fc706d85143f4d3d582ce3e4b6e4b
Maksymiv Viktor <viktor.maksymiv.23@pnu.edu.ua>
2025-04-14 03:00:07 +0300

    add init and add methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
+from __future__ import annotations
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
