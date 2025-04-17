0fc9c5b7bed0862abebc74b0c9954aa30a10a3c8
Maksymiv Viktor <viktor.maksymiv.23@pnu.edu.ua>
2025-04-17 16:06:45 +0300

    provide flake8 rules in code

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 import math
 
+
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -19,8 +20,11 @@ class Vector:
         return Vector(self.x * other, self.y * other)
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> Vector:
-        return Vector(end_point[0] - start_point[0], end_point[1] - start_point[1])
+    def create_vector_by_two_points(cls,
+                                    start_point: tuple,
+                                    end_point: tuple) -> Vector:
+        return Vector(end_point[0] - start_point[0],
+                      end_point[1] - start_point[1])
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
@@ -38,6 +42,8 @@ class Vector:
         return round(math.degrees(math.acos(self.y / self.get_length())))
 
     def rotate(self, degrees: int) -> Vector:
-        x = self.x * math.cos(math.radians(degrees)) - self.y * math.sin(math.radians(degrees))
-        y = self.x * math.sin(math.radians(degrees)) + self.y * math.cos(math.radians(degrees))
-        return Vector(x, y)
+        x_coord = (self.x * math.cos(math.radians(degrees))
+                   - self.y * math.sin(math.radians(degrees)))
+        y_coord = (self.x * math.sin(math.radians(degrees))
+                   + self.y * math.cos(math.radians(degrees)))
+        return Vector(x_coord, y_coord)
