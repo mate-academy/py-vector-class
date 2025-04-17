9f9e582e7dbde18d80e22169e8e7cd04b67363ce
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-04-17 23:03:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,56 @@
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: "Vector") -> "Vector":
+        if isinstance(other, (int, float)):
+            return Vector(round(self.x * other, 2), round(self.y * other, 2))
+        elif isinstance(other, Vector):
+            return round(self.x * other.x + self.y * other.y, 4)
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: tuple,
+                                    end_point: tuple) -> "Vector":
+        return cls(end_point[0] - start_point[0],
+                   end_point[1] - start_point[1])
+
+    def get_length(self) -> float:
+        return math.sqrt((self.x ** 2) + (self.y ** 2))
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        if length == 0:
+            return Vector(0, 0)
+        return Vector(round(self.x / length, 2), round(self.y / length, 2))
+
+    def angle_between(self, other: "Vector") -> int:
+        dot = self * other
+        len_product = self.get_length() * other.get_length()
+        if len_product == 0:
+            return 0
+        cos_a = max(-1, min(1, dot / len_product))
+        angle_rad = math.acos(cos_a)
+        return round(math.degrees(angle_rad))
+
+    def get_angle(self) -> int:
+        angle_rad = math.atan2(self.x, self.y)
+        return round(math.degrees(angle_rad))
+
+    def rotate(self, degrees: int) -> "Vector":
+        radians = math.radians(degrees)
+        cos_theta = math.cos(radians)
+        sin_theta = math.sin(radians)
+        x_new = self.x * cos_theta - self.y * sin_theta
+        y_new = self.x * sin_theta + self.y * cos_theta
+        return Vector(round(x_new, 2), round(y_new, 2))
