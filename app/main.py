09a824caf675929258b065e70c7e9191357480c1
Konstantin Nahorni <konstantinnahorni@gmail.com>
2025-04-30 18:55:03 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,63 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(
+            x=self.x + other.x,
+            y=self.y + other.y,
+        )
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(
+            x=self.x - other.x,
+            y=self.y - other.y,
+        )
+
+    def __mul__(self, other: int | float | Vector) -> Vector:
+        if isinstance(other, Vector):
+            return ((self.x * other.x)
+                    + (self.y * other.y))
+        return Vector(
+            x=self.x * other,
+            y=self.y * other
+        )
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: tuple,
+                                    end_point: tuple) -> Vector:
+        cls.x = end_point[0] - start_point[0]
+        cls.y = end_point[1] - start_point[1]
+        return Vector(cls.x, cls.y)
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> Vector:
+        return Vector(
+            x=self.x / self.get_length(),
+            y=self.y / self.get_length()
+        )
+
+    def angle_between(self, other: Vector) -> int:
+        cos_a = (self * other) / (self.get_length() * other.get_length())
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> float:
+        length = self.get_length()
+        cos_a = self.y / length
+        return round(math.degrees(math.acos(cos_a)))
+
+    def rotate(self, degrees: int) -> Vector:
+        radians = math.radians(degrees)
+        cos_corner = math.cos(radians)
+        sin_corner = math.sin(radians)
+        return Vector(
+            x=self.x * cos_corner - self.y * sin_corner,
+            y=self.x * sin_corner + self.y * cos_corner
+        )
