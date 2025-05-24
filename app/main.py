1f9bbe7f4023c22975cf24e79fcd1a899b87df13
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-05-24 15:25:32 +0700

    Due to a contradiction between pytest, which searches for vector.x and vector.y and flake8, which do not allow single letter variables, I had to use @property

--- app/main.py
+++ app/main.py
@@ -4,9 +4,17 @@ import math
 
 class Vector:
 
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, v_x: float, v_y: float) -> None:
+        self.v_x = round(v_x, 2)
+        self.v_y = round(v_y, 2)
+
+    @property
+    def x(self) -> float:
+        return self.v_x
+
+    @property
+    def y(self) -> float:
+        return self.v_y
 
     def __add__(self, other: Vector) -> Vector:
         return Vector((self.x + other.x), (self.y + other.y))
@@ -14,7 +22,7 @@ class Vector:
     def __sub__(self, other: Vector) -> Vector:
         return Vector((self.x - other.x), (self.y - other.y))
 
-    def __mul__(self, other: Vector) -> Vector:
+    def __mul__(self, other: Vector | float) -> Vector | float:
         if isinstance(other, Vector):
             return (self.x * other.x) + (self.y * other.y)
         else:
