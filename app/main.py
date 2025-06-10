5773c80b03a456341a60c5b2228164ec314b0fb0
PlayWithDen4ik <velichkodenis34@gmail.com>
2025-06-10 23:06:42 +0300

    final solution

--- app/main.py
+++ app/main.py
@@ -4,46 +4,32 @@ from math import acos, degrees, ceil, cos, sin, radians
 
 class Vector:
 
-    def __init__(self, x: float, y: float) -> None:  # noqa: VNE001
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_axis: float, y_axis: float) -> None:
+        self.x = round(x_axis, 2)
+        self.y = round(y_axis, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
-        return Vector(
-            x=self.x + other.x,
-            y=self.y + other.y
-        )
+        return Vector(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other: "Vector") -> "Vector":
-        return Vector(
-            x=self.x - other.x,
-            y=self.y - other.y
-        )
+        return Vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self, other: Union["Vector", float]) -> Union["Vector", float]:
         if isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
-        return Vector(
-            x=self.x * other,
-            y=self.y * other
-        )
+        return Vector(self.x * other, self.y * other)
 
     @classmethod
     def create_vector_by_two_points(cls, start_point: tuple,
                                     end_point: tuple) -> "Vector":
-        return cls(
-            x=end_point[0] - start_point[0],
-            y=end_point[1] - start_point[1]
-        )
+        return cls(end_point[0] - start_point[0],
+                   end_point[1] - start_point[1])
 
     def get_length(self) -> float:
         return (self.x ** 2 + self.y ** 2) ** 0.5
 
     def get_normalized(self) -> "Vector":
-        return Vector(
-            x=self.x / self.get_length(),
-            y=self.y / self.get_length()
-        )
+        return Vector(self.x / self.get_length(), self.y / self.get_length())
 
     def angle_between(self, other: "Vector") -> int:
         return ceil(degrees(acos(
@@ -58,6 +44,6 @@ class Vector:
 
     def rotate(self, degrees: int) -> "Vector":
         return Vector(
-            x=cos(radians(degrees)) * self.x - sin(radians(degrees)) * self.y,
-            y=sin(radians(degrees)) * self.x + cos(radians(degrees)) * self.y
+            cos(radians(degrees)) * self.x - sin(radians(degrees)) * self.y,
+            sin(radians(degrees)) * self.x + cos(radians(degrees)) * self.y
         )
