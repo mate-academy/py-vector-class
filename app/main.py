bc395d5b31484ca2d0433ba985bf2981dd620d79
Mykhailo Vasyliev <marchinglobster@gmail.com>
2025-06-10 15:33:15 +0300

    fixed variables names

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_axis: float, y_axis: float) -> None:
+        self.x = round(x_axis, 2)
+        self.y = round(y_axis, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -23,9 +23,9 @@ class Vector:
     def create_vector_by_two_points(
             cls, start_point: tuple, end_point: tuple
     ) -> Vector:
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - start_point[1]
-        return cls(x, y)
+        x_axis = end_point[0] - start_point[0]
+        y_axis = end_point[1] - start_point[1]
+        return cls(x_axis, y_axis)
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
