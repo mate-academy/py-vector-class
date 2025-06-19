91a44d7273942f1941fff28718d0d99d992d8a7e
Mykola Totsevych <totsevich02@gmail.com>
2025-06-19 18:44:26 +0300

    Created magic methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,30 @@
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: int, y: int) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(
+            x=self.x + other.x,
+            y=self.y + other.y
+        )
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(
+            x=self.x - other.x,
+            y=self.y - other.y
+        )
+
+    def __mul__(self, other: "Vector | int") -> "Vector | int":
+        if not isinstance(other, (Vector, int)):
+            raise TypeError(f"unsupported operand "
+                            f"type(s) for +: 'Distance' and {type(other)}")
+
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+
+        return Vector(
+            x=self.x * other,
+            y=self.y * other
+        )
+
