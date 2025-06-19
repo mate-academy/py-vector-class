31c523b3fd1ba4e202691a4628be5060150051ce
Mykola Totsevych <totsevich02@gmail.com>
2025-06-19 22:32:27 +0300

    Fixed pytest errors and flake8 errors + added solution

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@ import math
 
 
 class Vector:
-    def __init__(self, x: int, y: int) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, coor_x: int, coor_y: int) -> None:
+        self.x = round(coor_x, 2)
+        self.y = round(coor_y, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
         if not isinstance(other, Vector):
             raise TypeError(f"unsupported operand "
                             f"type(s) for +: 'Distance' and {type(other)}")
         return Vector(
-            x=self.x + other.x,
-            y=self.y + other.y
+            coor_x=self.x + other.x,
+            coor_y=self.y + other.y
         )
 
     def __sub__(self, other: "Vector") -> "Vector":
@@ -20,12 +20,12 @@ class Vector:
             raise TypeError(f"unsupported operand "
                             f"type(s) for -: 'Distance' and {type(other)}")
         return Vector(
-            x=self.x - other.x,
-            y=self.y - other.y
+            coor_x=self.x - other.x,
+            coor_y=self.y - other.y
         )
 
-    def __mul__(self, other: "Vector | int") -> "Vector | int":
-        if not isinstance(other, (Vector, int)):
+    def __mul__(self, other: "Vector | int | float") -> "Vector | int | float":
+        if not isinstance(other, (Vector, int, float)):
             raise TypeError(f"unsupported operand "
                             f"type(s) for *: 'Distance' and {type(other)}")
 
@@ -33,24 +33,27 @@ class Vector:
             return self.x * other.x + self.y * other.y
 
         return Vector(
-            x=self.x * other,
-            y=self.y * other
+            coor_x=self.x * other,
+            coor_y=self.y * other
         )
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> "Vector":
+    def create_vector_by_two_points(
+            cls,
+            start_point: tuple,
+            end_point: tuple) -> "Vector":
         return cls(
-            x=end_point[0] - start_point[0],
-            y =end_point[1] - start_point[1]
+            coor_x=end_point[0] - start_point[0],
+            coor_y=end_point[1] - start_point[1]
         )
 
-    def get_length(self) -> int:
+    def get_length(self) -> int | float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
 
     def get_normalized(self) -> "Vector":
         return Vector(
-            x=self.x / self.get_length(),
-            y=self.y / self.get_length()
+            coor_x=self.x / self.get_length(),
+            coor_y=self.y / self.get_length()
         )
 
     def angle_between(self, other: "Vector") -> int:
@@ -59,19 +62,12 @@ class Vector:
         return round(angle)
 
     def get_angle(self) -> int:
-        coordinates_of_y = Vector(x=0, y=1)
+        coordinates_of_y = Vector(coor_x=0, coor_y=1)
         return self.angle_between(coordinates_of_y)
 
-    def rotate(self, degrees: int) -> "Vector":
+    def rotate(self, degrees: int | float) -> "Vector":
         radians = math.radians(degrees)
         return Vector(
-            x=(self.x * math.cos(radians)) - (self.y * math.sin(radians)),
-            y = (self.x * math.sin(radians)) + (self.y * math.cos(radians))
+            coor_x=(self.x * math.cos(radians)) - (self.y * math.sin(radians)),
+            coor_y=(self.x * math.sin(radians)) + (self.y * math.cos(radians))
         )
-
-
-
-
-
-
-
