eede593713cc5971e5c82500f84d6a3f82b9d3a4
Lukian <lukian55d@gmail.com>
2025-06-20 06:17:36 +0100

    donetask

--- app/main.py
+++ app/main.py
@@ -4,45 +4,55 @@ from typing import Tuple, Union
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coordinate: float, y_coordinate: float) -> None:
+        self.x_coordinate = round(x_coordinate, 2)
+        self.y_coordinate = round(y_coordinate, 2)
 
     def __add__(self, other: Vector) -> Vector:
         if not isinstance(other, Vector):
             raise TypeError("Can only add Vector to Vector")
-        return Vector(self.x + other.x, self.y + other.y)
+        return Vector(
+            self.x_coordinate + other.x_coordinate,
+            self.y_coordinate + other.y_coordinate
+        )
 
     def __sub__(self, other: Vector) -> Vector:
         if not isinstance(other, Vector):
             raise TypeError("Can only subtract Vector from Vector")
-        return Vector(self.x - other.x, self.y - other.y)
+        return Vector(
+            self.x_coordinate - other.x_coordinate,
+            self.y_coordinate - other.y_coordinate
+        )
 
     def __mul__(self, other: Union[float, Vector]) -> Union[Vector, float]:
         if isinstance(other, (int, float)):
-            return Vector(round(self.x * other, 2), round(self.y * other, 2))
+            return Vector(
+                round(self.x_coordinate * other, 2),
+                round(self.y_coordinate * other, 2)
+            )
         elif isinstance(other, Vector):
-            return self.x * other.x + self.y * other.y
+            return (self.x_coordinate * other.x_coordinate
+                    + self.y_coordinate * other.y_coordinate)
         raise TypeError("Can only multiply by number or Vector")
 
     @classmethod
     def create_vector_by_two_points(
-            cls,
-            start_point: Tuple[float, float],
-            end_point: Tuple[float, float]
+        cls,
+        start_point: Tuple[float, float],
+        end_point: Tuple[float, float]
     ) -> Vector:
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - start_point[1]
-        return cls(x, y)
+        x_coordinate = end_point[0] - start_point[0]
+        y_coordinate = end_point[1] - start_point[1]
+        return cls(x_coordinate, y_coordinate)
 
     def get_length(self) -> float:
-        return math.sqrt(self.x ** 2 + self.y ** 2)
+        return math.sqrt(self.x_coordinate ** 2 + self.y_coordinate ** 2)
 
     def get_normalized(self) -> Vector:
         length = self.get_length()
         if length == 0:
             return Vector(0, 0)
-        return Vector(self.x / length, self.y / length)
+        return Vector(self.x_coordinate / length, self.y_coordinate / length)
 
     def angle_between(self, other: Vector) -> int:
         dot_product = self * other
@@ -67,15 +77,18 @@ class Vector:
         angle_rad = math.acos(cos_angle)
         angle_deg = math.degrees(angle_rad)
 
+        if self.x_coordinate < 0:
+            angle_deg = 360 - angle_deg
+
         return round(angle_deg)
 
     def rotate(self, degrees: int) -> Vector:
         radians = math.radians(degrees)
         cos = math.cos(radians)
         sin = math.sin(radians)
-        new_x = self.x * cos - self.y * sin
-        new_y = self.x * sin + self.y * cos
+        new_x = self.x_coordinate * cos - self.y_coordinate * sin
+        new_y = self.x_coordinate * sin + self.y_coordinate * cos
         return Vector(round(new_x, 2), round(new_y, 2))
 
     def __repr__(self) -> str:
-        return f"Vector({self.x}, {self.y})"
+        return f"Vector({self.x_coordinate}, {self.y_coordinate})"
