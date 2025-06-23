8f31a6fae1d7380ab2b98c6a91cfaef2eac32b94
Lev <lev01111989@gmail.com>
2025-06-23 14:36:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,63 @@
+import math
+from typing import Tuple, Union
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x: float = round(x, 2)
+        self.y: float = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(
+            self,
+            other: Union["Vector", int, float]
+    ) -> Union["Vector", float]:
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        else:
+            raise TypeError("Multiplication not supported for this type.")
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls,
+            start_point: Tuple[float, float],
+            end_point: Tuple[float, float]
+    ) -> "Vector":
+        return cls(end_point[0] - start_point[0],
+                   end_point[1] - start_point[1])
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        if length == 0:
+            return Vector(0, 0)
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other: "Vector") -> float:
+        dot_product = self * other
+        len_self = self.get_length()
+        len_other = other.get_length()
+        if len_self == 0 or len_other == 0:
+            return 0.0
+        cos_a = dot_product / (len_self * len_other)
+        cos_a = max(-1.0, min(1.0, cos_a))
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> float:
+        y_axis_vector = Vector(0, 1)
+        return self.angle_between(y_axis_vector)
+
+    def rotate(self, degrees: float) -> "Vector":
+        radians = math.radians(degrees)
+        new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(new_x, new_y)
