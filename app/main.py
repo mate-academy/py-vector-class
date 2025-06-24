30a66386284bb571cc474669d270427333eb74fe
Oleksandr Pivtorak <Sanjok150397@gmail.com>
2025-06-24 18:33:03 +0300

    Edited

--- app/main.py
+++ app/main.py
@@ -4,20 +4,21 @@ import math
 
 class Vector:
 
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: float | int, y_coord: float | int) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: Vector) -> Vector:
-        return Vector(x=(self.x + other.x), y=(self.y + other.y))
+        return Vector(x_coord=(self.x + other.x), y_coord=(self.y + other.y))
 
     def __sub__(self, other: Vector) -> Vector:
-        return Vector(x=(self.x - other.x), y=(self.y - other.y))
+        return Vector(x_coord=(self.x - other.x), y_coord=(self.y - other.y))
 
     def __mul__(self, other: Vector | int | float) -> Vector | float:
         if not isinstance(other, Vector):
             return Vector(
-                x=round(self.x * other, 2), y=round(self.y * other, 2)
+                x_coord=round(self.x * other, 2),
+                y_coord=round(self.y * other, 2)
             )
         else:
             return self.x * other.x + self.y * other.y
@@ -27,7 +28,8 @@ class Vector:
         cls, start_point: tuple, end_point: tuple
     ) -> Vector:
         return cls(
-            x=end_point[0] - start_point[0], y=end_point[1] - start_point[1]
+            x_coord=end_point[0] - start_point[0],
+            y_coord=end_point[1] - start_point[1]
         )
 
     def get_length(self) -> float:
@@ -37,7 +39,7 @@ class Vector:
         length = self.get_length()
         if length == 0:
             raise ValueError("Can't normalize")
-        return Vector(x=self.x / length, y=self.y / length)
+        return Vector(x_coord=self.x / length, y_coord=self.y / length)
 
     def angle_between(self, other: Vector) -> int:
         dot_product = self * other
@@ -56,4 +58,4 @@ class Vector:
         new_x = self.x * cos_theta - self.y * sin_theta
         new_y = self.x * sin_theta + self.y * cos_theta
 
-        return Vector(x=new_x, y=new_y)
+        return Vector(x_coord=new_x, y_coord=new_y)
