6965333a6f8debb5aafb802efa9bdc227b949632
ArtemZakhar <artemzakharchuk@gmail.com>
2025-07-06 13:29:27 +0300

    feat: solution

--- app/main.py
+++ app/main.py
@@ -5,9 +5,9 @@ MultiplyType = Union["Vector", float]
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, horizontal: float, vertical: float) -> None:
+        self.x = round(horizontal, 2)
+        self.y = round(vertical, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
         return Vector(self.x + other.x, self.y + other.y)
