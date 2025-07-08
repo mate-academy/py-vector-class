43fa8e5a89bb6d1f885112aafe1143292669e7e3
slop <slopcloset@gmail.com>
2025-07-07 21:10:01 -0400

    Solution

--- app/main.py
+++ app/main.py
@@ -4,20 +4,20 @@ from math import sqrt, degrees, acos, cos, sin, radians
 
 class Vector:
     def __init__(self, x_coord: float, y_coord: float) -> None:
-        self.x_coord = round(x_coord, 2)
-        self.y_coord = round(y_coord, 2)
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, vector: Vector) -> Vector:
-        return Vector(self.x_coord + vector.x_coord, self.y_coord + vector.y_coord)
+        return Vector(self.x + vector.x, self.y + vector.y)
 
     def __sub__(self, vector: Vector) -> Vector:
-        return Vector(self.x_coord - vector.x_coord, self.y_coord - vector.y_coord)
+        return Vector(self.x - vector.x, self.y - vector.y)
 
     def __mul__(self, multiplier: Vector | int | float) -> Vector | float:
         if isinstance(multiplier, (int, float)):
-            return Vector(self.x_coord * multiplier, self.y_coord * multiplier)
+            return Vector(self.x * multiplier, self.y * multiplier)
         else:
-            return self.x_coord * multiplier.x_coord + self.y_coord * multiplier.y_coord
+            return self.x * multiplier.x + self.y * multiplier.y
 
     @classmethod
     def create_vector_by_two_points(cls,
@@ -28,10 +28,10 @@ class Vector:
                    end_point[1] - start_point[1])
 
     def get_length(self) -> float:
-        return sqrt(self.x_coord ** 2 + self.y_coord ** 2)
+        return sqrt(self.x ** 2 + self.y ** 2)
 
     def get_normalized(self) -> Vector:
-        return Vector(self.x_coord / self.get_length(), self.y_coord / self.get_length())
+        return Vector(self.x / self.get_length(), self.y / self.get_length())
 
     def angle_between(self, vector: Vector) -> int:
         cos_a = max(-1,
@@ -47,6 +47,6 @@ class Vector:
         return self.angle_between(y_axis)
 
     def rotate(self, degrees: int) -> Vector:
-        new_x = self.x_coord * cos(radians(degrees)) - self.y_coord * sin(radians(degrees))
-        new_y = self.x_coord * sin(radians(degrees)) + self.y_coord * cos(radians(degrees))
+        new_x = self.x * cos(radians(degrees)) - self.y * sin(radians(degrees))
+        new_y = self.x * sin(radians(degrees)) + self.y * cos(radians(degrees))
         return Vector(new_x, new_y)
