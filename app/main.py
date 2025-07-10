544725e7d9a2bf2b162c122cf2e9a5f4d2640413
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-07-10 19:34:04 +0200

    correction of too long lines and names of parameters + classmethod

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ from math import radians
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, px: float, py: float) -> None:
+        self.x = round(px, 2)
+        self.y = round(py, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -19,9 +19,11 @@ class Vector:
             return Vector(self.x * other, self.y * other)
         return self.x * other.x + self.y * other.y
 
-    def create_vector_by_two_points(start_point: (float, float), end_point: (float, float)) \
-            -> Vector:
-        return Vector(end_point[0] - start_point[0],
+    @classmethod
+    def create_vector_by_two_points(cls,
+                                    start_point: (float, float),
+                                    end_point: (float, float)) -> Vector:
+        return cls(end_point[0] - start_point[0],
                       end_point[1] - start_point[1])
 
     def get_length(self) -> float:
@@ -31,8 +33,8 @@ class Vector:
         return Vector(self.x / self.get_length(), self.y / self.get_length())
 
     def angle_between(self, other: Vector) -> int:
-        return round(math.degrees(math.acos((self * other)
-                                            / (self.get_length() * other.get_length()))))
+        return round(math.degrees(math.acos(
+            (self * other) / (self.get_length() * other.get_length()))))
 
     def get_angle(self) -> int:
         return self.angle_between(Vector(0, 1))
