302a635bc44c07006a5f8de51e93d95d45673747
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-07-10 19:28:19 +0200

    correction of too long lines

--- app/main.py
+++ app/main.py
@@ -19,8 +19,10 @@ class Vector:
             return Vector(self.x * other, self.y * other)
         return self.x * other.x + self.y * other.y
 
-    def create_vector_by_two_points(start_point: (float, float), end_point: (float, float)) -> Vector:
-        return Vector(end_point[0] - start_point[0], end_point[1] - start_point[1])
+    def create_vector_by_two_points(start_point: (float, float), end_point: (float, float)) \
+            -> Vector:
+        return Vector(end_point[0] - start_point[0],
+                      end_point[1] - start_point[1])
 
     def get_length(self) -> float:
         return (self.x ** 2 + self.y ** 2) ** 0.5
@@ -29,7 +31,8 @@ class Vector:
         return Vector(self.x / self.get_length(), self.y / self.get_length())
 
     def angle_between(self, other: Vector) -> int:
-        return round(math.degrees(math.acos((self * other) / (self.get_length() * other.get_length()))))
+        return round(math.degrees(math.acos((self * other)
+                                            / (self.get_length() * other.get_length()))))
 
     def get_angle(self) -> int:
         return self.angle_between(Vector(0, 1))
