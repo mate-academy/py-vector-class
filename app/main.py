878328d69cdf0628d5b8a915698017f28f7e90f5
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-07-11 09:51:45 +0200

    tuple instead of (float, float)

--- app/main.py
+++ app/main.py
@@ -21,8 +21,8 @@ class Vector:
 
     @classmethod
     def create_vector_by_two_points(cls,
-                                    start_point: (float, float),
-                                    end_point: (float, float)) -> Vector:
+                                    start_point: tuple(float, float),
+                                    end_point: tuple(float, float)) -> Vector:
         return cls(end_point[0] - start_point[0],
                    end_point[1] - start_point[1])
 
