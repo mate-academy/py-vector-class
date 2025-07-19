f7116c7d832e3f53d0b326bc989c1801dd839a93
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-07-20 01:07:24 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,52 @@
+from typing import Union
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self,
+                other: Union["Vector", float, int]) -> Union["Vector", float]:
+        if isinstance(other, (float, int)):
+            return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        else:
+            raise TypeError(f"Unsupported operand type: {type(other)} for *")
+
+    @classmethod
+    def create_vector_by_two_points(cls,
+                                    start_point: tuple,
+                                    end_point: tuple) -> "Vector":
+        return cls(end_point[0] - start_point[0],
+                   end_point[1] - start_point[1])
+
+    def get_length(self) -> float:
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other: "Vector") -> int:
+        cosine = self * other / (self.get_length() * other.get_length())
+        return round(math.degrees(math.acos(cosine)))
+
+    def get_angle(self) -> int:
+        return self.angle_between(Vector(0, 1))
+
+    def rotate(self, degrees: int) -> "Vector":
+        degrees_rad = math.radians(degrees)
+        return Vector(self.x * math.cos(degrees_rad)
+                      - self.y * math.sin(degrees_rad),
+                      self.x * math.sin(degrees_rad)
+                      + self.y * math.cos(degrees_rad))
