f9e1decda31c1ab84b56fd30e37d78de4d116aaa
Maksym Kiporenko <kiporenkomaks8@gmail.com>
2025-07-21 00:33:58 +0300

    Finish

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ import math
 
 
 class Vector:
+    # write your code here
+    pass
+
     def __init__(self, x: float, y: float) -> None:
         self.x = round(x, 2)
         self.y = round(y, 2)
