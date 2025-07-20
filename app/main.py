9652830e2c53fcb377e8485bad90d999c18b1868
Maksym Kiporenko <kiporenkomaks8@gmail.com>
2025-07-21 00:40:59 +0300

    Finish!!!

--- app/main.py
+++ app/main.py
@@ -6,9 +6,9 @@ class Vector:
     # write your code here
     pass
 
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: Vector) -> Vector:
         if isinstance(other, Vector):
