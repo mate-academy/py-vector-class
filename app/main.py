ab895e2485e45c4e7cb9968cf0183bcdd0bd0f73
artem <artemasarkisan598@gmail.com>
2025-08-24 19:40:38 +0300

    py-vector-class

--- app/main.py
+++ app/main.py
@@ -1,92 +1,61 @@
+from __future__ import annotations
 import math
 
 
 class Vector:
-    """Класс для работы с двумерными векторами."""
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
-    def __init__(self, x_coordinate: float, y_coordinate: float) -> None:
-        """Инициализация вектора с округлением координат до 2 знаков."""
-        self.x_coordinate = round(x_coordinate, 2)
-        self.y_coordinate = round(y_coordinate, 2)
+    def __add__(self, other: Vector) -> Vector:
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
+        raise TypeError("Operand must be Vector")
 
-    def __repr__(self) -> str:
-        """Строковое представление для отладки."""
-        return f"Vector({self.x_coordinate}, {self.y_coordinate})"
+    def __sub__(self, other: Vector) -> Vector:
+        if isinstance(other, Vector):
+            return Vector(self.x - other.x, self.y - other.y)
+        raise TypeError("Operand must be Vector")
 
-    def __str__(self) -> str:
-        """Красивое строковое представление вектора."""
-        return f"({self.x_coordinate}, {self.y_coordinate})"
-
-    def __add__(self, other: "Vector") -> "Vector":
-        """Сумма двух векторов."""
-        return Vector(
-            self.x_coordinate + other.x_coordinate,
-            self.y_coordinate + other.y_coordinate
-        )
-
-    def __sub__(self, other: "Vector") -> "Vector":
-        """Разность двух векторов."""
-        return Vector(
-            self.x_coordinate - other.x_coordinate,
-            self.y_coordinate - other.y_coordinate
-        )
-
-    def __mul__(self, other: int | float | "Vector") -> "Vector" | float:
-        """
-        Умножение вектора:
-        - на число (возвращает новый вектор),
-        - на другой вектор (скалярное произведение).
-        """
+    def __mul__(self, other: float | Vector) -> Vector | float:
         if isinstance(other, (int, float)):
-            return Vector(
-                self.x_coordinate * other,
-                self.y_coordinate * other
-            )
-        return (self.x_coordinate * other.x_coordinate
-                + self.y_coordinate * other.y_coordinate)
+            return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        raise TypeError("Operand must be int, float, or Vector")
 
     @classmethod
-    def create_vector_by_two_points(
-        cls, start_point: tuple[float, float], end_point: tuple[float, float]
-    ) -> "Vector":
-        """Создание вектора по двум точкам."""
-        delta_x = end_point[0] - start_point[0]
-        delta_y = end_point[1] - start_point[1]
-        return cls(delta_x, delta_y)
+    def create_vector_by_two_points(cls, start_point: tuple[float, float],
+                                    end_point: tuple[float, float]) -> Vector:
+        x_diff = end_point[0] - start_point[0]
+        y_diff = end_point[1] - start_point[1]
+        return cls(x_diff, y_diff)
 
     def get_length(self) -> float:
-        """Длина вектора."""
-        return math.sqrt(self.x_coordinate ** 2 + self.y_coordinate ** 2)
+        return math.sqrt(self.x ** 2 + self.y ** 2)
 
-    def get_normalized(self) -> "Vector":
-        """Нормализованный вектор."""
+    def get_normalized(self) -> Vector:
         length = self.get_length()
         if length == 0:
             return Vector(0, 0)
-        return Vector(
-            self.x_coordinate / length,
-            self.y_coordinate / length
-        )
+        return Vector(self.x / length, self.y / length)
 
-    def angle_between(self, vector: "Vector") -> int:
-        """Угол между текущим вектором и другим вектором (в градусах)."""
-        dot_product = (self.x_coordinate * vector.x_coordinate
-                       + self.y_coordinate * vector.y_coordinate)
+    def angle_between(self, vector: Vector) -> int:
+        dot_product = self.x * vector.x + self.y * vector.y
         length_self = self.get_length()
         length_other = vector.get_length()
         cos_theta = dot_product / (length_self * length_other)
-        return round(math.degrees(math.acos(cos_theta)))
+        theta = math.degrees(math.acos(cos_theta))
+        return round(theta)
 
     def get_angle(self) -> int:
-        """Угол между вектором и положительной осью Y (в градусах)."""
-        cos_theta = self.y_coordinate / self.get_length()
-        return round(math.degrees(math.acos(cos_theta)))
+        length_self = self.get_length()
+        cos_theta = self.y / length_self
+        theta_rad = math.acos(cos_theta)
+        return round(math.degrees(theta_rad))
 
-    def rotate(self, degrees: float) -> "Vector":
-        """Поворот вектора на указанный угол (в градусах)."""
+    def rotate(self, degrees: float) -> Vector:
         rad = math.radians(degrees)
-        new_x = (self.x_coordinate * math.cos(rad)
-                 - self.y_coordinate * math.sin(rad))
-        new_y = (self.x_coordinate * math.sin(rad)
-                 + self.y_coordinate * math.cos(rad))
+        new_x = self.x * math.cos(rad) - self.y * math.sin(rad)
+        new_y = self.x * math.sin(rad) + self.y * math.cos(rad)
         return Vector(new_x, new_y)
