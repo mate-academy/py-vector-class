28a1ba5628ecc805b14629ceb55b4b47adf22a9f
tomech <109527747+tomech4@users.noreply.github.com>
2025-09-06 18:26:30 +0200

    complete init method

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x_coordinate: float, y_coordinate: float):
+        self.x = x_coordinate
+        self.y = y_coordinate
+
