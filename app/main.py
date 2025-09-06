8d8fb54431cb377a1b009f5fc8ed8228f1e30591
tomech <109527747+tomech4@users.noreply.github.com>
2025-09-06 18:31:53 +0200

    complete sub magic method

--- app/main.py
+++ app/main.py
@@ -10,3 +10,9 @@ class Vector:
             x_coordinate=self.x + other.x,
             y_coordinate=self.y + other.y
         )
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(
+            x_coordinate=self.x - other.x,
+            y_coordinate=self.y - other.y
+        )
