2055f461238892cf4376ba1559c8057dd51d2f4a
tomech <109527747+tomech4@users.noreply.github.com>
2025-09-06 18:30:26 +0200

    complete add magic method

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
+from __future__ import annotations
+
 class Vector:
     def __init__(self, x_coordinate: float, y_coordinate: float):
         self.x = x_coordinate
         self.y = y_coordinate
 
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(
+            x_coordinate=self.x + other.x,
+            y_coordinate=self.y + other.y
+        )
