10e679258b932d918f3ae63623300c2a4d0c344a
Vorongor <mr.maddarknes@gmail.com>
2025-09-09 09:34:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,56 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: int | float, y: int | float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: int | float | Vector) -> Vector | float | int:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point: tuple,
+                                    end_point: tuple) -> Vector:
+        return cls(end_point[0] - start_point[0],
+                   end_point[1] - start_point[1])
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> Vector:
+        return Vector(self.x / self.get_length(),
+                      self.y / self.get_length())
+
+    def angle_between(self, other: Vector) -> int | float:
+        pod_prod = self * other
+        self_lenght = self.get_length()
+        other_lenght = other.get_length()
+        cos_a = pod_prod / (self_lenght * other_lenght)
+        angle = round(math.degrees(math.acos(cos_a)))
+        if angle < 0:
+            return - angle
+        return angle
+
+    def get_angle(self) -> float:
+        angle = round(math.degrees(math.atan2(self.x, self.y)))
+        if angle < 0:
+            return -angle
+        return angle
+
+    def rotate(self, angle: int) -> Vector:
+        radians_angle = math.radians(angle)
+        return Vector(self.x * math.cos(radians_angle)
+                      - self.y * math.sin(radians_angle),
+                      self.x * math.sin(radians_angle)
+                      + self.y * math.cos(radians_angle))
+# =========================Tests===============================#
