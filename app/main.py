a17d2c47c43a21d09b1466188066d558e0807285
Matheus Bessa <mathebessamo@gmail.com>
2025-09-11 22:38:30 -0300

    solution

--- app/main.py
+++ app/main.py
@@ -7,17 +7,19 @@ class Vector:
         self.x: float = round(coordinate_x, 2)
         self.y: float = round(coordinate_y, 2)
 
-    def __add__(self, other_vector: 'Vector') -> 'Vector':
+    def __add__(self, other_vector: "Vector") -> "Vector":
         new_x: float = self.x + other_vector.x
         new_y: float = self.y + other_vector.y
         return Vector(new_x, new_y)
 
-    def __sub__(self, other_vector: 'Vector') -> 'Vector':
+    def __sub__(self, other_vector: "Vector") -> "Vector":
         new_x: float = self.x - other_vector.x
         new_y: float = self.y - other_vector.y
         return Vector(new_x, new_y)
 
-    def __mul__(self, other: Union['Vector', float]) -> Union['Vector', float]:
+    def __mul__(
+        self, other: Union["Vector", float]
+    ) -> Union["Vector", float]:
         if isinstance(other, Vector):
             dot_product: float = (
                 self.x * other.x + self.y * other.y
@@ -32,7 +34,7 @@ class Vector:
     def create_vector_by_two_points(
         cls, start_point: Tuple[float, float],
         end_point: Tuple[float, float]
-    ) -> 'Vector':
+    ) -> "Vector":
         delta_x: float = end_point[0] - start_point[0]
         delta_y: float = end_point[1] - start_point[1]
         return cls(delta_x, delta_y)
@@ -41,7 +43,7 @@ class Vector:
         length: float = math.sqrt(self.x ** 2 + self.y ** 2)
         return length
 
-    def get_normalized(self) -> 'Vector':
+    def get_normalized(self) -> "Vector":
         vector_length: float = self.get_length()
         if vector_length == 0:
             return Vector(0, 0)
@@ -49,7 +51,7 @@ class Vector:
         normalized_y: float = self.y / vector_length
         return Vector(normalized_x, normalized_y)
 
-    def angle_between(self, other_vector: 'Vector') -> int:
+    def angle_between(self, other_vector: "Vector") -> int:
         dot_product: float = self * other_vector
         self_length: float = self.get_length()
         other_length: float = other_vector.get_length()
@@ -71,7 +73,7 @@ class Vector:
             angle_in_degrees += 360
         return round(angle_in_degrees)
 
-    def rotate(self, degrees_to_rotate: int) -> 'Vector':
+    def rotate(self, degrees_to_rotate: int) -> "Vector":
         radians_to_rotate: float = math.radians(degrees_to_rotate)
         cosine: float = math.cos(radians_to_rotate)
         sine: float = math.sin(radians_to_rotate)
