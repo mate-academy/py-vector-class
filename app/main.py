7dd8cdc8e72ce124c327ef601ce52bd399a217e2
Maksym Dazidov <maksymdazidov@gmail.com>
2025-09-16 18:05:00 +0200

    hard task №2

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class Vector:
 
     def __mul__(self, other: int | float | Vector) -> Vector | int | float:
         if isinstance(other, Vector):
-            return (self.x * other.x) + (self.y + other.y)
+            return round(self.x * other.x + self.y * other.y, 4)
         return Vector(round(self.x * other, 2), round(self.y * other, 2))
 
     @classmethod
@@ -33,7 +33,7 @@ class Vector:
 
     def get_normalized(self) -> Vector:
         length = self.get_length()
-        return Vector(self.x / length, self.y / length)
+        return Vector(round(self.x / length, 2), round(self.y / length, 2))
 
     def angle_between(self, vector: Vector) -> int:
         return round(
@@ -47,7 +47,7 @@ class Vector:
         )
 
     def get_angle(self) -> int:
-        return round(math.degrees(math.acos(self.x / self.get_length())))
+        return round(math.degrees(math.acos(self.y / self.get_length())))
 
     def rotate(self, degrees: int) -> Vector:
         return Vector(
