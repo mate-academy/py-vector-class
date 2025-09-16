ed6f0be5ddee646ec2ee9ba9be161cb052837fc8
Maksym Dazidov <maksymdazidov@gmail.com>
2025-09-16 17:43:08 +0200

    hard task

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -43,10 +43,10 @@ class Vector:
                 math.acos(
                     (self * vector)
                     / (self.get_length()
-                        * vector.get_length()
+                        * vector.get_length())
                 )
             )
-        ))
+        )
 
     def get_angel(self) -> int:
         return round(math.degrees(math.acos(self.y / self.get_length())))
