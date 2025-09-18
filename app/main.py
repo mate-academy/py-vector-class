34bccb15b184bd27588fd30d54699a0ccbfe0253
Alex <midgard044@gmail.com>
2025-09-18 19:53:12 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, end_x: float, end_y: float) -> None:
+        self.x = round(end_x, 2)
+        self.y = round(end_y, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return self.__class__(self.x + other.x, self.y + other.y)
