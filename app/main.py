43fe8c523e60024f7738f0716b4a13a89bb3a48d
Nik <nikita.pitushkin31@mail.com>
2025-10-10 11:41:56 +0300

    fix bag2

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
+
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -26,7 +27,10 @@ class Vector:
         start_point: tuple[float, float],
         end_point: tuple[float, float]
     ) -> Vector:
-        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
