172c9b38b7087f6183d7b5b30fed5dd780dee26a
Mike <tradewcs@gmail.com>
2025-10-27 16:41:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,60 @@
+from __future__ import annotations
+from math import sin, cos, acos, degrees, radians
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:  # noqa: VNE001
+        self.x = round(x, 2)  # noqa: VNE001
+        self.y = round(y, 2)  # noqa: VNE001
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: float | Vector) -> float | Vector:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+
+        if not isinstance(other, (int, float)):
+            return NotImplemented
+
+        return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(
+        cls,
+        start_point: tuple[int, int],
+        end_point: tuple[int, int]
+    ) -> Vector:
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
+
+    def get_length(self) -> float:
+        return (self * self) ** 0.5
+
+    def get_normalized(self) -> Vector:
+        return Vector(self.x / self.get_length(), self.y / self.get_length())
+
+    def angle_between(self, other: Vector) -> int:
+        return round(
+            degrees(
+                acos(
+                    self * other
+                    / (self.get_length() * other.get_length())
+                )
+            )
+        )
+
+    def get_angle(self) -> float:
+        return self.angle_between(Vector(0, 1))
+
+    def rotate(self, degrees_angle: float) -> Vector:
+        angle = radians(degrees_angle)
+        return Vector(
+            self.x * cos(angle) - self.y * sin(angle),
+            self.x * sin(angle) + self.y * cos(angle)
+        )
