a216e1b7d7dcf314ccb9f75f3a16a5bfbdf5b305
Vadim <vadimchubok@gmail.com>
2025-10-29 17:30:22 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,53 @@
+from __future__ import annotations
+
+import math
+
+
 class Vector:
     # write your code here
-    pass
+    def __init__(self, x_point: float, y_point: float) -> None:
+        self.x = round(x_point, 2)
+        self.y = round(y_point, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: Vector | int | float) -> Vector | float:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        return Vector(round(self.x * other, 2), round(self.y * other, 2))
+
+    @classmethod
+    def create_vector_by_two_points(cls,
+                                    start_point: tuple,
+                                    end_point: tuple) -> Vector:
+        x_point = end_point[0] - start_point[0]
+        y_point = end_point[1] - start_point[1]
+        return cls(x_point, y_point)
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> Vector:
+        return Vector(round(self.x / self.get_length(), 2),
+                      round(self.y / self.get_length(), 2))
+
+    def angle_between(self, other: Vector) -> float:
+        cos_a = (self * other) / (self.get_length() * other.get_length())
+        angle = math.degrees(math.acos(cos_a))
+        return round(angle)
+
+    def get_angle(self) -> float:
+        axis_y = Vector(0, 1)
+        cos_a = (self * axis_y) / (self.get_length() * axis_y.get_length())
+        angle = math.degrees(math.acos(cos_a))
+        return round(angle)
+
+    def rotate(self, degrees: float) -> Vector:
+        degrees = math.radians(degrees)
+        x_new = self.x * math.cos(degrees) - self.y * math.sin(degrees)
+        y_new = self.x * math.sin(degrees) + self.y * math.cos(degrees)
+        return Vector(x_new, y_new)
