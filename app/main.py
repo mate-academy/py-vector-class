c9dfc9710d158f2ccb6bbb2af39720a72180aa55
Oleksandr Polikovskiy <trotpolik@gmail.com>
2025-11-02 22:16:38 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: int, y: int) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_point: int, y_point: int) -> None:
+        self.x = round(x_point, 2)
+        self.y = round(y_point, 2)
 
     def __add__(self, other: Vector) -> Vector:
         if isinstance(other, Vector):
