5bef7ab7c23ee048d17f1ab3f9fce186f4e7f3b4
Ming <wmj327728951@gmail.com>
2025-11-16 16:15:51 +0100

    New one

--- app/main.py
+++ app/main.py
@@ -3,19 +3,19 @@ from typing import Union
 
 class Vector:
 
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, dot_x: float, dot_y: float) -> None:
+        self.x = round(dot_x, 2)
+        self.y = round(dot_y, 2)
 
-    def __add__(self, other: 'Vector') -> 'Vector':
+    def __add__(self, other: "Vector") -> "Vector":
         return Vector(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other: 'Vector') -> 'Vector':
+    def __sub__(self, other: "Vector") -> "Vector":
         return Vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self,
-                scalar: Union['Vector', float, int]
-                ) -> Union[float, 'Vector', int]:
+                scalar: Union["Vector", float, int]
+                ) -> Union[float, "Vector", int]:
         if isinstance(scalar, Vector):
             return self.x * scalar.x + self.y * scalar.y
         return Vector(round(self.x * scalar, 2), round(self.y * scalar, 2))
@@ -23,7 +23,7 @@ class Vector:
     @classmethod
     def create_vector_by_two_points(cls,
                                     start_point: tuple,
-                                    end_point: tuple) -> 'Vector':
+                                    end_point: tuple) -> "Vector":
         x_diff = end_point[0] - start_point[0]
         y_diff = end_point[1] - start_point[1]
         return cls(x_diff, y_diff)
@@ -31,13 +31,13 @@ class Vector:
     def get_length(self) -> float:
         return (self.x ** 2 + self.y ** 2) ** 0.5
 
-    def get_normalized(self) -> 'Vector':
+    def get_normalized(self) -> "Vector":
         length = self.get_length()
         if length == 0:
             return Vector(0, 0)
         return Vector(round(self.x / length, 2), round(self.y / length, 2))
 
-    def angle_between(self, other: 'Vector') -> float:
+    def angle_between(self, other: "Vector") -> float:
         from math import acos, degrees
         dot_product = self * other
         length_product = self.get_length() * other.get_length()
@@ -57,7 +57,7 @@ class Vector:
             angle_deg = -angle_deg
         return round(angle_deg)
 
-    def rotate(self, angle: float) -> 'Vector':
+    def rotate(self, angle: float) -> "Vector":
         from math import cos, sin, radians
         angle_rad = radians(angle)
         new_x = self.x * cos(angle_rad) - self.y * sin(angle_rad)
