ac7daa4e49102291477db8f49c4c1119d269518e
Yurii <pamkivurij92@gmail.com>
2025-11-28 15:45:14 +0100

    Solution, corrected flake8 mistakes

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, cord_x: float, cord_y: float) -> None:
+        self.x = round(cord_x, 2)
+        self.y = round(cord_y, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -24,7 +24,10 @@ class Vector:
             start_point: tuple,
             end_point: tuple
     ) -> Vector:
-        return Vector(end_point[0] - start_point[0], end_point[1] - start_point[1])
+        return Vector(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
