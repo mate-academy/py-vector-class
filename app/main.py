63269faa0e2aacd5e8ad1c593fc934df0e34809a
Kamil <kmirosz6@gmail.com>
2025-12-09 23:40:10 +0100

    Refactor Vector class: rename parameters for clarity and update __repr__ method; add pyproject.toml to .gitignore

--- app/main.py
+++ app/main.py
@@ -4,52 +4,52 @@ import math
 
 
 class Vector:
-    def __init__(self, x: int | float, y: int | float) -> None:  # noqa: VNE001
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, new_x: int | float, new_y: int | float) -> None:
+        self.x = round(new_x, 2)
+        self.y = round(new_y, 2)
 
     def __repr__(self) -> str:
-        return f"(x: {self.x}, y: {self.y})"
+        return f"(x: {self.x}, new_y: {self.y})"
 
     def __add__(self, other: Vector) -> Vector:
         if isinstance(other, Vector):
-            x = self.x + other.x  # noqa: VNE001
-            y = self.y + other.y  # noqa: VNE001
-            return Vector(x, y)
+            new_x = self.x + other.x
+            new_y = self.y + other.y
+            return Vector(new_x, new_y)
 
     def __sub__(self, other: Vector) -> Vector:
         if isinstance(other, Vector):
-            x = self.x - other.x  # noqa: VNE001
-            y = self.y - other.y  # noqa: VNE001
-            return Vector(x, y)
+            new_x = self.x - other.x
+            new_y = self.y - other.y
+            return Vector(new_x, new_y)
 
     def __mul__(self, other: Vector | int | float) -> Vector | float:
         if isinstance(other, Vector):
-            x = self.x * other.x  # noqa: VNE001
-            y = self.y * other.y  # noqa: VNE001
-            return x + y
+            new_x = self.x * other.x
+            new_y = self.y * other.y
+            return new_x + new_y
 
-        x = round(self.x * other, 2)  # noqa: VNE001
-        y = round(self.y * other, 2)  # noqa: VNE001
-        return Vector(x, y)
+        new_x = round(self.x * other, 2)
+        new_y = round(self.y * other, 2)
+        return Vector(new_x, new_y)
 
     @classmethod
     def create_vector_by_two_points(
         cls, start_point: Tuple[int, int], end_point: Tuple[int, int]
     ) -> Vector:
-        x = end_point[0] - start_point[0]  # noqa: VNE001
-        y = end_point[1] - start_point[1]  # noqa: VNE001
+        new_x = end_point[0] - start_point[0]
+        new_y = end_point[1] - start_point[1]
 
-        return Vector(x, y)
+        return Vector(new_x, new_y)
 
     def get_length(self) -> float:
         return (self.x**2 + self.y**2) ** 0.5
 
     def get_normalized(self) -> Vector:
         vector_len = self.get_length()
-        x = self.x / vector_len  # noqa: VNE001
-        y = self.y / vector_len  # noqa: VNE001
-        return Vector(x, y)
+        new_x = self.x / vector_len
+        new_y = self.y / vector_len
+        return Vector(new_x, new_y)
 
     def dot(self, other: Vector) -> float:
         return self.x * other.x + self.y * other.y
@@ -67,9 +67,9 @@ class Vector:
         rad_rotation = math.radians(rotation)
         new_x = self.x * math.cos(rad_rotation) - self.y * math.sin(
             rad_rotation
-        )  # noqa: VNE001
+        )
         new_y = self.x * math.sin(rad_rotation) + self.y * math.cos(
             rad_rotation
-        )  # noqa: VNE001
+        )
 
         return Vector(new_x, new_y)
