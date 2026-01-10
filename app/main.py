d33fd52ea0c1da2086401e0c6e1719cd64d4b030
Marian Hrytsan <gricanmaran@gmail.com>
2026-01-10 17:39:55 +0200

    Corection Solution

--- app/main.py
+++ app/main.py
@@ -19,14 +19,17 @@ class Vector:
         return (self.x * other.x) + (self.y * other.y)
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> Vector:
-        return cls(x=end_point[0] - start_point[0], y=end_point[1] - start_point[1])
+    def create_vector_by_two_points(cls, start_point: tuple,
+                                    end_point: tuple) -> Vector:
+        return cls(x=end_point[0] - start_point[0],
+                   y=end_point[1] - start_point[1])
 
     def get_length(self) -> float:
         return math.sqrt((self.x ** 2) + (self.y ** 2))
 
     def get_normalized(self) -> Vector:
-        return Vector(x=self.x / self.get_length(), y=self.y / self.get_length())
+        return Vector(x=self.x / self.get_length(),
+                      y=self.y / self.get_length())
 
     def angle_between(self, other: Vector) -> int:
         cos_a = (self * other) / (self.get_length() * other.get_length())
@@ -37,5 +40,7 @@ class Vector:
 
     def rotate(self, degrees: int) -> Vector:
         radians = math.radians(degrees)
-        return Vector(x=self.x * math.cos(radians) - self.y * math.sin(radians),
-                      y=self.x * math.sin(radians) + self.y * math.cos(radians))
+        return Vector(
+            x=self.x * math.cos(radians) - self.y * math.sin(radians),
+            y=self.x * math.sin(radians) + self.y * math.cos(radians),
+        )
