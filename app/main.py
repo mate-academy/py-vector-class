685f92d430775c71432207ee2762fd81c8b9ba90
Seroga <ilchuk.cergey@gmail.com>
2026-01-15 10:31:37 +0200

    fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, coord_x: float, coord_y: float) -> None:
+        self.x = round(coord_x, 2)
+        self.y = round(coord_y, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
         return Vector(self.x + other.x, self.y + other.y)
@@ -12,7 +12,7 @@ class Vector:
     def __sub__(self, other: "Vector") -> "Vector":
         return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other: "float | Vector") -> "Vector | float":
+    def __mul__(self, other: "int | float | Vector") -> "Vector | float | int":
         if isinstance(other, (int, float)):
             return Vector(self.x * other, self.y * other)
         elif isinstance(other, Vector):
@@ -22,10 +22,14 @@ class Vector:
             return NotImplemented
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> "Vector":
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - start_point[1]
-        return cls(x, y)
+    def create_vector_by_two_points(
+        cls,
+        start_point: tuple,
+        end_point: tuple
+    ) -> "Vector":
+        coord_x = end_point[0] - start_point[0]
+        coord_y = end_point[1] - start_point[1]
+        return cls(coord_x, coord_y)
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
@@ -49,4 +53,3 @@ class Vector:
         new_x = self.x * math.cos(rad) - self.y * math.sin(rad)
         new_y = self.x * math.sin(rad) + self.y * math.cos(rad)
         return Vector(new_x, new_y)
-
