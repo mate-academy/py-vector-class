b6529952fd267099f7249027e2938eaadd5ae1bb
Mafialate <mafialate@ukr.net>
2026-01-21 18:19:11 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,54 @@
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: Vector | float) -> Vector | float:
+        if isinstance(other, (float, int)):
+            return Vector(self.x * other, self.y * other)
+
+        return self.x * other.x + self.y * other.y
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls,
+            start_point: tuple[float, float],
+            end_point: tuple[float, float]
+    ) -> Vector:
+        x = end_point[0] - start_point[0]
+        y = end_point[1] - start_point[1]
+        return cls(x, y)
+
+    def get_length(self) -> float:
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def get_normalized(self) -> Vector:
+        x = self.x / self.get_length()
+        y = self.y / self.get_length()
+
+        return Vector(x, y)
+
+    def angle_between(self, other: Vector) -> float:
+        cos_theta = (self * other) / (self.get_length() * other.get_length())
+
+        return math.ceil(math.degrees(math.acos(cos_theta)))
+
+    def get_angle(self) -> int:
+        return math.floor(math.degrees(math.acos(self.y / self.get_length())))
+
+    def rotate(self, degrees: float) -> Vector:
+        x = ((math.cos(math.radians(degrees)) * self.x)
+             - (math.sin(math.radians(degrees)) * self.y))
+        y = ((math.sin(math.radians(degrees)) * self.x)
+             + (math.cos(math.radians(degrees)) * self.y))
+
+        return Vector(x, y)
