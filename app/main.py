949a491cf7d7120bbb0d9a290e0c0f768db9714d
mykolashashliyk-coder <mykolashashliyk@gmail.com>
2026-01-27 12:44:21 +0000

    Vector Class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,57 @@
+import math
+from typing import Tuple, Union
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: Union["Vector", float, int]) -> Union["Vector", float]:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(
+        cls,
+        start_point: Tuple[float, float],
+        end_point: Tuple[float, float],
+    ) -> "Vector":
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1],
+        )
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, vector: "Vector") -> int:
+        dot = self * vector
+        lengths = self.get_length() * vector.get_length()
+        cos_a = dot / lengths
+        angle = math.degrees(math.acos(cos_a))
+        return round(angle)
+
+    def get_angle(self) -> int:
+        # angle with positive Y axis
+        cos_a = self.y / self.get_length()
+        angle = math.degrees(math.acos(cos_a))
+        return round(angle)
+
+    def rotate(self, degrees: int) -> "Vector":
+        radians = math.radians(degrees)
+        x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(x, y)
+
