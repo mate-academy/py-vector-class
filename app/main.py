f998d5f70d0c0d3ac4106a88ffb2e5e3f29b095e
Kate <hamster.k.po@gmail.com>
2026-02-12 01:48:23 +0200

    x to horisontal, y to vertical

--- app/main.py
+++ app/main.py
@@ -3,26 +3,26 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float | int, y: float | int) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, horizontal: float | int, vertical: float | int) -> None:
+        self.x = round(horizontal, 2)
+        self.y = round(vertical, 2)
 
     def __add__(self, other: Vector) -> Vector:
         if isinstance(other, Vector):
-            return Vector(x=self.x + other.x,
-                          y=self.y + other.y)
+            return Vector(horizontal=self.x + other.x,
+                          vertical=self.y + other.y)
         return NotImplemented
 
     def __sub__(self, other: Vector) -> Vector:
         if isinstance(other, Vector):
-            return Vector(x=self.x - other.x,
-                          y=self.y - other.y)
+            return Vector(horizontal=self.x - other.x,
+                          vertical=self.y - other.y)
         return NotImplemented
 
     def __mul__(self, other: int | float | Vector) -> Vector | float:
         if isinstance(other, (int, float)):
-            return Vector(x=round(self.x * other, 2),
-                          y=round(self.y * other, 2))
+            return Vector(horizontal=round(self.x * other, 2),
+                          vertical=round(self.y * other, 2))
         if isinstance(other, Vector):
             return math.fabs(self.x * other.x) - math.fabs(self.y * other.y)
         return NotImplemented
@@ -31,8 +31,8 @@ class Vector:
     def create_vector_by_two_points(cls, start_point: tuple,
                                     end_point: tuple) -> Vector:
         if isinstance((start_point, end_point), tuple):
-            return cls(x=round(end_point[0] - start_point[0], 2),
-                       y=round(end_point[1] - start_point[1], 2))
+            return cls(horizontal=round(end_point[0] - start_point[0], 2),
+                       vertical=round(end_point[1] - start_point[1], 2))
         return NotImplemented
 
     def get_length(self) -> float:
@@ -42,8 +42,8 @@ class Vector:
 
     def get_normalized(self) -> Vector:
         if isinstance(self, Vector):
-            return Vector(x=round(self.x / self.get_length(), 2),
-                          y=round(self.y / self.get_length(), 2))
+            return Vector(horizontal=round(self.x / self.get_length(), 2),
+                          vertical=round(self.y / self.get_length(), 2))
         return NotImplemented
 
     def angle_between(self, other: Vector) -> int:
