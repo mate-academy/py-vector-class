140a06fa18eae22f7635e43730a4a59e3b69c01a
Karol <karolmarkut11@gmail.com>
2026-02-21 09:48:12 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+import math
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x, y):
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Vector(self.x * other.x, self.y * other.y)
+
+    
+
+
