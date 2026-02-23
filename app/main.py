380fb00c70c7d85d9310f1bfe5fba967a6f9da4b
Karol <karolmarkut11@gmail.com>
2026-02-23 08:39:06 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -14,6 +14,28 @@ class Vector:
     def __mul__(self, other):
         return Vector(self.x * other.x, self.y * other.y)
 
-    
+    def create_vector_by_two_points(cls, start_point, end_point):
+        x = end_point[0] - start_point[0]
+        y = end_point[1] - start_point[1]
+        return cls(x, y)
+
+    def get_length(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self):
+        length = self.get_length()
+        if length == 0:
+            return Vector(0, 0)
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other):
+        dot_product = self.x * other.x + self.y * other.y
+        magnitudes = self.get_length() * other.get_length()
+        if magnitudes == 0:
+            return 0
+        cos_theta = dot_product / magnitudes
+        cos_theta = max(-1, min(1, cos_theta))
+        angle_rad = math.acos(cos_theta)
+        return round(math.degrees(angle_rad))
 
 
