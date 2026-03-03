41acb4bf9148e47bfe2fc741b5fe1595f387a798
Pavlo <pavelkryvutsa@gmail.com>
2026-03-03 21:16:21 +0200

    Solution by py-vector-class2

--- app/main.py
+++ app/main.py
@@ -2,23 +2,27 @@ import math
 
 
 class Vector:
-    def __init__(self, x : float, y : float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, coord_x : float, coord_y : float) -> None:
+        self.x = round(coord_x, 2)
+        self.y = round(coord_y, 2)
 
     def __add__(self, other : Vector) -> Vector:
-        return Vector(self.x + other.x, self.y + other.y)
+        return Vector(self.x + other.x,
+                      self.y + other.y)
 
     def __sub__(self, other : Vector) -> Vector:
-        return Vector(self.x - other.x, self.y - other.y)
+        return Vector(self.x - other.x,
+                      self.y - other.y)
 
     def __mul__(self, other : Vector | int | float) -> Vector | int | float:
         if isinstance(other, (int, float)):
             return Vector(self.x * other, self.y * other)
-        return (self.x * other.x) + (self.y * other.y)
+        return ((self.x * other.x)
+                + (self.y * other.y))
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> Vector:
+    def create_vector_by_two_points(cls, start_point: tuple,
+                                    end_point: tuple) -> Vector:
         new_point = tuple(map(lambda x, y : y - x, start_point, end_point))
         return cls(*new_point)
 
@@ -26,13 +30,14 @@ class Vector:
         return (self.x ** 2 + self.y ** 2) ** 0.5
 
     def get_normalized(self) -> Vector:
-        return Vector(self.x / self.get_length(), self.y / self.get_length())
+        return Vector(self.x / self.get_length(),
+                      self.y / self.get_length())
 
     def angle_between(self, other : Vector) -> int:
         cos_angle = self.__mul__(other) / (self.get_length()
                                            * other.get_length())
-        arcCos = math.degrees(math.acos(cos_angle))
-        return round(arcCos)
+        arccos = math.degrees(math.acos(cos_angle))
+        return round(arccos)
 
     def get_angle(self) -> int:
         angle = math.degrees(math.acos(self.y / self.get_length()))
