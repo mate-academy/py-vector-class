9c5b3389dcc2000caba8556e7d9bd036efeb16a4
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-03-08 11:37:44 +0200

    changed variable name

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: int | float, y: int | float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, vector_x: int | float, vector_y: int | float) -> None:
+        self.x = round(vector_x, 2)
+        self.y = round(vector_y, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
