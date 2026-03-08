df4edde0e4595e80bb99376c73f2ead497e0555c
Dmytro Ostapenko <dr.dm.ostapenko@gmail.com>
2026-03-08 12:41:10 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,48 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: "Vector" | float) -> "Vector" | float:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        else:
+            return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(cls, point1: tuple,
+                                    point2: tuple) -> "Vector":
+        return cls(point2[0] - point1[0], point2[1] - point1[1])
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other: "Vector") -> int:
+        dot = self.x * other.x + self.y * other.y
+        len1 = math.sqrt(self.x ** 2 + self.y ** 2)
+        len2 = math.sqrt(other.x ** 2 + other.y ** 2)
+        cos_a = dot / (len1 * len2)
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> int:
+        return round(math.degrees(math.acos(self.y / self.get_length())))
+
+    def rotate(self, degrees: int) -> "Vector":
+        radians = math.radians(degrees)
+        new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(new_x, new_y)
