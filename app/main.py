b36f27dcd284f745cb2708a89f28b4d5e402c87d
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-03-08 21:26:31 -0500

    Refactor Vector class methods and attributes
    Refactor Vector class to improve readability and functionality.

--- app/main.py
+++ app/main.py
@@ -1,31 +1,51 @@
+import math
+
 class Vector:
-    def __init__(self, point_x: int | float, point_y: int | float) -> None:
-        self.point_x = round(point_x, 2)
-        self.point_y = round(point_y, 2)
+    def __init__(self, x: float, y: float):
+        self.x = round(x, 2)
+        self.y = round(y, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
-        point_x = self.point_x + other.point_x
-        point_y = self.point_y + other.point_y
-        return Vector(point_x, point_y)
+        return Vector(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other: "Vector") -> "Vector":
-        point_x = self.point_x - other.point_x
-        point_y = self.point_y - other.point_y
-        return Vector(point_x, point_y)
+        return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other: "Vector | float | int") -> "Vector | float | int":
-        if isinstance(other, (int, float)):
-            return Vector(self.point_x * other, self.point_y * other)
-        elif isinstance(other, Vector):
-            return self.point_x * other.point_x + self.point_y * other.point_y
-        else:
-            raise TypeError("Unsupported type for multiplication")
+    def __mul__(self, other: "Vector | float | int") -> "Vector | float":
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        return Vector(self.x * other, self.y * other)
 
     @classmethod
-    def create_vector_by_two_points(
-            cls,
-            start_point: int | float,
-            end_point: int | float) -> "Vector":
-        point_x = end_point[0] - start_point[0]
-        point_y = end_point[1] - start_point[1]
-        return cls(point_x, point_y)
+    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> "Vector":
+        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other: "Vector") -> int:
+        dot_product = self * other
+        cos_a = dot_product / (self.get_length() * other.get_length())
+        # Ensure cos_a stays within [-1, 1] to avoid math domain errors
+        cos_a = max(-1, min(1, cos_a))
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> int:
+        y_axis = Vector(0, 1)
+        return self.angle_between(y_axis)
+
+    def rotate(self, degrees: int) -> "Vector":
+        radians = math.radians(degrees)
+        cos_theta = math.cos(radians)
+        sin_theta = math.sin(radians)
+
+        new_x = self.x * cos_theta - self.y * sin_theta
+        new_y = self.x * sin_theta + self.y * cos_theta
+        return Vector(new_x, new_y)
+
+    def __repr__(self):
+        return f"Vector(x={self.x}, y={self.y})"
