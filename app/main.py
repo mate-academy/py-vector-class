afc354ba03d6c2a7f4341b58e5625ae1d39f303a
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-03-08 21:05:20 -0500

    Implement Vector class with arithmetic operations

--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x, y) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other) -> "Vector | float | int":
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        else:
+            raise TypeError("Unsupported type for multiplication")
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point, end_point) -> "Vector":
+        x = end_point[0] - start_point[0]
+        y = end_point[1] - start_point[1]
+        return cls(x, y)
