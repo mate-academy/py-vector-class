fde3e2e2d8778b46ac4fa8627b47e1964e35ad01
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-03-08 21:30:14 -0500

    Fix type hint for __mul__ method in Vector class

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ class Vector:
     def __sub__(self, other: "Vector") -> "Vector":
         return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other: Union["Vector", float, int]) -> Union["Vector", float]:
+    def __mul__(self, other: "Vector", float, int) -> "Vector", float:
         if isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
         return Vector(self.x * other, self.y * other)
