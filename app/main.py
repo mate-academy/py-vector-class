bd6755e0080def508e22773990cfb6b693caccce
Denis <dikikim@gmail.com>
2026-04-01 17:49:40 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float | int, y: float | int) -> None:
-        self.x = round(float(x), 2)
-        self.y = round(float(y), 2)
+    def __init__(self, x_coord: float | int, y_coord: float | int) -> None:
+        self.x = round(float(x_coord), 2)
+        self.y = round(float(y_coord), 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -20,9 +20,13 @@ class Vector:
 
     @classmethod
     def create_vector_by_two_points(
-            cls, start_point: tuple[float, float], end_point: tuple[float, float]
+            cls,
+            start_point: tuple[float, float],
+            end_point: tuple[float, float]
     ) -> Vector:
-        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+        x_diff = end_point[0] - start_point[0]
+        y_diff = end_point[1] - start_point[1]
+        return cls(x_diff, y_diff)
 
     def get_length(self) -> float:
         return math.hypot(self.x, self.y)
@@ -32,17 +36,17 @@ class Vector:
         return Vector(self.x / length, self.y / length)
 
     def angle_between(self, other: Vector) -> int:
-        dot_product = self * other
-        lengths_multiplied = self.get_length() * other.get_length()
+        dot_product = (self.x * other.x) + (self.y * other.y)
+        len_mult = math.hypot(self.x, self.y) * math.hypot(other.x, other.y)
 
-        if lengths_multiplied == 0:
+        if len_mult == 0:
             return 0
 
-        cos_a = dot_product / lengths_multiplied
+        cos_a = dot_product / len_mult
 
         cos_a = max(-1.0, min(1.0, cos_a))
 
-        return round(math.degrees(math.acos(cos_a)))
+        return int(round(math.degrees(math.acos(cos_a))))
 
     def get_angle(self) -> int:
         return self.angle_between(Vector(0, 1))
