3cf62785ae88a974aaab1877fe10e985efc89c69
Karol <karolmarkut11@gmail.com>
2026-04-13 18:55:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,46 +4,56 @@ from typing import Tuple, Self, Union
 
 class Vector:
     def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+        self.x = x
+        self.y = y
 
     def __add__(self, other: Self) -> Self:
         return Vector(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other: Self) -> Self:
         return Vector(self.x - other.x, self.y - other.y)
-    #
-    # def __mul__(self, other: Union[Self, int, float]) -> Union[Self, float]:
-    #     if isinstance(other, (int, float)):
-    #         return Vector(self.x * other, self.y * other)
-    #     return self.x * other.x + self.y * other.y
+
+    def __mul__(self, other: Union[Self, int, float]) -> Union[Self, float]:
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        return self.x * other.x + self.y * other.y
 
     @classmethod
     def create_vector_by_two_points(
-        cls, start_point: Tuple[float, float], end_point: Tuple[float, float]
+            cls, start_point: Tuple[float, float], end_point: Tuple[float, float]
     ) -> Self:
         return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
 
-    # def get_normalized(self) -> Self:
-    #     length = self.get_length()
-    #     return Vector(self.x / length, self.y / length)
-    #
-    # def angle_between(self, other: Self) -> int:
-    #     dot_product = self.x * other.x + self.y * other.y
-    #     cos_a = dot_product / (self.get_length() * other.get_length())
-    #     cos_a = max(-1.0, min(1.0,))
-    #     return round(math.degrees(math.acos(cos_a)))
+    def get_normalized(self) -> Self:
+        length = self.get_length()
+        if length == 0:
+            return Vector(0, 0)
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other: Self) -> int:
+        length_prod = self.get_length() * other.get_length()
+        if length_prod == 0:
+            return 0
+
+        dot_product = self * other  # Korzystamy z zdefiniowanego __mul__
+        cos_a = dot_product / length_prod
+        # Poprawiony błąd w min()
+        cos_a = max(-1.0, min(1.0, cos_a))
+        return round(math.degrees(math.acos(cos_a)))
 
     def get_angle(self) -> int:
-        angle_rad = math.atan2(self.x, self.y)
+        angle_rad = math.atan2(self.y, self.x)
         angle_deg = math.degrees(angle_rad)
-    #     return round(angle_deg)
+        return round(angle_deg)
 
     def rotate(self, degrees: int) -> Self:
         rad = math.radians(degrees)
         new_x = self.x * math.cos(rad) - self.y * math.sin(rad)
         new_y = self.x * math.sin(rad) + self.y * math.cos(rad)
         return Vector(new_x, new_y)
+
+    def __repr__(self) -> str:
+        return f"Vector({self.x:.2f}, {self.y:.2f})"
