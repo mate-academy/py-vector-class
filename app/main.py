858411106fc67c08608e3b920bb7615d84c29bb8
Gena <genada89@gmail.com>
2026-04-15 18:31:02 +0300

    create class Vector

--- app/main.py
+++ app/main.py
@@ -1,3 +1,66 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
+        raise TypeError("Can only add Vector to Vector")
+
+    def __sub__(self, other: Vector) -> Vector:
+        if isinstance(other, Vector):
+            return Vector(self.x - other.x, self.y - other.y)
+        raise TypeError("Can only subtract Vector from Vector")
+
+    def __mul__(self, other: int | float | Vector) -> int | float | Vector:
+        if isinstance(other, (int, float)):
+            return Vector(round(self.x * other, 2), round(self.y * other, 2))
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        raise TypeError("Can only multiply Vector by int, float or Vector")
+
+    @classmethod
+    def create_vector_by_two_points(
+        cls,
+        start_point: tuple[float, float],
+        end_point: tuple[float, float]
+    ) -> Vector:
+        return Vector(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
+
+    def get_length(self) -> float:
+        return (self.x**2 + self.y**2) ** 0.5
+
+    def get_normalized(self) -> Vector:
+        length = self.get_length()
+        if length == 0:
+            return Vector(0, 0)
+        return Vector(round(self.x / length, 2), round(self.y / length, 2))
+
+    def angle_between(self, other: Vector) -> int:
+        if isinstance(other, Vector):
+            dot_product = self * other
+            lengths_product = self.get_length() * other.get_length()
+            if lengths_product == 0:
+                return 0
+            cos_angle = max(min(dot_product / lengths_product, 1), -1)
+            return round(math.degrees(math.acos(cos_angle)))
+        raise TypeError("Can only calculate angle between two Vectors")
+
+    def get_angle(self) -> int:
+        return round(math.degrees(math.acos(self.y / self.get_length())))
+
+    def rotate(self, angle: int | float) -> Vector:
+        radians = math.radians(angle)
+        cos_angle = math.cos(radians)
+        sin_angle = math.sin(radians)
+        new_x = round(self.x * cos_angle - self.y * sin_angle, 2)
+        new_y = round(self.x * sin_angle + self.y * cos_angle, 2)
+        return Vector(new_x, new_y)
