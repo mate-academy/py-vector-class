08b878cb571cf8a3f41127139c4e6c6f71528aea
Pasha Liashenko <zedxi236@ukr.net>
2026-04-23 14:27:58 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 
 class Vector:
     def __init__(self, vector_x: float, vector_y: float) -> None:
-        self.x = vector_x
-        self.y = vector_y
+        self.x = round(vector_x, 2)
+        self.y = round(vector_y, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -12,11 +12,13 @@ class Vector:
     def __sub__(self, other: Vector) -> Vector:
         return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, scalar: float) -> Vector:
-        return Vector(self.x * scalar, self.y * scalar)
+    def __mul__(self, other: Vector | float) -> Vector | float:
+        if isinstance(other, Vector):
+            return Vector(self.x * other.x, self.y * other.y)
+        return self.x * other + self.y * other
 
-    def __truediv__(self, scalar: float) -> Vector:
-        return Vector(self.x / scalar, self.y / scalar)
+    def __truediv__(self, other: Vector | float) -> Vector | float:
+        return Vector(self.x / other.x, self.y / other.y)
 
     def __repr__(self) -> str:
         return f"Vector({self.x}, {self.y})"
