0200c3e46fa3907d3ad69c496d4f3a8b8b8ab054
Pasha Liashenko <zedxi236@ukr.net>
2026-04-23 14:08:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
+from __future__ import annotations
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = x
+        self.y = y
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, scalar: float) -> Vector:
+        return Vector(self.x * scalar, self.y * scalar)
+
+    def __truediv__(self, scalar: float) -> Vector:
+        return Vector(self.x / scalar, self.y / scalar)
+
+    def __repr__(self):
+        return f"Vector({self.x}, {self.y})"
+    
+
