a2f9114a3b0c480eabcd549d7f56619f982fd3ed
Pasha Liashenko <zedxi236@ukr.net>
2026-04-23 14:36:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import math
+
 
 class Vector:
     def __init__(self, vector_x: float, vector_y: float) -> None:
@@ -17,8 +19,34 @@ class Vector:
             return self.x * other + self.y * other
         return Vector(self.x * other.x, self.y * other.y)
 
-    def __truediv__(self, other: Vector | float) -> Vector | float:
-        return Vector(self.x / other.x, self.y / other.y)
+    def __truediv__(self, scalar: float) -> Vector:
+        return Vector(round(self.x / scalar, 2), round(self.y / scalar, 2))
 
     def __repr__(self) -> str:
         return f"Vector({self.x}, {self.y})"
+
+    @classmethod
+    def create_vector_by_two_points(
+        cls, start: tuple[float, float], end: tuple[float, float]
+    ) -> Vector:
+        return cls(end[0] - start[0], end[1] - start[1])
+
+    def get_length(self) -> float:
+        return round(math.sqrt(self.x ** 2 + self.y ** 2), 2)
+
+    def get_normalized(self) -> Vector:
+        length = self.get_length()
+        return Vector(round(self.x / length, 2), round(self.y / length, 2))
+
+    def angle_between(self, other: Vector) -> int:
+        cos_angle = (self * other) / (self.get_length() * other.get_length())
+        return round(math.degrees(math.acos(cos_angle)))
+
+    def get_angle(self) -> int:
+        return round(math.degrees(math.atan2(self.x, self.y)))
+
+    def rotate(self, degrees: int) -> Vector:
+        radians = math.radians(degrees)
+        new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(round(new_x, 2), round(new_y, 2))
