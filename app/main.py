a3307da2bb77b608a87eba0b72659a7832156a1a
Mirash1l1 <killer28392@gmail.com>
2026-05-14 20:49:26 +0300

    Сделано задание №4

--- app/main.py
+++ app/main.py
@@ -1,92 +1,87 @@
 import math
-from typing import Tuple, Union
+from typing import Union
+
+
+Number = Union[int, float]
 
 
 class Vector:
-    def __init__(self, coord_x: float, coord_y: float) -> None:
-        self.coord_x = round(coord_x, 2)
-        self.coord_y = round(coord_y, 2)
+    """Two-dimensional vector with basic vector operations."""
+
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
-        return Vector(
-            self.coord_x + other.coord_x,
-            self.coord_y + other.coord_y,
-        )
+        """Return the sum of two vectors."""
+        return Vector(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other: "Vector") -> "Vector":
-        return Vector(
-            self.coord_x - other.coord_x,
-            self.coord_y - other.coord_y,
-        )
+        """Return the difference between two vectors."""
+        return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(
-        self,
-        other: Union[int, float, "Vector"],
-    ) -> Union["Vector", float]:
+    def __mul__(self, other: Union["Vector", Number]) -> Union["Vector", float]:
+        """
+        Multiply vector by scalar or calculate dot product.
+
+        If `other` is a number, returns scaled vector.
+        If `other` is a vector, returns dot product.
+        """
         if isinstance(other, (int, float)):
-            return Vector(
-                self.coord_x * other,
-                self.coord_y * other,
-            )
+            return Vector(self.x * other, self.y * other)
 
-        return (
-            self.coord_x * other.coord_x
-            + self.coord_y * other.coord_y
-        )
+        return self.x * other.x + self.y * other.y
 
     @classmethod
     def create_vector_by_two_points(
         cls,
-        start_point: Tuple[float, float],
-        end_point: Tuple[float, float],
+        start_point: tuple[float, float],
+        end_point: tuple[float, float],
     ) -> "Vector":
+        """Create vector from two points."""
         return cls(
             end_point[0] - start_point[0],
             end_point[1] - start_point[1],
         )
 
     def get_length(self) -> float:
-        return (self.coord_x ** 2 + self.coord_y ** 2) ** 0.5
+        """Return vector length."""
+        return (self.x ** 2 + self.y ** 2) ** 0.5
 
     def get_normalized(self) -> "Vector":
+        """Return normalized vector."""
         length = self.get_length()
-        return Vector(
-            self.coord_x / length,
-            self.coord_y / length,
-        )
+        return Vector(self.x / length, self.y / length)
 
-    def angle_between(self, other_vector: "Vector") -> int:
-        dot_product = (
-            self.coord_x * other_vector.coord_x
-            + self.coord_y * other_vector.coord_y
-        )
+    def angle_between(self, other: "Vector") -> int:
+        """Return angle in degrees between two vectors."""
+        dot = self.x * other.x + self.y * other.y
+        denominator = self.get_length() * other.get_length()
 
-        denominator = self.get_length() * other_vector.get_length()
+        cos_a = dot / denominator
+        cos_a = max(-1.0, min(1.0, cos_a))
 
-        cos_angle = dot_product / denominator
-        cos_angle = max(-1.0, min(1.0, cos_angle))
-
-        return round(math.degrees(math.acos(cos_angle)))
+        return round(math.degrees(math.acos(cos_a)))
 
     def get_angle(self) -> int:
+        """Return angle between vector and Y-axis in degrees."""
         length = self.get_length()
+        cos_a = self.y / length
+        cos_a = max(-1.0, min(1.0, cos_a))
 
-        cos_angle = self.coord_y / length
-        cos_angle = max(-1.0, min(1.0, cos_angle))
-
-        return round(math.degrees(math.acos(cos_angle)))
+        return round(math.degrees(math.acos(cos_a)))
 
     def rotate(self, degrees: float) -> "Vector":
-        radians = math.radians(degrees)
+        """Return vector rotated by given degrees."""
+        theta = math.radians(degrees)
 
         new_x = (
-            self.coord_x * math.cos(radians)
-            - self.coord_y * math.sin(radians)
+            self.x * math.cos(theta)
+            - self.y * math.sin(theta)
         )
-
         new_y = (
-            self.coord_x * math.sin(radians)
-            + self.coord_y * math.cos(radians)
+            self.x * math.sin(theta)
+            + self.y * math.cos(theta)
         )
 
         return Vector(new_x, new_y)
