0795f0acbc63c432c9cd18dc003a014f36cf8d13
Anderson <anderson.medeiros3011@gmail.com>
2026-05-21 14:30:16 -0300

    correcao1

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ from typing import Tuple, Union
 
 
 class Vector:
-    def __init__(self, x: Union[int, float], y: Union[int, float]) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(
+        self, x_coordinate: Union[int, float], y_coordinate: Union[int, float]
+    ) -> None:
+        self.x = round(x_coordinate, 2)
+        self.y = round(y_coordinate, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
         return Vector(self.x + other.x, self.y + other.y)
@@ -13,20 +15,25 @@ class Vector:
     def __sub__(self, other: "Vector") -> "Vector":
         return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other: Union["Vector", int, float]) -> Union["Vector", int, float]:
+    def __mul__(
+        self, other: Union["Vector", int, float]
+    ) -> Union["Vector", int, float]:
         if isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
         return Vector(self.x * other, self.y * other)
 
     @classmethod
     def create_vector_by_two_points(
-            cls, start_point: Tuple[int, float], end_point: Tuple[int, float])-> "Vector":
-        x = end_point[0] - start_point[0]
-        y = end_point[1] - start_point[1]
-        return cls(x, y)
+        cls,
+        start_point: Tuple[Union[int, float], Union[int, float]],
+        end_point: Tuple[Union[int, float], Union[int, float]]
+    ) -> "Vector":
+        x_diff = end_point[0] - start_point[0]
+        y_diff = end_point[1] - start_point[1]
+        return cls(x_diff, y_diff)
 
     def get_length(self) -> float:
-        return math.sqrt(self.x**2 + self.y**2)
+        return math.sqrt(self.x ** 2 + self.y ** 2)
 
     def get_normalized(self) -> "Vector":
         length = self.get_length()
@@ -40,7 +47,7 @@ class Vector:
         return round(math.degrees(math.acos(cos_a)))
 
     def get_angle(self) -> int:
-        y_axis = Vector(0,1)
+        y_axis = Vector(0, 1)
         return self.angle_between(y_axis)
 
     def rotate(self, degrees: int) -> "Vector":
