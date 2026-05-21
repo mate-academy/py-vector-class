797947c1fd63993049d3d07c10afc11330417966
Anderson <anderson.medeiros3011@gmail.com>
2026-05-21 14:26:51 -0300

    Solução do exercício Vector com métodos mágicos e geometria

--- app/main.py
+++ app/main.py
@@ -1,3 +1,50 @@
+import math
+from typing import Tuple, Union
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: Union[int, float], y: Union[int, float]) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: Union["Vector", int, float]) -> Union["Vector", int, float]:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls, start_point: Tuple[int, float], end_point: Tuple[int, float])-> "Vector":
+        x = end_point[0] - start_point[0]
+        y = end_point[1] - start_point[1]
+        return cls(x, y)
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        if length == 0:
+            return Vector(0, 0)
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other: "Vector") -> int:
+        cos_a = (self * other) / (self.get_length() * other.get_length())
+        cos_a = max(-1.0, min(1.0, cos_a))
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> int:
+        y_axis = Vector(0,1)
+        return self.angle_between(y_axis)
+
+    def rotate(self, degrees: int) -> "Vector":
+        rad = math.radians(degrees)
+        new_x = self.x * math.cos(rad) - self.y * math.sin(rad)
+        new_y = self.x * math.sin(rad) + self.y * math.cos(rad)
+        return Vector(new_x, new_y)
