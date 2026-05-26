b60bc74d67ec23b0b3d4e13c0870e73eccac384f
askawa <ask_awa@icloud.com>
2026-05-26 16:29:29 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,16 +2,21 @@ import math
 
 
 class Vector:
-    # write your code here
-    def __init__(self, x, y) -> None:
+    def __init__(self, x: float, y: float) -> None:
         self.x = round(x, 2)
         self.y = round(y, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
-        return Vector(self.x + other.x, self.y + other.y)
+        return Vector(
+            self.x + other.x,
+            self.y + other.y,
+        )
 
     def __sub__(self, other: "Vector") -> "Vector":
-        return Vector(self.x - other.x, self.y - other.y)
+        return Vector(
+            self.x - other.x,
+            self.y - other.y,
+        )
 
     def __mul__(self, other: Vector | int | float) -> Vector | float:
         if isinstance(other, Vector):
