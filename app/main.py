6d20570b203c7bfda068810c60df724f8e12f250
Bogdan <bdemidkin@gmail.com>
2026-06-10 16:51:44 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,31 +5,31 @@ import math
 class Vector:
 
     def __init__(self, x_coordinate: float, y_coordinate: float) -> None:
-        self.x_coordinate = round(x_coordinate, 2)
-        self.y_coordinate = round(y_coordinate, 2)
+        self.x = round(x_coordinate, 2)
+        self.y = round(y_coordinate, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(
-            self.x_coordinate + other.x_coordinate,
-            self.y_coordinate + other.y_coordinate,
+            self.x + other.x,
+            self.y + other.y,
         )
 
     def __sub__(self, other: Vector) -> Vector:
         return Vector(
-            self.x_coordinate - other.x_coordinate,
-            self.y_coordinate - other.y_coordinate,
+            self.x - other.x,
+            self.y - other.y,
         )
 
     def __mul__(self, other: Vector | float) -> Vector | float:
 
         if isinstance(other, Vector):
-            return (self.x_coordinate * other.x_coordinate) + (
-                self.y_coordinate * other.y_coordinate
+            return (self.x * other.x) + (
+                self.y * other.y
             )
         else:
             return Vector(
-                round(self.x_coordinate * other, 2),
-                round(self.y_coordinate * other, 2)
+                round(self.x * other, 2),
+                round(self.y * other, 2)
             )
 
     @classmethod
@@ -43,17 +43,17 @@ class Vector:
                    )
 
     def get_length(self) -> float:
-        return (self.x_coordinate**2 + self.y_coordinate**2) ** 0.5
+        return (self.x**2 + self.y**2) ** 0.5
 
     def get_normalized(self) -> Vector:
         length = self.get_length()
-        return Vector(self.x_coordinate / length, self.y_coordinate / length)
+        return Vector(self.x / length, self.y / length)
 
     def angle_between(self, vector: Vector) -> int:
 
         cos_a = (
-            self.x_coordinate * vector.x_coordinate
-            + self.y_coordinate * vector.y_coordinate
+            self.x * vector.x
+            + self.y * vector.y
         ) / (self.get_length() * vector.get_length())
 
         angle = math.degrees(math.acos(cos_a))
@@ -61,7 +61,7 @@ class Vector:
         return round(angle)
 
     def get_angle(self) -> int:
-        angle = math.degrees(math.atan2(self.x_coordinate, self.y_coordinate))
+        angle = math.degrees(math.atan2(self.x, self.y))
 
         return round(abs(angle))
 
@@ -69,8 +69,8 @@ class Vector:
 
         radians = math.radians(degrees)
         return Vector(
-            self.x_coordinate * math.cos(radians)
-            - self.y_coordinate * math.sin(radians),
-            self.x_coordinate * math.sin(radians)
-            + self.y_coordinate * math.cos(radians),
+            self.x * math.cos(radians)
+            - self.y * math.sin(radians),
+            self.x * math.sin(radians)
+            + self.y * math.cos(radians),
         )
