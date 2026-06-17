4723b59058ff31ee357438e52d3e4ed35e36a5a6
Ruslan <makar80008@gmail.com>
2026-06-17 14:45:24 +0300

    ГОТОВО!

--- app/main.py
+++ app/main.py
@@ -1,3 +1,59 @@
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: ("Vector | float | int")) -> ("Vector | float"):
+        if isinstance(other, Vector):
+            # множення вектор на вектор -> скалярний добуток (число)
+            return self.x * other.x + self.y * other.y
+        # множення вектор на число -> новий вектор
+        return Vector(self.x * other, self.y * other)
+
+    @classmethod
+    def create_vector_by_two_points(
+        cls,
+        start_point: tuple,
+        end_point: tuple,
+    ) -> "Vector":
+        # кінець мінус початок
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1],
+        )
+
+    def get_length(self) -> float:
+        # довжина без округлення
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other: "Vector") -> int:
+        # косинус кута = скалярний добуток / (довжина1 * довжина2)
+        cos_a = (self * other) / (self.get_length() * other.get_length())
+        return round(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> int:
+        # кут між вектором і додатною віссю Y
+        # вектор осі Y -> Vector(0, 1)
+        y_axis = Vector(0, 1)
+        return self.angle_between(y_axis)
+
+    def rotate(self, degrees: int) -> "Vector":
+        radians = math.radians(degrees)
+        cos = math.cos(radians)
+        sin = math.sin(radians)
+        new_x = self.x * cos - self.y * sin
+        new_y = self.x * sin + self.y * cos
+        return Vector(new_x, new_y)
