dfc816c1303aac0154c9e3de5af3808ecdf2330e
Ruslan <makar80008@gmail.com>
2026-06-17 14:48:46 +0300

    ГОТОВО!2

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, coordinate_x: float, coordinate_y: float) -> None:
+        self.x = round(coordinate_x, 2)
+        self.y = round(coordinate_y, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
         return Vector(self.x + other.x, self.y + other.y)
@@ -14,9 +14,7 @@ class Vector:
 
     def __mul__(self, other: ("Vector | float | int")) -> ("Vector | float"):
         if isinstance(other, Vector):
-            # множення вектор на вектор -> скалярний добуток (число)
             return self.x * other.x + self.y * other.y
-        # множення вектор на число -> новий вектор
         return Vector(self.x * other, self.y * other)
 
     @classmethod
@@ -25,14 +23,12 @@ class Vector:
         start_point: tuple,
         end_point: tuple,
     ) -> "Vector":
-        # кінець мінус початок
         return cls(
             end_point[0] - start_point[0],
             end_point[1] - start_point[1],
         )
 
     def get_length(self) -> float:
-        # довжина без округлення
         return math.sqrt(self.x ** 2 + self.y ** 2)
 
     def get_normalized(self) -> "Vector":
@@ -40,13 +36,10 @@ class Vector:
         return Vector(self.x / length, self.y / length)
 
     def angle_between(self, other: "Vector") -> int:
-        # косинус кута = скалярний добуток / (довжина1 * довжина2)
         cos_a = (self * other) / (self.get_length() * other.get_length())
         return round(math.degrees(math.acos(cos_a)))
 
     def get_angle(self) -> int:
-        # кут між вектором і додатною віссю Y
-        # вектор осі Y -> Vector(0, 1)
         y_axis = Vector(0, 1)
         return self.angle_between(y_axis)
 
