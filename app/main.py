6d43cff0fa6a7318a55708ef593cf4715ee3e589
Tanya <tanya210300@gmail.com>
2026-06-30 20:32:08 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -8,14 +8,14 @@ class Vector:
         self.y = round(y_coord, 2)
 
     def __add__(self, other: Vector) -> Vector:
-        return Vector(x=self.x + other.x, y=self.y + other.y)
+        return Vector(x_coord=self.x + other.x, y_coord=self.y + other.y)
 
     def __sub__(self, other: Vector) -> Vector:
-        return Vector(x=self.x - other.x, y=self.y - other.y)
+        return Vector(x_coord=self.x - other.x, y_coord=self.y - other.y)
 
     def __mul__(self, other: Vector | float) -> Vector | float:
         if isinstance(other, (int, float)):
-            return Vector(x=self.x * other, y=self.y * other)
+            return Vector(x_coord=self.x * other, y_coord=self.y * other)
         if isinstance(other, Vector):
             return (self.x * other.x) + (self.y * other.y)
         return NotImplemented
@@ -27,15 +27,18 @@ class Vector:
         end_point: tuple[float, float]
     ) -> Vector:
         return cls(
-            x=end_point[0] - start_point[0],
-            y=end_point[1] - start_point[1]
+            x_coord=end_point[0] - start_point[0],
+            y_coord=end_point[1] - start_point[1]
         )
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
 
     def get_normalized(self) -> object:
-        return Vector(x=self.x / self.get_length(), y=self.y / self.length())
+        return Vector(
+            x_coord=self.x / self.get_length(),
+            y_coord=self.y / self.get_length()
+        )
 
     def angle_between(self, vector: object) -> float:
         skal = (self.x * vector.x) + (self.y * vector.y)
@@ -45,7 +48,7 @@ class Vector:
         return round(math.degrees(math.acos(cos_u)))
 
     def get_angle(self) -> float:
-        return round(math.degrees(math.atan2(self.x, self.y)))
+        return round(math.degrees(math.atan2(self.y, self.x)))
 
     def rotate(self, degrees: int) -> object:
         rad = math.radians(degrees)
