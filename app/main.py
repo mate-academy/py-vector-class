51c521a09a9cf384c664d58a17066c25139f2feb
Krzysztof Sobczak <krzysztof.sobczak5081@gmail.com>
2026-07-17 21:15:52 +0200

    Implement class with magic methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+from typing import Union
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: Union[int, float, "Vector"]) -> Union["Vector", float]:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        return Vector(self.x * other, self.y * other)
+        
