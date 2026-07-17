1999c1ad4cc18978c7da7624b64b3400458763f1
Krzysztof Sobczak <krzysztof.sobczak5081@gmail.com>
2026-07-17 21:41:42 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+import math
 from typing import Union
 
 
@@ -6,14 +8,47 @@ class Vector:
         self.x = round(x, 2)
         self.y = round(y, 2)
 
-    def __add__(self, other: "Vector") -> "Vector":
+    def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other: "Vector") -> "Vector":
+    def __sub__(self, other: Vector) -> Vector:
         return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other: Union[int, float, "Vector"]) -> Union["Vector", float]:
+    def __mul__(self, other: Union[int, float, Vector]) -> Union[Vector, float]:
         if isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
         return Vector(self.x * other, self.y * other)
-        
+
+    @classmethod
+    def create_vector_by_two_points(
+        cls, start_point: tuple[float, float], end_point: tuple[float, float]
+    ) -> Vector:
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
+
+    def get_length(self) -> float:
+        return math.hypot(self.x, self.y)
+
+    def get_normalized(self) -> Vector:
+        length = self.get_length()
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other: Vector) -> int:
+        cos_theta = (self * other) / (self.get_length() * other.get_length())
+        cos_theta = max(-1.0, min(1.0, cos_theta))
+        return round(math.degrees(math.acos(cos_theta)))
+
+    def get_angle(self) -> int:
+        cos_theta = self.y / self.get_length()
+        cos_theta = max(-1.0, min(1.0, cos_theta))
+        return round(math.degrees(math.acos(cos_theta)))
+
+    def rotate(self, degrees: int) -> Vector:
+        radians = math.radians(degrees)
+        cos_val = math.cos(radians)
+        sin_val = math.sin(radians)
+        x_prime = self.x * cos_val - self.y * sin_val
+        y_prime = self.x * sin_val + self.y * cos_val
+        return Vector(x_prime, y_prime)
