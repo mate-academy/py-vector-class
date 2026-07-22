08890c87e842a723441a09f880ea69689010150c
Zheka <evgenijsturmak@gmail.com>
2026-07-23 00:53:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,61 @@
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+
+    def __init__(self, point_x: float, point_y: float) -> None:
+        self.x = round(point_x, 2)
+        self.y = round(point_y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(
+            self.x + other.x,
+            self.y + other.y
+        )
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(
+            self.x - other.x,
+            self.y - other.y
+        )
+
+    def __mul__(self, other: int | Vector) -> Vector | float:
+        if not isinstance(other, Vector):
+            return Vector(
+                self.x * other,
+                self.y * other
+            )
+        else:
+            return (self.x * other.x) + (self.y * other.y)
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls, start_vector: tuple, end_vector: tuple) -> Vector:
+        return Vector(
+            end_vector[0] - start_vector[0],
+            end_vector[1] - start_vector[1]
+        )
+
+    def get_length(self) -> float:
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def get_normalized(self) -> Vector:
+        return Vector(
+            self.x / self.get_length(),
+            self.y / self.get_length()
+        )
+
+    def angle_between(self, other: Vector) -> int:
+        numerator = self * other
+        denominator = self.get_length() * other.get_length()
+        return round(math.degrees(math.acos(numerator / denominator)))
+
+    def get_angle(self) -> float:
+        y_axis = Vector(0, 1)
+        return self.angle_between(y_axis)
+
+    def rotate(self, degrees: int) -> Vector:
+        radians = math.radians(degrees)
+        new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(new_x, new_y)
